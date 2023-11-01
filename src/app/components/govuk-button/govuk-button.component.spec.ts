0e69c6940bab0188caec8a1c8d3028008c92762e
Max <02.quad_ideal@icloud.com>
2023-11-01 15:13:50 +0000

    Testing unit tests

--- src/app/components/govuk-button/govuk-button.component.spec.ts
+++ src/app/components/govuk-button/govuk-button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GovukButtonComponent } from './govuk-button.component';
+import { GovukButtonClasses } from '@enums';
 
 describe('GovukButtonComponent', () => {
   let component: GovukButtonComponent;
@@ -8,7 +9,7 @@ describe('GovukButtonComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [GovukButtonComponent]
+      imports: [GovukButtonComponent],
     });
     fixture = TestBed.createComponent(GovukButtonComponent);
     component = fixture.componentInstance;
@@ -17,5 +18,57 @@ describe('GovukButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the warning class', () => {
+    component.buttonStyle = 'warning';
+    fixture.detectChanges();
+
+    expect(component.buttonClassSig()).toBe(
+      `${GovukButtonClasses.default} ${GovukButtonClasses.warning} govuk-!-margin-bottom-0`
+    );
+  });
+
+  it('should have the default class', () => {
+    component.buttonStyle = 'default';
+    fixture.detectChanges();
+
+    expect(component.buttonClassSig()).toBe(`${GovukButtonClasses.default} govuk-!-margin-bottom-0`);
+  });
+
+  it('should have the secondary class', () => {
+    component.buttonStyle = 'secondary';
+    fixture.detectChanges();
+
+    expect(component.buttonClassSig()).toBe(
+      `${GovukButtonClasses.default} ${GovukButtonClasses.secondary} govuk-!-margin-bottom-0`
+    );
+  });
+
+  it('should have the inverse class', () => {
+    component.buttonStyle = 'inverse';
+    fixture.detectChanges();
+
+    expect(component.buttonClassSig()).toBe(
+      `${GovukButtonClasses.default} ${GovukButtonClasses.inverse} govuk-!-margin-bottom-0`
+    );
+  });
+
+  it('should have the start class', () => {
+    component.buttonStyle = 'start';
+    fixture.detectChanges();
+
+    expect(component.buttonClassSig()).toBe(
+      `${GovukButtonClasses.default} ${GovukButtonClasses.start} govuk-!-margin-bottom-0`
+    );
+  });
+
+  it('should have the disabled class', () => {
+    component.buttonStyle = 'disabled';
+    fixture.detectChanges();
+
+    expect(component.buttonClassSig()).toBe(
+      `${GovukButtonClasses.default} ${GovukButtonClasses.disabled} govuk-!-margin-bottom-0`
+    );
   });
 });
