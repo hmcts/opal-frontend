14b88d7e993e93e779cd638afe6b7afccac2767f
Dan Lysiak <danlysiak@hotmail.com>
2023-11-09 23:00:58 +0000

    Prettier fix

--- karma.conf.js
+++ karma.conf.js
@@ -3,29 +3,29 @@
 
 module.exports = function (config) {
   config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-          require('karma-jasmine'),
-          require('karma-chrome-launcher'),
-          require('karma-jasmine-html-reporter'),
-          require('karma-coverage'),
-          require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-      },
-      coverageReporter: {
-        type : 'lcov',
-        dir : 'coverage/'
-      },
-      reporters: ['progress', 'kjhtml', 'coverage'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['ChromeHeadless'],
-      singleRun: false,
-      restartOnFileChange: true
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/',
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true,
   });
 };
