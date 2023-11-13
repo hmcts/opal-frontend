a4a130c419a38b0c1a59f2695499a7e30d35b25a
Max <Max.Holland@HMCTS.NET>
2023-11-13 17:15:23 +0000

    Unit test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { GovukHeaderComponent } from './components/govuk-header/govuk-header.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, GovukHeaderComponent],
       declarations: [AppComponent],
     }),
   );
