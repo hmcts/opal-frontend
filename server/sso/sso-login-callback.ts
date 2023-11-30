f245e509714fe47984a52e53791541fb2893d338
Max Holland <Max.Holland@HMCTS.NET>
2023-11-30 16:46:42 +0000

    Added timeout (#54)

--- server/sso/sso-login-callback.ts
+++ server/sso/sso-login-callback.ts
@@ -13,6 +13,9 @@ export default (req: Request, res: Response, next: NextFunction) => {
     if (err) {
       return next(err);
     }
-    res.redirect('/');
+
+    setTimeout(() => {
+      res.redirect('/');
+    }, 500);
   });
 };
