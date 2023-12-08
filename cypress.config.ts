25b8fbb7f79a7b9259cb296bad4faa15d595c9ea
CadeFaulkner <cade.faulkner@hmcts.net>
2023-12-08 10:53:15 +0000

    fixed formatting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'cypress';
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import { preprocessor } from "@badeball/cypress-cucumber-preprocessor/browserify";
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import { preprocessor } from '@badeball/cypress-cucumber-preprocessor/browserify';
 
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
+  config: Cypress.PluginConfigOptions,
 ): Promise<Cypress.PluginConfigOptions> {
   await addCucumberPreprocessorPlugin(on, config);
 
   on(
-    "file:preprocessor",
+    'file:preprocessor',
     preprocessor(config, {
-      typescript: require.resolve("typescript"),
-    })
+      typescript: require.resolve('typescript'),
+    }),
   );
   return config;
 }
@@ -28,7 +28,7 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: process.env['TEST_URL'] || 'http://localhost:4000/',
-    specPattern: "**/*.feature",
+    specPattern: '**/*.feature',
     screenshotsFolder: 'cypress/results/assets',
     setupNodeEvents,
   },
