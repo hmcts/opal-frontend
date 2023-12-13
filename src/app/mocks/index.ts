afdf9a2f9d7751d44e14fd3696c382dffa2bed05
Max <Max.Holland@HMCTS.NET>
2023-12-13 12:12:20 +0000

    Added state service unit tests

--- src/app/mocks/index.ts
+++ src/app/mocks/index.ts
@@ -3,5 +3,13 @@ import { SEARCH_TYPE_RADIOS_MOCK } from './search-type-radios.mock';
 import { COMPANY_CHECKBOXES_MOCK } from './company-checkboxes.mock';
 import { DATE_INPUTS_MOCK } from './date-inputs.mock';
 import { CT_LIST_MOCK } from './ct-list.mock';
+import { SEARCH_STATE_MOCK } from './search-state.mock';
 
-export { DEFENDANT_ACCOUNT_MOCK, SEARCH_TYPE_RADIOS_MOCK, COMPANY_CHECKBOXES_MOCK, DATE_INPUTS_MOCK, CT_LIST_MOCK };
+export {
+  DEFENDANT_ACCOUNT_MOCK,
+  SEARCH_TYPE_RADIOS_MOCK,
+  COMPANY_CHECKBOXES_MOCK,
+  DATE_INPUTS_MOCK,
+  CT_LIST_MOCK,
+  SEARCH_STATE_MOCK,
+};
