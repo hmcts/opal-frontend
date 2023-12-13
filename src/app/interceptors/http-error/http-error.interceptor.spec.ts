4f12184438d7a6d70d2c6c69c1beed0a34851d54
Max <Max.Holland@HMCTS.NET>
2023-12-13 10:17:21 +0000

    Added interceptor unit test

--- src/app/interceptors/http-error/http-error.interceptor.spec.ts
+++ src/app/interceptors/http-error/http-error.interceptor.spec.ts
@@ -1,17 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 import { httpErrorInterceptor } from './http-error.interceptor';
+import { throwError } from 'rxjs';
+import { StateService } from '@services';
 
 describe('httpErrorInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
+  const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => httpErrorInterceptor(req, next));
+  let stateService: StateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [StateService],
+    });
+    stateService = TestBed.inject(StateService);
   });
 
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should intercept and set an error', () => {
+    const errorResponse = new HttpErrorResponse({ status: 401 });
+    const request = new HttpRequest('GET', '/test');
+    const next: HttpHandlerFn = () => throwError(() => errorResponse);
+    interceptor(request, next).subscribe({
+      error: () => {
+        const errorState = stateService.error().error;
+        expect(errorState).toBeTruthy();
+      },
+    });
   });
 });
