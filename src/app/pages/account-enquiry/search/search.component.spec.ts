7ee13411ad1bda7828fe1f4f47a97342395d7752
Max <Max.Holland@HMCTS.NET>
2023-12-14 16:10:29 +0000

    Added search unit tests

--- src/app/pages/account-enquiry/search/search.component.spec.ts
+++ src/app/pages/account-enquiry/search/search.component.spec.ts
@@ -1,21 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { SearchComponent } from './search.component';
 import { StateService } from '@services';
 import { SEARCH_STATE_MOCK } from '@mocks';
+import { AccountEnquiryRoutes } from '@enums';
 
 fdescribe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
   let stateService: StateService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent],
+      imports: [SearchComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     stateService = TestBed.inject(StateService);
 
     fixture.detectChanges();
@@ -50,5 +54,21 @@ fdescribe('SearchComponent', () => {
 
     component.handleClearForm();
     expect(component.searchForm.value.court).toBeNull();
+  });
+
+  it('should submit the form', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    stateService.accountEnquiry.set({ search: null });
+    expect(stateService.accountEnquiry().search).toBeNull();
+
+    component.searchForm.controls['court'].setValue('Bristol');
+    expect(component.searchForm.value.court).toBe('Bristol');
+
+    component.handleFormSubmit();
+
+    expect(stateService.accountEnquiry().search?.court).toBe('Bristol');
+
+    expect(navigateSpy).toHaveBeenCalledWith([AccountEnquiryRoutes.matches]);
   });
 });
