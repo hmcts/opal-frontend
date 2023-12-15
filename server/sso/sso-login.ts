4703ca94767c58be71de94377dd93b855b1eaaf6
Max <Max.Holland@HMCTS.NET>
2023-12-15 14:30:51 +0000

    Added user login path

--- server/sso/sso-login.ts
+++ server/sso/sso-login.ts
@@ -1,17 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import axios from 'axios';
+import config from 'config';
+
+const INTERNAL_USER_LOGIN = `${config.get('opal-api.url')}/internal-user/login-or-refresh`;
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  res.redirect(INTERNAL_USER_LOGIN);
 
-  try {
-    const response = await axios.get(url);
+  // const url = 'http://localhost:4550/internal-user/login-or-refresh';
 
-    // For now to test session creation
-    if (response) {
-      res.redirect('/sso/login-callback');
-    }
-  } catch (error) {
-    next(new Error('Error trying to fetch login page'));
-  }
+  // try {
+  //   const response = await axios.get(url);
+  //   // console.log(response.data.res);
+  //   // For now to test session creation
+  //   if (url) {
+  //     res.redirect(INTERNAL_USER_LOGIN);
+  //   }
+  // } catch (error) {
+  //   next(new Error('Error trying to fetch login page'));
+  // }
 };
