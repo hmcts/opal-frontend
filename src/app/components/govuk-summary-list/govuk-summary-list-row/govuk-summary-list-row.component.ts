b367e563874e8e841b92d9a61ab71a0604750e28
Max <Max.Holland@HMCTS.NET>
2023-12-20 16:37:44 +0000

    Remove ViewEncapsulation import in govuk-summary-list-row.component.ts

--- src/app/components/govuk-summary-list/govuk-summary-list-row/govuk-summary-list-row.component.ts
+++ src/app/components/govuk-summary-list/govuk-summary-list-row/govuk-summary-list-row.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-govuk-summary-list-row',
