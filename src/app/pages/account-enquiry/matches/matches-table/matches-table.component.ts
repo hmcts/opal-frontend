a4f197c537451658605fec5a4e5f5dc198797b90
Max <Max.Holland@HMCTS.NET>
2023-12-20 13:57:48 +0000

    Added 117 signal changes

--- src/app/pages/account-enquiry/matches/matches-table/matches-table.component.ts
+++ src/app/pages/account-enquiry/matches/matches-table/matches-table.component.ts
@@ -1,6 +1,6 @@
 import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, signal } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { GovukButtonComponent, PaginationComponent } from '@components';
@@ -12,15 +12,17 @@ import { ISearchDefendantAccount, ISearchDefendantAccounts } from '@interfaces';
   imports: [CommonModule, RouterModule, GovukButtonComponent, PaginationComponent, CdkTableModule, PaginationComponent],
   templateUrl: './matches-table.component.html',
   styleUrl: './matches-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MatchesTableComponent implements OnInit {
-  @Input() data!: ISearchDefendantAccounts;
+  @Input({ required: true }) data!: ISearchDefendantAccounts;
 
-  public tableData!: MatTableDataSource<ISearchDefendantAccount>;
-
-  public currentPage: number = 1;
-  public pageLimit: number = 10;
-  public pagedRows!: MatTableDataSource<ISearchDefendantAccount>;
+  public currentPage = signal(1);
+  public pageLimit = signal(10);
+  // public pagedRows!: MatTableDataSource<ISearchDefendantAccount>;
+  public pagedRows = signal<MatTableDataSource<ISearchDefendantAccount>>(
+    this.wrapTableDataSource(this.paginate([], this.pageLimit(), this.currentPage())),
+  );
 
   public readonly displayedColumns: string[] = [
     'accountNo',
@@ -60,11 +62,15 @@ export class MatchesTableComponent implements OnInit {
    * @param event The new page number.
    */
   public onPageChanged(event: number): void {
-    this.currentPage = event;
-    this.pagedRows = this.wrapTableDataSource(this.paginate(this.data.searchResults, this.pageLimit, this.currentPage));
+    this.currentPage.set(event);
+    this.pagedRows.set(
+      this.wrapTableDataSource(this.paginate(this.data.searchResults, this.pageLimit(), this.currentPage())),
+    );
   }
 
   public ngOnInit(): void {
-    this.pagedRows = this.wrapTableDataSource(this.paginate(this.data.searchResults, this.pageLimit, this.currentPage));
+    this.pagedRows.set(
+      this.wrapTableDataSource(this.paginate(this.data.searchResults, this.pageLimit(), this.currentPage())),
+    );
   }
 }
