a4b5e7a04ba506b9a9f1792f9455513c451ac380
Max <Max.Holland@HMCTS.NET>
2024-01-05 12:44:59 +0000

    Update frontend hostname configuration

--- server/sso/sso-login.ts
+++ server/sso/sso-login.ts
@@ -5,5 +5,8 @@ import config from 'config';
 const INTERNAL_USER_LOGIN = `${config.get('opal-api.url')}/internal-user/login-or-refresh`;
 
 export default async (req: Request, res: Response) => {
-  res.redirect(INTERNAL_USER_LOGIN);
+  const env = process.env['NODE_ENV'] || 'development';
+  const hostname = env === 'development' ? config.get('frontend-hostname.dev') : config.get('frontend-hostname.prod');
+  const url = `${INTERNAL_USER_LOGIN}?redirect_uri=${hostname}/sso/login-callback`;
+  res.redirect(url);
 };
--- server/sso/sso-logout.ts
+++ server/sso/sso-logout.ts
@@ -5,6 +5,10 @@ import config from 'config';
 const INTERNAL_USER_LOGOUT = `${config.get('opal-api.url')}/internal-user/logout`;
 
 export default async (req: Request, res: Response, next: NextFunction) => {
+  const env = process.env['NODE_ENV'] || 'development';
+  const hostname = env === 'development' ? config.get('frontend-hostname.dev') : config.get('frontend-hostname.prod');
+  const url = `${INTERNAL_USER_LOGOUT}?redirect_uri=${hostname}/sso/login-callback`;
+
   try {
     let accessToken;
 
@@ -17,7 +21,7 @@ export default async (req: Request, res: Response, next: NextFunction) => {
       next(new Error('No access token found in session'));
     }
 
-    const response = await axios.get(INTERNAL_USER_LOGOUT, {
+    const response = await axios.get(url, {
       headers: { Authorization: `Bearer ${accessToken}` },
     });
 
