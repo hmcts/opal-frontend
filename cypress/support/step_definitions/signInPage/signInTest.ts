cd2f3ba1364b330c5d307586633711c4f7cbf522
CadeFaulkner <cade.faulkner@hmcts.net>
2024-01-18 10:36:01 +0000

    lint fixes

--- cypress/support/step_definitions/signInPage/signInTest.ts
+++ cypress/support/step_definitions/signInPage/signInTest.ts
@@ -9,36 +9,35 @@ When('I sign in', () => {
   const emailSSO: string = 'opal-test@hmcts.net';
   const passwordSSO: string = 'OpalFinesService1';
 
-  cy.location("href").then((href: string) => {
+  cy.location('href').then((href: string) => {
+    if (href.includes('pr-')) {
+      cy.get('app-govuk-button > #fetch-api-data').contains('Sign in').click();
+      cy.get('.govuk-fieldset__heading').should('contain', 'Account Enquiry');
+    } else {
+      cy.get('app-govuk-button > #fetch-api-data').contains('Sign in').click();
 
-  if (href.includes("pr-")) {
-    cy.get('app-govuk-button > #fetch-api-data').contains('Sign in').click();
-    cy.get('.govuk-fieldset__heading').should('contain', 'Account Enquiry');
-  } else {
-    cy.get('app-govuk-button > #fetch-api-data').contains('Sign in').click();
-
-    cy.origin(
-      'https://login.microsoftonline.com',
-      {
-        args: {
-          emailSSO,
-          passwordSSO,
+      cy.origin(
+        'https://login.microsoftonline.com',
+        {
+          args: {
+            emailSSO,
+            passwordSSO,
+          },
         },
-      },
-      ({ emailSSO, passwordSSO }) => {
-        cy.wait(500);
-        cy.get('input[type="email"]').type(emailSSO);
-        cy.get('input[type="submit"]').click();
+        ({ emailSSO, passwordSSO }) => {
+          cy.wait(500);
+          cy.get('input[type="email"]').type(emailSSO);
+          cy.get('input[type="submit"]').click();
 
-        cy.get('input[type="password"]').type(passwordSSO);
-        cy.get('input[type="submit"]').click();
-        cy.get('#idBtn_Back').click();
-      },
-    );
+          cy.get('input[type="password"]').type(passwordSSO);
+          cy.get('input[type="submit"]').click();
+          cy.get('#idBtn_Back').click();
+        },
+      );
 
-    cy.get('.govuk-fieldset__heading').should('contain', 'Account Enquiry');
-  }
-})
+      cy.get('.govuk-fieldset__heading').should('contain', 'Account Enquiry');
+    }
+  });
 });
 
 Then('I see {string} in the header', (header) => {
