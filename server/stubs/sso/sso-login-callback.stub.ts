7ff5506c7b12ecd4beae35c6bc22e7c98799ad0e
Max <Max.Holland@HMCTS.NET>
2024-01-18 18:06:23 +0000

    Debugging

--- server/stubs/sso/sso-login-callback.stub.ts
+++ server/stubs/sso/sso-login-callback.stub.ts
@@ -7,19 +7,20 @@ import config from 'config';
 const INTERNAL_JWT = `${config.get('opal-api.url')}/api/testing-support/handle-oauth-code`;
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  const logger = Logger.getLogger('login-callback-stub');
-  const result = await axios.post(INTERNAL_JWT);
+  res.send('login-callback-stub');
+  // const logger = Logger.getLogger('login-callback-stub');
+  // const result = await axios.post(INTERNAL_JWT);
 
-  const mockSecurityToken = result.data;
-  req.session.securityToken = mockSecurityToken;
+  // const mockSecurityToken = result.data;
+  // req.session.securityToken = mockSecurityToken;
 
-  req.session.save((err) => {
-    if (err) {
-      return next(err);
-    }
+  // req.session.save((err) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
 
-    logger.info(`token saved`);
+  //   logger.info(`token saved`);
 
-    res.redirect('/');
-  });
+  //   res.redirect('/');
+  // });
 };
