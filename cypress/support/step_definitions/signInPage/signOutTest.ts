987166d313d128d92a3854309b3eee3cca631890
CadeFaulkner <cade.faulkner@hmcts.net>
2024-01-19 14:08:31 +0000

    automated tests for PO-151

---
+++ cypress/support/step_definitions/signInPage/signOutTest.ts
@@ -0,0 +1,13 @@
+import { When, Then } from '@badeball/cypress-cucumber-preprocessor/';
+
+When('I attempt to get back to the account enquiry search screen by changing the url', () => {
+  cy.visit('/account-enquiry/search');
+});
+When('I attempt to get back to the account enquiry matches screen by changing the url', () => {
+  cy.visit('/account-enquiry/matches');
+});
+Then('I do not see {string} in the header', (header) => {
+  cy.get('.h1.govuk-fieldset__heading').should('not.contain', header);
+});
+
+
