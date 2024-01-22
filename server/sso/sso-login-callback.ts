a7904ccaca8982fc6a5136d48155aae95cea4270
Max <Max.Holland@HMCTS.NET>
2024-01-22 09:56:33 +0000

    Add logging statements to SSO routes

--- server/sso/sso-login-callback.ts
+++ server/sso/sso-login-callback.ts
@@ -7,19 +7,29 @@ const INTERNAL_USER_CALLBACK = `${config.get('opal-api.url')}/internal-user/hand
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   const logger = Logger.getLogger('login-callback');
+
+  logger.info(`Entered login callback file`);
   try {
+    logger.info(`Trying to make a request to ${INTERNAL_USER_CALLBACK}`);
+
     const result = await axios.post<any>(INTERNAL_USER_CALLBACK, req.body, {
       headers: { 'content-type': 'application/x-www-form-urlencoded' },
     });
 
     const securityToken = result.data;
 
+    logger.info(`Access Token received`);
+    logger.info(`Access Token: ${securityToken}`);
+
     req.session.securityToken = securityToken;
 
     req.session.save((err) => {
       if (err) {
+        logger.error('Error saving session', err);
         return next(err);
       }
+
+      logger.info(`Token saved in session`);
       res.redirect('/');
     });
   } catch (error) {
--- server/sso/sso-logout.ts
+++ server/sso/sso-logout.ts
@@ -1,36 +1,46 @@
 import axios from 'axios';
 import { NextFunction, Request, Response } from 'express';
 import config from 'config';
+import { Logger } from '@hmcts/nodejs-logging';
 
 const INTERNAL_USER_LOGOUT = `${config.get('opal-api.url')}/internal-user/logout`;
 
 export default async (req: Request, res: Response, next: NextFunction) => {
+  const logger = Logger.getLogger('login');
+
   const env = process.env['NODE_ENV'] || 'development';
   const hostname = env === 'development' ? config.get('frontend-hostname.dev') : config.get('frontend-hostname.prod');
   const url = `${INTERNAL_USER_LOGOUT}?redirect_uri=${hostname}/sso/login-callback`;
+
+  logger.info(`Entered logout file`);
+  logger.info(`Environment: ${env}`);
+  logger.info(`Hostname: ${hostname}`);
+  logger.info(`Redirect url: ${url}`);
 
   try {
     let accessToken;
 
     if (req.session.securityToken) {
       accessToken = req.session.securityToken.accessToken;
+      logger.info(`Access token: ${accessToken}`);
     }
-    console.log('accessToken', accessToken);
 
     if (!accessToken) {
+      logger.error('No access token found in session');
       next(new Error('No access token found in session'));
     }
 
+    logger.info(`Trying to make a request to ${url}`);
     const response = await axios.get(url, {
       headers: { Authorization: `Bearer ${accessToken}` },
     });
 
-    // For now to test session creation
     if (response) {
+      logger.info(`Response received, redirecting to /sso/logout-callback`);
       res.redirect('/sso/logout-callback');
     }
   } catch (error) {
-    console.log('Error logging out', error);
+    logger.error('Error logging out', error);
     next(new Error('Error logging out'));
   }
 };
