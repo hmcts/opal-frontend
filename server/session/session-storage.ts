8e4bc37d5240b2a8ef68a270eeb2aef0b92149ba
Max <Max.Holland@HMCTS.NET>
2024-01-25 10:44:38 +0000

    Remove commented out code for session storage configuration

--- server/session/session-storage.ts
+++ server/session/session-storage.ts
@@ -10,41 +10,6 @@ import FileStoreFactory from 'session-file-store';
 const FileStore = FileStoreFactory(session);
 const logger = Logger.getLogger('session-storage');
 
-// const getStore = () => {
-//   if (config.get('features.redis.enabled')) {
-//     logger.info('Using Redis session store', config.get('secrets.opal.redis-connection-string'));
-//     const client = createClient({ url: config.get('secrets.opal.redis-connection-string') });
-
-//     client.connect().catch(logger.error);
-
-//     return new RedisStore({ client });
-//   }
-
-//   return new FileStore({ path: '/tmp' });
-// };
-
-// export default (app: Application) => {
-//   app.use(cookieParser());
-//   app.set('trust proxy', 1);
-
-//   app.use(
-//     session({
-//       name: config.get('session.prefix'),
-//       resave: false,
-//       saveUninitialized: false,
-//       secret: config.get('secrets.opal.opal-frontend-session-secret'),
-//       cookie: {
-//         httpOnly: true,
-//         maxAge: config.get('session.maxAge'),
-//         sameSite: 'lax',
-//         secure: config.get('session.secure'),
-//       },
-//       rolling: true,
-//       store: getStore(),
-//     }),
-//   );
-// };
-
 export default class SessionStorage {
   public enableFor(app: Application): void {
     app.use(cookieParser());
