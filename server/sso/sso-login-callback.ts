f1bf5c07b3beb246f5a61f6b967384deca16e4d0
Max Holland <Max.Holland@HMCTS.NET>
2024-01-25 16:51:51 +0000

    Revert "Added 504 logging" (#165)

--- server/sso/sso-login-callback.ts
+++ server/sso/sso-login-callback.ts
@@ -4,28 +4,24 @@ import axios from 'axios';
 import config from 'config';
 
 const INTERNAL_USER_CALLBACK = `${config.get('opal-api.url')}/internal-user/handle-oauth-code`;
-const logger = Logger.getLogger('login-callback');
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  logger.info('Login callback received');
+  const logger = Logger.getLogger('login-callback');
+
   try {
-    logger.info('Calling internal user callback', INTERNAL_USER_CALLBACK);
     const result = await axios.post<any>(INTERNAL_USER_CALLBACK, req.body, {
       headers: { 'content-type': 'application/x-www-form-urlencoded' },
     });
 
-    logger.info('Received security token', result.data);
-
     const securityToken = result.data;
     req.session.securityToken = securityToken;
 
-    logger.info('Saving session');
     req.session.save((err) => {
       if (err) {
         logger.error('Error saving session', err);
         return next(err);
       }
-      logger.info('Session saved');
+
       res.redirect('/');
     });
   } catch (error) {
