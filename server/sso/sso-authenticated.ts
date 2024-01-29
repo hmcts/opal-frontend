0eb8fd9ccd720b6289cb6f1c157ebbf2bd024f8e
Max Holland <Max.Holland@HMCTS.NET>
2024-01-29 16:29:54 +0000

    Staging sso test (#183)

--- server/sso/sso-authenticated.ts
+++ server/sso/sso-authenticated.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
+import { Logger } from '@hmcts/nodejs-logging';
+
+const logger = Logger.getLogger('authenticated');
 
 export default (req: Request, res: Response) => {
   const token = req.session.securityToken?.accessToken;
+
+  logger.info(`Checking if user is authenticated. Token: ${token}`);
 
   // Don't allow caching of this endpoint
   res.header('Cache-Control', 'no-store, must-revalidate');
