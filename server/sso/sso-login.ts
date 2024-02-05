56e31262b095a44b47924f5b4b88be6c43040d0a
Max <Max.Holland@HMCTS.NET>
2024-02-05 11:12:26 +0000

    Add optimization flag and set NODE_ENV to production
    This commit adds the optimization flag and sets the NODE_ENV variable to production in the angular.json and values.yaml files.

--- server/sso/sso-login.ts
+++ server/sso/sso-login.ts
@@ -7,9 +7,11 @@ const INTERNAL_USER_LOGIN = `${config.get('opal-api.url')}/internal-user/login-o
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   const logger = Logger.getLogger('login');
-  const env = process.env['NODE_ENV'] || 'development';
+  const env = config.get('node-env') || 'development';
   const hostname = env === 'development' ? config.get('frontend-hostname.dev') : config.get('frontend-hostname.prod');
   const url = `${INTERNAL_USER_LOGIN}?redirect_uri=${hostname}/sso/login-callback`;
+
+  console.log(env);
 
   try {
     const response = await axios.get(url);
