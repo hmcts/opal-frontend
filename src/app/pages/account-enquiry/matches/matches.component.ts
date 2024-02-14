e84fccb3674158e385034a77a2571cff5e9a89a5
Max <Max.Holland@HMCTS.NET>
2024-02-14 10:53:49 +0000

    Add handleViewDefendantAccount method to MatchesComponent

--- src/app/pages/account-enquiry/matches/matches.component.ts
+++ src/app/pages/account-enquiry/matches/matches.component.ts
@@ -37,6 +37,11 @@ export class MatchesComponent {
     this.router.navigate([AccountEnquiryRoutes.search]);
   }
 
+  /**
+   * Navigates to the details page of a defendant account.
+   *
+   * @param defendantAccountId - The ID of the defendant account.
+   */
   public handleViewDefendantAccount(defendantAccountId: number): void {
     this.router.navigate([AccountEnquiryRoutes.details, defendantAccountId]);
   }
