376dee71a7549aaf29eacb1f19363e45dbd14aed
Sabah Irfan <sabah.irfan@gmail.com>
2024-02-28 13:17:41 +0000

    Update SecurityToken interface

--- server/interfaces/securityToken.ts
+++ server/interfaces/securityToken.ts
@@ -1,8 +1,11 @@
 // import UserState from './userState';
 
+import UserState from "./userState";
+
 class SecurityToken {
   //   userState: UserState | undefined;
   accessToken!: string;
+  userState?: UserState;
 }
 
 export default SecurityToken;
