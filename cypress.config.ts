e883f48490020e1ace8b88d9f9465f2c57c834b4
CadeFaulkner <cade.faulkner@hmcts.net>
2024-02-28 15:47:09 +0000

    mode switch and scripts for opal/legacy mode

--- cypress.config.ts
+++ cypress.config.ts
@@ -29,5 +29,7 @@ export default defineConfig({
   env: {
     CYPRESS_TEST_EMAIL: process.env['OPAL_TEST_USER_EMAIL'],
     CYPRESS_TEST_PASSWORD: process.env['OPAL_TEST_USER_PASSWORD'],
+    TEST_MODE: process.env['TEST_MODE'] || 'OPAL',
+    API_URL: process.env['TEST_URL'] || 'http://localhost:4550',
   },
 });
