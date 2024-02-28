659cf08e029d0d41360f06af5f95964cf8317b2a
Max <Max.Holland@HMCTS.NET>
2024-02-28 10:05:59 +0000

    Refactor CourtService caching logic

--- src/app/services/court-service/court.service.ts
+++ src/app/services/court-service/court.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnDestroy, inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { ApiPaths } from '@enums';
 import { ISearchCourt, ISearchCourtBody } from '@interfaces';
 import { Observable, shareReplay } from 'rxjs';
@@ -7,17 +7,9 @@ import { Observable, shareReplay } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class CourtService implements OnDestroy {
+export class CourtService {
   private readonly http = inject(HttpClient);
-
-  private courtCache$!: Observable<ISearchCourt[]>;
-  private courtCacheTime: Date | null = null;
-  private courtCacheExpiry = 30000;
-
-  private refreshCacheIfNeeded(cacheTime: Date | null, cacheExpiry: number): boolean {
-    const now = new Date();
-    return cacheTime && now.getTime() - cacheTime.getTime() > cacheExpiry ? true : false;
-  }
+  private courtCache$: { [key: string]: Observable<ISearchCourt[]> } = {};
 
   /**
    * Searches for courts based on the provided search criteria.
@@ -25,18 +17,13 @@ export class CourtService implements OnDestroy {
    * @returns An Observable that emits an array of ISearchCourt objects.
    */
   public searchCourt(body: ISearchCourtBody): Observable<ISearchCourt[]> {
-    const refreshCache = this.refreshCacheIfNeeded(this.courtCacheTime, this.courtCacheExpiry);
-    const setCache = !this.courtCache$ || refreshCache;
+    const key = `${JSON.stringify(body.courtId)}${JSON.stringify(body.courtCode)}}`;
 
-    if (setCache) {
-      this.courtCache$ = this.http.post<ISearchCourt[]>(ApiPaths.courtSearch, body).pipe(shareReplay(1));
-      this.courtCacheTime = new Date();
+    // Court search is cached to prevent multiple requests for the same data.
+    if (!this.courtCache$[key]) {
+      this.courtCache$[key] = this.http.post<ISearchCourt[]>(ApiPaths.courtSearch, body).pipe(shareReplay(1));
     }
 
-    return this.courtCache$;
-  }
-
-  ngOnDestroy(): void {
-    console.log('CourtService ngOnDestroy');
+    return this.courtCache$[key];
   }
 }
