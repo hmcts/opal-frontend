9d5538381526bb9190a12f64f8aea7a936b04c66
CadeFaulkner <cade.faulkner@hmcts.net>
2024-02-28 18:24:37 +0000

    ammending endpoint

--- cypress/support/step_definitions/modeSwitch/modeSwitch.ts
+++ cypress/support/step_definitions/modeSwitch/modeSwitch.ts
@@ -2,13 +2,13 @@ import { AfterAll, BeforeAll } from '@badeball/cypress-cucumber-preprocessor';
 
 BeforeAll(() => {
   if (Cypress.env('TEST_MODE') == 'OPAL') {
-    cy.request('PUT', '/api/testing-support/app-mode', { mode: 'opal' });
+    cy.request('PUT', 'api/testing-support/app-mode', { mode: 'opal' });
   }
   if (Cypress.env('TEST_MODE') == 'LEGACY') {
-    cy.request('PUT', '/api/testing-support/app-mode', { mode: 'legacy' });
+    cy.request('PUT', 'api/testing-support/app-mode', { mode: 'legacy' });
   }
 });
 
 AfterAll(() => {
-  cy.request('PUT', '/api/testing-support/app-mode', { mode: 'opal' });
+  cy.request('PUT', 'api/testing-support/app-mode', { mode: 'opal' });
 });
