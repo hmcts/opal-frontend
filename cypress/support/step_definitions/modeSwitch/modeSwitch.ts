d9d64ce78a8996def788a8101ae5fdb3454e2ea7
CadeFaulkner <cade.faulkner@hmcts.net>
2024-02-29 11:50:45 +0000

    test if GET works from endpoint

--- cypress/support/step_definitions/modeSwitch/modeSwitch.ts
+++ cypress/support/step_definitions/modeSwitch/modeSwitch.ts
@@ -2,13 +2,19 @@ import { AfterAll, BeforeAll } from '@badeball/cypress-cucumber-preprocessor';
 
 BeforeAll(() => {
   if (Cypress.env('TEST_MODE') == 'OPAL') {
+    cy.visit('/');
+    cy.request('GET', 'api/testing-support/app-mode');
     cy.request('PUT', 'api/testing-support/app-mode', { mode: 'opal' });
   }
   if (Cypress.env('TEST_MODE') == 'LEGACY') {
+    cy.visit('/');
+    cy.request('GET', 'api/testing-support/app-mode');
     cy.request('PUT', 'api/testing-support/app-mode', { mode: 'legacy' });
   }
 });
 
 AfterAll(() => {
+  cy.visit('/');
+  cy.request('GET', 'api/testing-support/app-mode');
   cy.request('PUT', 'api/testing-support/app-mode', { mode: 'opal' });
 });
