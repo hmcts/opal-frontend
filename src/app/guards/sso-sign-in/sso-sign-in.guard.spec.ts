8b0f55550a3a5efb7d14c9a4228331ce3ce73808
Max <Max.Holland@HMCTS.NET>
2024-03-25 16:04:01 +0000

    Fix typo in sso-sign-in.guard.ts and update unit tests

--- src/app/guards/sso-sign-in/sso-sign-in.guard.spec.ts
+++ src/app/guards/sso-sign-in/sso-sign-in.guard.spec.ts
@@ -1,17 +1,76 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlSegmentGroup,
+  UrlTree,
+} from '@angular/router';
 
-import { ssoEnabledGuard } from './sso-sign-in..guard';
+import { ssoSignInGuard } from './sso-sign-in.guard';
 
-describe('ssoEnabledGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => ssoEnabledGuard(...guardParameters));
+import { StateService } from '@services';
+
+function runSsoSignInGuard(guard: typeof ssoSignInGuard, guardParameters: boolean, urlPath: string) {
+  const dummyRoute = new ActivatedRouteSnapshot();
+  dummyRoute.url = [new UrlSegment(urlPath, {})];
+  const dummyState: RouterStateSnapshot = { url: urlPath, root: new ActivatedRouteSnapshot() };
+  return TestBed.runInInjectionContext(() => guard(guardParameters)(dummyRoute, dummyState));
+}
+
+fdescribe('ssoSignInGuard', () => {
+  let mockStateService: jasmine.SpyObj<StateService>;
+  let mockRouter: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockStateService = jasmine.createSpyObj(ssoSignInGuard, ['ssoEnabled']);
+    mockRouter = jasmine.createSpyObj(ssoSignInGuard, ['navigate', 'createUrlTree', 'parseUrl']);
+    mockRouter.parseUrl.and.callFake((url: string) => {
+      const urlTree = new UrlTree();
+      const urlSegment = new UrlSegment(url, {});
+      urlTree.root = new UrlSegmentGroup([urlSegment], {});
+      return urlTree;
+    });
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Router,
+          useValue: mockRouter,
+        },
+        {
+          provide: StateService,
+          useValue: mockStateService,
+        },
+      ],
+    });
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(TestBed.runInInjectionContext(() => ssoSignInGuard(true))).toBeTruthy();
+  });
+
+  it('should return true if sso enabled and sso route ', () => {
+    mockStateService.ssoEnabled = true;
+    expect(runSsoSignInGuard(ssoSignInGuard, true, '/sign-in')).toBeTruthy();
+  });
+
+  it('should return true if sso enabled and sso route ', () => {
+    mockStateService.ssoEnabled = false;
+    expect(runSsoSignInGuard(ssoSignInGuard, false, '/sign-in-stub')).toBeTruthy();
+  });
+
+  it('should re-route if no access', () => {
+    mockStateService.ssoEnabled = false;
+    runSsoSignInGuard(ssoSignInGuard, true, '/sign-in');
+    expect(mockRouter.createUrlTree).toHaveBeenCalledOnceWith([`/sign-in-stub`]);
+  });
+
+  it('should re-route if no access', () => {
+    mockStateService.ssoEnabled = true;
+    runSsoSignInGuard(ssoSignInGuard, false, '/sign-in-stub');
+    expect(mockRouter.createUrlTree).toHaveBeenCalledOnceWith([`/sign-in`]);
   });
 });
