3eb4e655ed123afc9e27e6567193fa5b01156bba
Max <Max.Holland@HMCTS.NET>
2024-03-26 16:43:47 +0000

    Add SSO route handling in sso-sign-in.guard.ts

--- src/app/guards/sso-sign-in/sso-sign-in.guard.ts
+++ src/app/guards/sso-sign-in/sso-sign-in.guard.ts
@@ -8,10 +8,12 @@ export const ssoSignInGuard = (ssoRoute: boolean): CanActivateFn => {
     const ssoEnabled = inject(StateService).ssoEnabled;
     const router = inject(Router);
 
+    // If it's an SSO route, and SSO is not enabled then go to the sign in stub
     if (ssoRoute && !ssoEnabled) {
       return router.createUrlTree([`/${RoutingPaths.signInStub}`]);
     }
 
+    // If it's not an SSO route, and SSO is enabled then go to the sign in page
     if (!ssoRoute && ssoEnabled) {
       return router.createUrlTree([`/${RoutingPaths.signIn}`]);
     }
