78b24160e4ae95c4c950feb5aa3dbaad51f8efed
Max <Max.Holland@HMCTS.NET>
2024-03-26 13:49:40 +0000

    Add sign-in form to sign-in-stub component

--- src/app/pages/sign-in-stub/sign-in-stub.component.ts
+++ src/app/pages/sign-in-stub/sign-in-stub.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GovukButtonComponent, GovukTextInputComponent } from '@components';
 
 @Component({
   selector: 'app-sign-in-stub',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, GovukTextInputComponent, GovukButtonComponent],
   templateUrl: './sign-in-stub.component.html',
-  styleUrl: './sign-in-stub.component.scss'
+  styleUrl: './sign-in-stub.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignInStubComponent {
+export class SignInStubComponent implements OnInit {
+  public signInForm!: FormGroup;
 
+  private setupSignInForm(): void {
+    this.signInForm = new FormGroup({
+      username: new FormControl(null),
+    });
+  }
+
+  public handleFormSubmit(): void {
+    console.log(this.signInForm.value);
+  }
+
+  public ngOnInit(): void {
+    this.setupSignInForm();
+  }
 }
