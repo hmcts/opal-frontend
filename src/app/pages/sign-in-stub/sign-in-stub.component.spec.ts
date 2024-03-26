dec29356bdbfd52c5006df37316999ba6de8c461
Max <Max.Holland@HMCTS.NET>
2024-03-26 15:48:50 +0000

    Fix SignInStubComponent tests

--- src/app/pages/sign-in-stub/sign-in-stub.component.spec.ts
+++ src/app/pages/sign-in-stub/sign-in-stub.component.spec.ts
@@ -8,10 +8,9 @@ describe('SignInStubComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignInStubComponent]
-    })
-    .compileComponents();
-    
+      imports: [SignInStubComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SignInStubComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,26 @@ describe('SignInStubComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call setupSignInForm on ngOnInit', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    spyOn<any>(component, 'setupSignInForm');
+    component.ngOnInit();
+    expect(component['setupSignInForm']).toHaveBeenCalled();
+  });
+
+  it('should initialize signInForm with email FormControl', () => {
+    component['setupSignInForm']();
+    expect(component.signInForm.get('email')).toBeTruthy();
+  });
+
+  it('should require email field to be filled', () => {
+    component['setupSignInForm']();
+    const emailControl = component.signInForm.get('email');
+    emailControl?.setValue(null);
+    expect(emailControl?.valid).toBeFalsy();
+    emailControl?.setValue('test@example.com');
+    expect(emailControl?.valid).toBeTruthy();
   });
 });
