a2630cabb7a09998223085b1526630b345ecf3f5
Max <Max.Holland@HMCTS.NET>
2024-04-07 08:35:35 +0100

    Update BrowserModule imports and add client hydration with no HTTP transfer cache

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,9 +3,11 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { provideClientHydration, withNoHttpTransferCache } from '@angular/platform-browser';
 
 @NgModule({
   imports: [AppModule, ServerModule],
   bootstrap: [AppComponent],
+  providers: [provideClientHydration(withNoHttpTransferCache())],
 })
 export class AppServerModule {}
