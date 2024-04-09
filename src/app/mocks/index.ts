3e5300b91b654a0846082b9cfa656e1c7928c7c1
Max <Max.Holland@HMCTS.NET>
2024-04-09 11:19:27 +0100

    Refactor transfer state service and add transfer state mock

--- src/app/mocks/index.ts
+++ src/app/mocks/index.ts
@@ -17,6 +17,7 @@ import { LAUNCH_DARKLY_FLAGS_MOCK } from './launch-darkly-flags.mock';
 import { USER_STATE_MOCK } from './user-state.mock';
 import { LAUNCH_DARKLY_CONFIG_MOCK } from './launch-darkly-config.mock';
 import { SIGN_IN_STUB_FORM_MOCK } from './sign-in-stub-form.mock';
+import { TRANSFER_STATE_MOCK } from './transfer-state-mock';
 
 export {
   DEFENDANT_ACCOUNT_MOCK,
@@ -38,4 +39,5 @@ export {
   USER_STATE_MOCK,
   LAUNCH_DARKLY_CONFIG_MOCK,
   SIGN_IN_STUB_FORM_MOCK,
+  TRANSFER_STATE_MOCK,
 };
--- src/app/services/transfer-state-service/transfer-state.service.spec.ts
+++ src/app/services/transfer-state-service/transfer-state.service.spec.ts
@@ -2,26 +2,38 @@ import { TestBed } from '@angular/core/testing';
 
 import { TransferStateService } from './transfer-state.service';
 
-import { LAUNCH_DARKLY_CONFIG_MOCK } from '@mocks';
+import { TRANSFER_STATE_MOCK } from '@mocks';
 import { StateService } from '../state-service/state.service';
+import { PLATFORM_ID, makeStateKey } from '@angular/core';
+import { ITransferServerState } from '@interfaces';
 
 describe('TransferStateService', () => {
   let service: TransferStateService;
   let stateService: StateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: PLATFORM_ID, useValue: 'server' },
+        {
+          provide: 'serverTransferState',
+          useValue: TRANSFER_STATE_MOCK,
+        },
+      ],
+    });
     service = TestBed.inject(TransferStateService);
     stateService = TestBed.inject(StateService);
-    service['storedServerTransferState'] = {
-      launchDarklyConfig: LAUNCH_DARKLY_CONFIG_MOCK,
-
-      ssoEnabled: true,
-    };
+    service['storedServerTransferState'] = TRANSFER_STATE_MOCK;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get the transfer state from the server', () => {
+    const storeKeyTransferState = makeStateKey<ITransferServerState>('serverTransferState');
+    const serverTransferState = service['transferState'].get(storeKeyTransferState, undefined);
+    expect(serverTransferState).toEqual(TRANSFER_STATE_MOCK);
   });
 
   it('should initialize SSO enabled', () => {
