97d4dadc14d2b94d59986d8afb111f03221b7b84
Max <Max.Holland@HMCTS.NET>
2024-04-12 12:06:22 +0100

    Fetch userState from cache if we have it.

--- src/app/services/session-service/session.service.spec.ts
+++ src/app/services/session-service/session.service.spec.ts
@@ -6,6 +6,7 @@ import { SessionEndpoints } from '@enums';
 import { USER_STATE_MOCK } from '@mocks';
 import { StateService } from '@services';
 import { IUserState } from '@interfaces';
+import { of } from 'rxjs';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -30,7 +31,16 @@ describe('SessionService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return user state', () => {
+  it('should return user state when stateService has a value', () => {
+    const mockUserState: IUserState = USER_STATE_MOCK;
+    stateService.userState = mockUserState;
+
+    service.getUserState().subscribe((userState: IUserState) => {
+      expect(userState).toEqual(mockUserState);
+    });
+  });
+
+  it('should return user state when stateService is empty', () => {
     const mockUserState: IUserState = USER_STATE_MOCK;
 
     service.getUserState().subscribe((userState: IUserState) => {
--- src/app/services/session-service/session.service.ts
+++ src/app/services/session-service/session.service.ts
@@ -4,7 +4,7 @@ import { SessionEndpoints } from '@enums';
 import { IUserState } from '@interfaces';
 import { StateService } from '@services';
 
-import { Observable, shareReplay, tap } from 'rxjs';
+import { Observable, of, shareReplay, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,14 +14,17 @@ export class SessionService {
   private readonly stateService = inject(StateService);
 
   /**
-   * Gets the user state.
-   *
-   * @returns An observable of the user state.
+   * Retrieves the user state as an observable.
+   * If the user state is already available, it returns it immediately.
+   * Otherwise, it makes an HTTP GET request to fetch the user state and caches it for future use.
+   * @returns An observable that emits the user state.
    */
   public getUserState(): Observable<IUserState> {
-    return this.http
-      .get<IUserState>(SessionEndpoints.userState)
-      .pipe(shareReplay(1))
-      .pipe(tap((userState) => (this.stateService.userState = userState)));
+    return this.stateService.userState
+      ? of(this.stateService.userState)
+      : this.http
+          .get<IUserState>(SessionEndpoints.userState)
+          .pipe(shareReplay(1))
+          .pipe(tap((userState) => (this.stateService.userState = userState)));
   }
 }
