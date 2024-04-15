35bb8b23121b55cb8b2ffeb1e74d237ab20ace1e
Max <Max.Holland@HMCTS.NET>
2024-04-15 13:10:44 +0100

    Add logging statements to user-state.ts and sso-login-callback.ts

--- server/session/user-state.ts
+++ server/session/user-state.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import { UserState } from '../interfaces';
+import { Logger } from '@hmcts/nodejs-logging';
+
+const logger = Logger.getLogger('session-user-state');
 
 export default (req: Request, res: Response) => {
   const userState: UserState | undefined = req.session.securityToken?.userState;
-
+  logger.info('User state requested', req.session.securityToken);
   // Don't allow caching of this endpoint
   res.header('Cache-Control', 'no-store, must-revalidate');
 
