d26cc36dd0f50539c66664bfeb4bb722a0759ef5
Max <Max.Holland@HMCTS.NET>
2024-04-17 14:12:13 +0100

    Fix import error in dashboard component spec file

--- src/app/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { RouterModule } from '@angular/router';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,10 +9,9 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
-    })
-    .compileComponents();
-    
+      imports: [DashboardComponent, RouterModule.forRoot([])],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
