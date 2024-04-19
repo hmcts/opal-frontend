87a908c6a0b227b9775fcb06ead26e728b97f751
Max <Max.Holland@HMCTS.NET>
2024-04-19 11:55:26 +0100

    Switch back to use target as seems to be working now

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -3,11 +3,9 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default () => {
   return createProxyMiddleware({
+    target: config.get('opal-api.url') + '/api',
     changeOrigin: true,
     logger: console,
-    router: function () {
-      return config.get('opal-api.url') + '/api';
-    },
     on: {
       proxyReq: (proxyReq, req: any) => {
         if (req.session.securityToken) {
