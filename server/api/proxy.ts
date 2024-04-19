75883ff920828f98aaaa1f4a6a6dbc2da75594d4
Max <Max.Holland@HMCTS.NET>
2024-04-19 11:04:50 +0100

    V3 migration changes

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -3,15 +3,19 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default () => {
   return createProxyMiddleware({
-    target: config.get('opal-api.url'),
     changeOrigin: true,
-    logLevel: 'debug',
-    onProxyReq: (proxyReq, req) => {
-      if (req.session.securityToken) {
-        if (req.session.securityToken.accessToken) {
-          proxyReq.setHeader('Authorization', `Bearer ${req.session.securityToken.accessToken}`);
+    logger: console,
+    router: function () {
+      return config.get('opal-api.url') + '/api';
+    },
+    on: {
+      proxyReq: (proxyReq, req: any) => {
+        if (req.session.securityToken) {
+          if (req.session.securityToken.accessToken) {
+            proxyReq.setHeader('Authorization', `Bearer ${req.session.securityToken.accessToken}`);
+          }
         }
-      }
+      },
     },
   });
 };
