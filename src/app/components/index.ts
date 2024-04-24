b8053136e3de4bab276e6d722232e87d2e57eb2f
Frankie Moran <frankie.jack.moran@gmail.com>
2024-04-24 16:08:21 +0100

    New folders for components, components moved, and updated README (#426)
    Co-authored-by: Max Holland <Max.Holland@HMCTS.NET>

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,21 +1,21 @@
-import { GovukAccordionComponent } from './govuk-accordion/govuk-accordion.component';
-import { GovukButtonComponent } from './govuk-button/govuk-button.component';
-import { GovukHeaderComponent } from './govuk-header/govuk-header.component';
-import { GovukFooterComponent } from './govuk-footer/govuk-footer.component';
-import { GovukTextInputComponent } from './govuk-text-input/govuk-text-input.component';
-import { GovukRadiosComponent } from './govuk-radios/govuk-radios.component';
-import { GovukCheckboxesComponent } from './govuk-checkboxes/govuk-checkboxes.component';
-import { GovukDateInputComponent } from './govuk-date-input/govuk-date-input.component';
-import { GovukSelectComponent } from './govuk-select/govuk-select.component';
-import { GovukPaginationComponent } from './govuk-pagination/govuk-pagination.component';
-import { GovukTabsComponent } from './govuk-tabs/govuk-tabs.component';
-import { GovukSummaryListRowComponent } from './govuk-summary-list/govuk-summary-list-row/govuk-summary-list-row.component';
-import { GovukSummaryListComponent } from './govuk-summary-list/govuk-summary-list.component';
-import { GovukTabListItemComponent } from './govuk-tabs/govuk-tab-list-item/govuk-tab-list-item.component';
-import { GovukTabPanelComponent } from './govuk-tabs/govuk-tab-panel/govuk-tab-panel.component';
-import { GovukInsetTextComponent } from './govuk-inset-text/govuk-inset-text.component';
-import { MojSubNavigationComponent } from './moj-sub-navigation/moj-sub-navigation.component';
-import { MojSubNavigationItemComponent } from './moj-sub-navigation/moj-sub-navigation-item/moj-sub-navigation-item.component';
+import { GovukAccordionComponent } from './govuk/govuk-accordion/govuk-accordion.component';
+import { GovukButtonComponent } from './govuk/govuk-button/govuk-button.component';
+import { GovukHeaderComponent } from './govuk/govuk-header/govuk-header.component';
+import { GovukFooterComponent } from './govuk/govuk-footer/govuk-footer.component';
+import { GovukTextInputComponent } from './govuk/govuk-text-input/govuk-text-input.component';
+import { GovukRadiosComponent } from './govuk/govuk-radios/govuk-radios.component';
+import { GovukCheckboxesComponent } from './govuk/govuk-checkboxes/govuk-checkboxes.component';
+import { GovukDateInputComponent } from './govuk/govuk-date-input/govuk-date-input.component';
+import { GovukSelectComponent } from './govuk/govuk-select/govuk-select.component';
+import { GovukPaginationComponent } from './govuk/govuk-pagination/govuk-pagination.component';
+import { GovukTabsComponent } from './govuk/govuk-tabs/govuk-tabs.component';
+import { GovukSummaryListRowComponent } from './govuk/govuk-summary-list/govuk-summary-list-row/govuk-summary-list-row.component';
+import { GovukSummaryListComponent } from './govuk/govuk-summary-list/govuk-summary-list.component';
+import { GovukTabListItemComponent } from './govuk/govuk-tabs/govuk-tab-list-item/govuk-tab-list-item.component';
+import { GovukTabPanelComponent } from './govuk/govuk-tabs/govuk-tab-panel/govuk-tab-panel.component';
+import { GovukInsetTextComponent } from './govuk/govuk-inset-text/govuk-inset-text.component';
+import { MojSubNavigationComponent } from './moj/moj-sub-navigation/moj-sub-navigation.component';
+import { MojSubNavigationItemComponent } from './moj/moj-sub-navigation/moj-sub-navigation-item/moj-sub-navigation-item.component';
 
 export {
   GovukButtonComponent,
