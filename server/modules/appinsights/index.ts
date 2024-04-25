18ff039116fae169a8f775d8d69dc53392975443
Max <Max.Holland@HMCTS.NET>
2024-04-25 16:32:46 +0100

    Update secrets in opal-frontend configuration

--- server/modules/appinsights/index.ts
+++ server/modules/appinsights/index.ts
@@ -1,7 +1,8 @@
 import config from 'config';
 
 process.env['APPLICATIONINSIGHTS_CONFIGURATION_CONTENT'] = '{}';
-const appInsights = require('applicationinsights');
+// const appInsights = require('applicationinsights');
+import * as appInsights from 'applicationinsights';
 
 // As of 2.9.0 issue reading bundled applicationinsights.json
 // https://github.com/microsoft/ApplicationInsights-node.js/issues/1226
@@ -9,13 +10,32 @@ const appInsights = require('applicationinsights');
 
 export class AppInsights {
   enable(): void {
-    if (config.get('secrets.opal.AppInsightsInstrumentationKey')) {
-      appInsights.setup(config.get('secrets.opal.AppInsightsInstrumentationKey')).setSendLiveMetrics(true).start();
+    const appInsightsKey: string | null = config.has('secrets.opal.app-insights-connection-string')
+      ? config.get('secrets.opal.app-insights-connection-string')
+      : null;
 
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'opal-frontend';
-      appInsights.defaultClient.trackTrace({
-        message: 'App insights activated',
-      });
+    if (appInsightsKey) {
+      try {
+        appInsights
+          .setup(appInsightsKey)
+          .setAutoCollectRequests(true)
+          .setAutoCollectPerformance(true, true)
+          .setAutoCollectExceptions(true)
+          .setAutoCollectDependencies(true)
+          .setAutoCollectConsole(true, false)
+          .setAutoCollectPreAggregatedMetrics(true)
+          .setSendLiveMetrics(true)
+          .setInternalLogging(false, true)
+          .enableWebInstrumentation(false)
+          .start();
+
+        appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'opal-frontend';
+        appInsights.defaultClient.trackTrace({
+          message: 'App insights activated',
+        });
+      } catch (error) {
+        console.error('Error activating AppInsights:', error);
+      }
     }
   }
 }
