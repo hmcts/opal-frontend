d010b58058bbc48c04fcb083ed403f6bf4f2ea88
Frankie Moran <frankie.jack.moran@gmail.com>
2024-05-01 17:29:29 +0100

    Date Picker working as expected

--- src/app/components/scotgov/scotgov-date-picker/scotgov-date-picker.component.ts
+++ src/app/components/scotgov/scotgov-date-picker/scotgov-date-picker.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, afterNextRender } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, afterNextRender } from '@angular/core';
 
 @Component({
   selector: 'app-scotgov-date-picker',
@@ -8,18 +8,20 @@ import { ChangeDetectionStrategy, Component, afterNextRender } from '@angular/co
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScotgovDatePickerComponent {
+  @Input({ required: true }) labelText!: string;
+  @Input({ required: false }) hintText: string = '';
 
   constructor() {
     afterNextRender(() => {
       // Only trigger the render of the component in the browser
       this.configureDatePicker();
-    })
+    });
   }
 
   /**
    * Configures the date picker functionality using the scottish government library.
    */
-  private configureDatePicker(): void {
+  configureDatePicker(): void {
     import('@scottish-government/design-system/src/all').then((datePicker) => {
       datePicker.initAll();
     });
