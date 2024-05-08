181604655253cc57e958c209ebac792ca40e3418
Frankie Moran <frankie.jack.moran@gmail.com>
2024-05-08 13:49:06 +0100

    Govuk Error Summary component added

---
+++ src/app/components/govuk/govuk-error-summary/govuk-error-summary.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-govuk-error-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './govuk-error-summary.component.html',
+})
+export class GovukErrorSummaryComponent {
+  @Input() errors: { fieldId: string; message: string }[] = [];
+  @Output() errorClick = new EventEmitter<string>();
+
+  /**
+   * Handles the event when clicking an error in the summary
+   *
+   * @param event - The event object
+   * @param fieldId - The ID of the error field
+   */
+  public handleErrorClick(event: Event, fieldId: string): void {
+    event.preventDefault();
+
+    this.errorClick.emit(fieldId);
+  }
+}
