b8198260d9f5595a04a0221c062ce226a476b135
Max <Max.Holland@HMCTS.NET>
2024-05-16 12:20:39 +0100

    Fix imports in manual-account-creation.component.ts

--- src/app/pages/manual-account-creation/manual-account-creation.component.ts
+++ src/app/pages/manual-account-creation/manual-account-creation.component.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-manual-account-creation',
   standalone: true,
-  imports: [RouterOutlet],
   imports: [CommonModule, RouterOutlet],
   templateUrl: './manual-account-creation.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
