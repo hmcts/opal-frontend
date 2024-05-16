6dc1cc1faa3ec4979ca92bd7d43147f5f2c682c4
Max <Max.Holland@HMCTS.NET>
2024-05-16 12:13:34 +0100

    Update create-account.component.html and create-account.component.ts files

--- src/app/pages/manual-account-creation/create-account/create-account.component.ts
+++ src/app/pages/manual-account-creation/create-account/create-account.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { GovukTagComponent, GovukTaskListComponent, GovukTaskListItemComponent } from '@components';
+import {
+  GovukButtonComponent,
+  GovukTagComponent,
+  GovukTaskListComponent,
+  GovukTaskListItemComponent,
+} from '@components';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [GovukTagComponent, GovukTaskListComponent, GovukTaskListItemComponent],
+  imports: [GovukTagComponent, GovukTaskListComponent, GovukTaskListItemComponent, GovukButtonComponent],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss',
 })
