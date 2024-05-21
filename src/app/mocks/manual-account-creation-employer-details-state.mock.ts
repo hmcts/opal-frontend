af98d09dc70ef00650e62d4e38eeac7ffe5f74be
Frankie Moran <frankie.jack.moran@gmail.com>
2024-05-21 17:23:48 +0100

    Unit tests added for employer details

---
+++ src/app/mocks/manual-account-creation-employer-details-state.mock.ts
@@ -0,0 +1,13 @@
+export const MANUAL_ACCOUNT_CREATION_EMPLOYER_DETAILS_STATE_MOCK = {
+    employerName: 'Test Employer Name',
+    employeeReference: 'Test Reference',
+    employerEmailAddress: 'abc@def.co.uk',
+    employerTelephone: '12345678910',
+    employerAddress1: 'Test Employer Address 1',
+    employerAddress2: 'Test Employer Address 2',
+    employerAddress3: 'Test Employer Address 3',
+    employerAddress4: 'Test Employer Address 4',
+    employerAddress5: 'Test Employer Address 5',
+    employerPostcode: 'TE10 1ST',
+  };
+  
