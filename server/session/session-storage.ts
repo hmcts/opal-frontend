61770d0b0d272f66404c18ec8fec4f0dc2e0651d
Frankie Moran <frankie.jack.moran@gmail.com>
2024-06-07 15:47:48 +0100

    Fix logging statement in SessionStorage class

--- server/session/session-storage.ts
+++ server/session/session-storage.ts
@@ -41,7 +41,7 @@ export default class SessionStorage {
         socket: {
           reconnectStrategy: function (retries) {
             if (retries > 20) {
-              console.log('Too many attempts to reconnect. Redis connection was terminated');
+              logger.log('Too many attempts to reconnect. Redis connection was terminated');
               return new Error('Too many retries.');
             } else {
               return retries * 500;
