f744ac47dc138c521e15d62ebbd79dd9344d8dfd
CadeFaulkner <cade.faulkner@hmcts.net>
2024-06-20 17:48:26 +0100

    changing cypress viewport size

--- cypress.config.ts
+++ cypress.config.ts
@@ -18,6 +18,8 @@ async function setupNodeEvents(
 }
 
 export default defineConfig({
+  viewportWidth: 2560,
+  viewportHeight: 3680,
   reporter: 'junit',
   e2e: {
     baseUrl: process.env['TEST_URL'] || 'http://localhost:4000/',
