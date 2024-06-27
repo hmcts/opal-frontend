6a7ce96f58aa9961e4bd440f071de392557d3253
Max <Max.Holland@HMCTS.NET>
2024-06-27 12:58:51 +0100

    chore: Update HttpClient configuration in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,50 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withNoHttpTransferCache } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideHttpClient, withFetch, withInterceptors, provideHttpClient, withInterceptorsFromDi, withXsrfConfiguration } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+  withInterceptorsFromDi,
+  withXsrfConfiguration,
+} from '@angular/common/http';
 
 import { MojHeaderComponent, MojHeaderNavigationItemComponent, GovukFooterComponent } from '@components';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { httpErrorInterceptor } from '@interceptors';
 import { AppInitializerService } from '@services';
 
-@NgModule({ declarations: [AppComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        MojHeaderComponent,
-        MojHeaderNavigationItemComponent,
-        GovukFooterComponent,
-        BrowserAnimationsModule], providers: [
-        provideClientHydration(withNoHttpTransferCache()),
-        provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor])),
-        {
-            provide: APP_INITIALIZER,
-            multi: true,
-            useFactory: (appInitializerService: AppInitializerService) => {
-                return () => {
-                    return appInitializerService.initializeApp();
-                };
-            },
-            deps: [AppInitializerService],
-        },
-        provideHttpClient(withInterceptorsFromDi(), withXsrfConfiguration()),
-    ] })
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MojHeaderComponent,
+    MojHeaderNavigationItemComponent,
+    GovukFooterComponent,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    provideClientHydration(withNoHttpTransferCache()),
+    provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor])),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (appInitializerService: AppInitializerService) => {
+        return () => {
+          return appInitializerService.initializeApp();
+        };
+      },
+      deps: [AppInitializerService],
+    },
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withXsrfConfiguration({
+        headerName: 'X-XSRF-TOKEN',
+        cookieName: 'XSRF-TOKEN',
+      }),
+    ),
+  ],
+})
 export class AppModule {}
