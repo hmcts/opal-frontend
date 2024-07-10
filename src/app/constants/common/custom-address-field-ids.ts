017a8fdd0f20aa3f398d0fa1423c8884e2e39259
Frankie Moran <frankie.jack.moran@gmail.com>
2024-07-10 12:56:22 +0100

    WIP - prettier:fix

--- src/app/constants/common/custom-address-field-ids.ts
+++ src/app/constants/common/custom-address-field-ids.ts
@@ -1,8 +1,8 @@
-import { ICustomAddressFieldIds } from "@interfaces";
+import { ICustomAddressFieldIds } from '@interfaces';
 
 export const CUSTOM_ADDRESS_FIELD_IDS: ICustomAddressFieldIds = {
-    addressLineOneFieldId: 'addressLine1',
-    addressLineTwoFieldId: 'addressLine2',  
-    addressLineThreeFieldId: 'addressLine3',
-    postcodeFieldId: 'postcode',
+  addressLineOneFieldId: 'addressLine1',
+  addressLineTwoFieldId: 'addressLine2',
+  addressLineThreeFieldId: 'addressLine3',
+  postcodeFieldId: 'postcode',
 };
