bfe1e3edc83f20d168f704db6f6f066ef06bd493
CadeFaulkner <cade.faulkner@hmcts.net>
2024-07-23 14:53:20 +0100

    re-adding tests for PO-389

--- cypress/support/step_definitions/genericSteps/genericSteps.ts
+++ cypress/support/step_definitions/genericSteps/genericSteps.ts
@@ -11,6 +11,13 @@ Then('I click the {string} button and see {string} on the page header', (buttonN
       cy.contains('button', buttonName).click();
       cy.get('h1').should('contain', bodyHeader);
       cy.get('a').contains('Cancel').click();
+      break;
+    }
+    case 'Add personal details': {
+      cy.contains('button', buttonName).click();
+      cy.get('h1').should('contain', bodyHeader);
+      cy.get('a').contains('Cancel').click();
+      break;
     }
   }
 });
@@ -86,4 +93,33 @@ Then('I select {string} from the {string} dropdown', (option: string, dropdown: 
 });
 Then('I see {string} selected in the {string} dropdown', (option: string, dropdown: string) => {
   cy.contains('app-govuk-select', dropdown, { matchCase: false }).find('select').should('have.value', option);
+});
+Then('I see {string} in the {string} searchbox', (fieldValue: string, fieldName: string) => {
+  cy.contains('app-alphagov-accessible-autocomplete', fieldName, { matchCase: false })
+    .find('input')
+    .should('have.value', fieldValue);
+});
+When('I enter {string} into the {string} search box', (searchText: string, searchBox: string) => {
+  cy.contains('app-alphagov-accessible-autocomplete', searchBox)
+    .children('div')
+    .children('div')
+    .find('input')
+    .type(searchText);
+  cy.contains('app-alphagov-accessible-autocomplete', searchBox).should('not.contain', 'No results found');
+  cy.contains('app-alphagov-accessible-autocomplete', searchBox)
+    .children('div')
+    .children('div')
+    .find('input')
+    .type('{downArrow}{enter}');
+});
+
+When('I see {string} under the {string} search box', (text: string, fieldName: string) => {
+  cy.contains('app-alphagov-accessible-autocomplete', fieldName)
+    .find('label')
+    .next()
+    .invoke('text')
+    .should('contains', text);
+});
+When('I see {string} under the {string} field', (text: string, fieldName: string) => {
+  cy.contains('app-govuk-text-input', fieldName).find('input').prev().invoke('text').should('contains', text);
 });
