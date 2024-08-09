26bbf960d1abb68417828ad6cbfbf8d0557d5ff7
Max <Max.Holland@HMCTS.NET>
2024-08-09 13:57:43 +0100

    Remove unrequired enums

--- src/app/enums/routing-paths.ts
+++ src/app/enums/routing-paths.ts
@@ -1,6 +1,4 @@
 export enum RoutingPaths {
-  accountEnquiry = 'account-enquiry',
-  accountEnquirySearch = 'search',
   accessDenied = 'access-denied',
   signIn = 'sign-in',
   signInStub = 'sign-in-stub',
