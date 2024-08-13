20067a8cd8b8eb345eae6c5fcd6fd47092912f60
Max <Max.Holland@HMCTS.NET>
2024-08-13 12:25:04 +0100

    refactor: Remove unused enums and files related to fines-mac routing

--- src/app/flows/fines/fines-mac/enums/fines-mac-functions-permissions.ts
+++
@@ -1,3 +0,0 @@
-export enum FinesMacFunctionsPermission {
-  collectionOrder = 500,
-}
--- src/app/flows/fines/fines-mac/enums/index.ts
+++
@@ -1,4 +0,0 @@
-import { FinesMacFunctionsPermission } from './fines-mac-functions-permissions';
-import { FinesMacRoutes } from './fines-mac-routes';
-
-export { FinesMacRoutes, FinesMacFunctionsPermission };
