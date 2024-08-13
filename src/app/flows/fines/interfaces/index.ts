e5abd3a58536f8ae2a2644378efa7180dcc08fdd
Frankie Moran <frankie.jack.moran@gmail.com>
2024-08-13 10:42:00 +0100

    WIP - fines interfaces move to fines service

--- src/app/flows/fines/interfaces/index.ts
+++ src/app/flows/fines/interfaces/index.ts
@@ -1,16 +1,19 @@
 import { IFinesRoutingPermissions } from '../routing/interfaces';
-import { IFinesAddDefendantAccountNoteBody } from './fines-add-defendant-account-note-body.interface';
-import { IFinesBusinessUnit, IFinesBusinessUnitRefData } from './fines-business-unit-ref-data.interface';
-import { IFinesCourtRefData } from './fines-court-ref-data.interface';
-import { IFinesDefendantAccountDetails } from './fines-defendant-account-details.interface';
-import { IFinesDefendantAccountNote } from './fines-defendant-account-note.interface';
-import { IFinesDefendantAccount } from './fines-defendant-account.interface';
-import { IFinesGetDefendantAccountParams } from './fines-get-defendant-account-params.interface';
-import { IFinesLocalJusticeAreaRefData } from './fines-local-justice-area-ref-data.interface';
-import { IFinesSearchCourtBody } from './fines-search-court-body.interface';
-import { IFinesSearchCourt } from './fines-search-court.interface';
-import { IFinesSearchDefendantAccountBody } from './fines-search-defendant-account-body.interface';
-import { IFinesSearchDefendantAccounts } from './fines-search-defendant-accounts.interface';
+import {
+  IFinesAddDefendantAccountNoteBody,
+  IFinesBusinessUnit,
+  IFinesBusinessUnitRefData,
+  IFinesCourtRefData,
+  IFinesDefendantAccountDetails,
+  IFinesDefendantAccountNote,
+  IFinesDefendantAccount,
+  IFinesGetDefendantAccountParams,
+  IFinesLocalJusticeAreaRefData,
+  IFinesSearchCourtBody,
+  IFinesSearchCourt,
+  IFinesSearchDefendantAccountBody,
+  IFinesSearchDefendantAccounts,
+} from '../services/fines-service/interfaces';
 
 export {
   IFinesAddDefendantAccountNoteBody,
