fbcaac3047c90021b85f5e85c6571dac930544f4
Frankie Moran <frankie.jack.moran@gmail.com>
2024-08-19 11:00:03 +0100

    Remove unused file now field errors and in their own components

--- src/app/flows/fines/fines-mac/fines-mac-parent-guardian-details/constants/index.ts
+++ src/app/flows/fines/fines-mac/fines-mac-parent-guardian-details/constants/index.ts
@@ -6,7 +6,6 @@ import { FINES_MAC_PARENT_GUARDIAN_DETAILS_ADDRESS_POSTCODE_FIELD_ERRORS } from 
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_ALIAS } from './fines-mac-parent-guardian-details-alias';
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_ALIAS_FIELD_ERRORS } from './fines-mac-parent-guardian-details-alias-field-errors';
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_DATE_OF_BIRTH_FIELD_ERRORS } from './fines-mac-parent-guardian-details-date-of-birth-field-errors';
-import { FINES_MAC_PARENT_GUARDIAN_DETAILS_FIELD_ERRORS } from './fines-mac-parent-guardian-details-field-errors';
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_NAME_FIELD_IDS } from './fines-mac-parent-guardian-details-name-field-ids';
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_NATIONAL_INSURANCE_NUMBER_FIELD_ERRORS } from './fines-mac-parent-guardian-details-national-insurance-number-field-errors';
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_STATE } from './fines-mac-parent-guardian-details-state';
@@ -15,7 +14,6 @@ import { FINES_MAC_PARENT_GUARDIAN_DETAILS_NAME_FIELD_ERRORS } from './fines-mac
 import { FINES_MAC_PARENT_GUARDIAN_DETAILS_VEHICLE_DETAILS_FIELD_ERRORS } from './fines-mac-parent-guardian-details-vehicle-details-field-errors';
 
 export {
-  FINES_MAC_PARENT_GUARDIAN_DETAILS_FIELD_ERRORS,
   FINES_MAC_PARENT_GUARDIAN_DETAILS_STATE,
   FINES_MAC_PARENT_GUARDIAN_DETAILS_ALIAS,
   FINES_MAC_PARENT_GUARDIAN_DETAILS_ALIAS_FIELD_ERRORS,
