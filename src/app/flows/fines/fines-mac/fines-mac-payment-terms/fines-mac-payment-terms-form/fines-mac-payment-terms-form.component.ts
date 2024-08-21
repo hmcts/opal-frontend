5b4b7f98637121e8e6e2dcabd4995917b1d7c7a8
Frankie Moran <frankie.jack.moran@gmail.com>
2024-08-21 15:14:03 +0100

    PO-530 (#740)
    Co-authored-by: Max <Max.Holland@HMCTS.NET>
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
    Co-authored-by: CadeFaulkner <cade.faulkner@hmcts.net>
    Co-authored-by: rajanigandra <Rajani Gandra>
    Co-authored-by: rajanigandra <167096333+rajanigandra@users.noreply.github.com>
    Co-authored-by: Alfred-Gillingham2 <Alfred.Gillingham@HMCTS.NET>
    Co-authored-by: Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>

---
+++ src/app/flows/fines/fines-mac/fines-mac-payment-terms/fines-mac-payment-terms-form/fines-mac-payment-terms-form.component.ts
@@ -0,0 +1,70 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output, inject } from '@angular/core';
+import { AbstractFormBaseComponent } from '@components/abstract';
+import { IFinesMacPaymentTermsForm } from '../interfaces';
+import { FinesService } from '../../../services/fines-service/fines.service';
+import { FINES_MAC_ROUTING_PATHS } from '../../routing/constants';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IGovUkRadioOptions } from '@interfaces/components/govuk';
+import { CommonModule } from '@angular/common';
+import {
+  GovukButtonComponent,
+  GovukCancelLinkComponent,
+  GovukCheckboxesComponent,
+  GovukCheckboxesItemComponent,
+  GovukRadioComponent,
+  GovukRadiosItemComponent,
+} from '@components/govuk';
+import { FINES_MAC_PAYMENT_TERMS_OPTIONS } from '../constants';
+
+@Component({
+  selector: 'app-fines-mac-payment-terms-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GovukButtonComponent,
+    GovukRadioComponent,
+    GovukRadiosItemComponent,
+    GovukCheckboxesComponent,
+    GovukCheckboxesItemComponent,
+    GovukCancelLinkComponent,
+  ],
+  templateUrl: './fines-mac-payment-terms-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FinesMacPaymentTermsFormComponent extends AbstractFormBaseComponent implements OnInit, OnDestroy {
+  @Output() protected override formSubmit = new EventEmitter<IFinesMacPaymentTermsForm>();
+
+  protected readonly finesService = inject(FinesService);
+  protected readonly fineMacRoutingPaths = FINES_MAC_ROUTING_PATHS;
+
+  public readonly paymentTerms: IGovUkRadioOptions[] = Object.entries(FINES_MAC_PAYMENT_TERMS_OPTIONS).map(
+    ([key, value]) => ({ key, value }),
+  );
+
+  /**
+   * Sets up the payment terms form.
+   */
+  private setupPaymentTermsForm(): void {
+    this.form = new FormGroup({
+      paymentTerms: new FormControl(null),
+      holdEnforcementOnAccount: new FormControl(null),
+    });
+  }
+
+  /**
+   * Performs the initial setup for the fines-mac-payment-terms-form component.
+   * This method sets up the payment terms form and repopulates it with the
+   * payment terms from the finesMacState.
+   */
+  private initialPaymentTermsSetup(): void {
+    this.setupPaymentTermsForm();
+    this.rePopulateForm(this.finesService.finesMacState.paymentTerms);
+  }
+
+  public override ngOnInit(): void {
+    this.initialPaymentTermsSetup();
+    super.ngOnInit();
+  }
+}
