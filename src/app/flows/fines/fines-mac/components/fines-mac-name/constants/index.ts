d22970970e4827fb589916dcc655d786baf206cb
Frankie Moran <frankie.jack.moran@gmail.com>
2024-08-21 15:23:18 +0100

    Fix the error messages between adult and youth only and adult and youth with parent or guardian to pay specifically the personal details error messages for the Forenames and Surname fields

--- src/app/flows/fines/fines-mac/components/fines-mac-name/constants/index.ts
+++ src/app/flows/fines/fines-mac/components/fines-mac-name/constants/index.ts
@@ -1,4 +1,3 @@
-import { FINES_MAC_NAME_FIELD_ERRORS } from './fines-mac-name-field-errors';
 import { FINES_MAC_NAME_FIELD_IDS } from './fines-mac-name-field-ids';
 
-export { FINES_MAC_NAME_FIELD_IDS, FINES_MAC_NAME_FIELD_ERRORS };
+export { FINES_MAC_NAME_FIELD_IDS };
--- src/app/flows/fines/fines-mac/fines-mac-personal-details/constants/fines-mac-personal-details-name-field-errors.ts
+++ src/app/flows/fines/fines-mac/fines-mac-personal-details/constants/fines-mac-personal-details-name-field-errors.ts
@@ -1,6 +1,32 @@
-import { FINES_MAC_NAME_FIELD_ERRORS } from '../../components/fines-mac-name/constants';
 import { IFinesMacPersonalDetailsNameFieldErrors } from '../interfaces';
 
 export const FINES_MAC_PERSONAL_DETAILS_NAME_FIELD_ERRORS: IFinesMacPersonalDetailsNameFieldErrors = {
-  ...FINES_MAC_NAME_FIELD_ERRORS,
+  Forenames: {
+    required: {
+      message: `Enter defendant's first name(s)`,
+      priority: 1,
+    },
+    maxlength: {
+      message: `The defendant's first name(s) must be 20 characters or fewer`,
+      priority: 2,
+    },
+    alphabeticalTextPattern: {
+      message: `The defendant's first name(s) must only contain alphabetical text`,
+      priority: 2,
+    },
+  },
+  Surname: {
+    required: {
+      message: `Enter defendant's last name`,
+      priority: 1,
+    },
+    maxlength: {
+      message: `The defendant's last name must be 30 characters or fewer`,
+      priority: 2,
+    },
+    alphabeticalTextPattern: {
+      message: `The defendant's last name must only contain alphabetical text`,
+      priority: 2,
+    },
+  },
 };
