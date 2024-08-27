7aff52654ed90fa94a296fac14c4d3d999ffa469
Frankie Moran <frankie.jack.moran@gmail.com>
2024-08-27 09:50:41 +0100

    added component to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MojButtonMenuComponent } from '@components/moj';
 import { FINES_ROUTING_PATHS } from '@constants/fines';
 import { GlobalStateService } from '@services';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MojButtonMenuComponent],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
