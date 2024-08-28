449eabc86d1ee1d8010f12fbe57a3cf2ab5b1471
Frankie Moran <frankie.jack.moran@gmail.com>
2024-08-28 11:22:00 +0100

    Remove from dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MojPrimaryNavigationComponent, MojPrimaryNavigationItemComponent } from '@components/moj';
 import { FINES_ROUTING_PATHS } from '@constants/fines';
 import { GlobalStateService } from '@services';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule, MojPrimaryNavigationComponent, MojPrimaryNavigationItemComponent],
+  imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
