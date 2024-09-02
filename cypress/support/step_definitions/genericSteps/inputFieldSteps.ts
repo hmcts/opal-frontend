7f78ba5096799d80d421c4e519c37cb076be1f33
Alfred-Gillingham2 <Alfred.Gillingham@HMCTS.NET>
2024-09-02 09:43:14 +0100

    Featue file for PO-469, step defs created

--- cypress/support/step_definitions/genericSteps/inputFieldSteps.ts
+++ cypress/support/step_definitions/genericSteps/inputFieldSteps.ts
@@ -7,10 +7,37 @@ Then('I enter {string} into the {string} field', (value: string, fieldName: stri
   cy.contains('app-govuk-text-input', fieldName, { matchCase: false }).find('input').clear().type(value);
 });
 
+Then('I enter {string} into the {string} text field', (value: string, fieldName: string) => {
+  cy.contains('app-govuk-text-area', fieldName, { matchCase: false }).find('textarea').clear().type(value);
+});
+
 Then('I see {string} in the {string} field', (value: string, fieldName: string) => {
   cy.contains('app-govuk-text-input', fieldName, { matchCase: false }).find('input').should('have.value', value);
+});
+
+Then('I see {string} in the {string} text field', (value: string, fieldName: string) => {
+  cy.contains('app-govuk-text-area', fieldName, { matchCase: false }).find('textarea').should('have.value', value);
 });
 
 When('I see {string} under the {string} field', (text: string, fieldName: string) => {
   cy.contains('app-govuk-text-input', fieldName).find('input').prev().invoke('text').should('contains', text);
 });
+
+function generateString(length: number): string {
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    result += charset[randomIndex];
+  }
+  return result;
+}
+
+Then(
+  'the characters remaining counter should show {int} after entering {int} characters into the {string} input field',
+  (expectedRemaining: number, numCharsEntered: number, inputField: string) => {
+    const textToEnter = generateString(numCharsEntered);
+    cy.contains('app-govuk-text-area', inputField).find('textarea').clear().type(textToEnter);
+    cy.get('.govuk-hint').should('contain.text', expectedRemaining.toString());
+  },
+);
