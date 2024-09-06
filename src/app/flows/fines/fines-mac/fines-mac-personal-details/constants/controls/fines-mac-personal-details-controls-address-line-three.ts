cf248418142cddb5bef683e63d713aadc34556b4
Max <Max.Holland@HMCTS.NET>
2024-09-06 14:35:02 +0100

    chore: Update address line control name in fines-mac module

--- src/app/flows/fines/fines-mac/fines-mac-personal-details/constants/controls/fines-mac-personal-details-controls-address-line-three.ts
+++ src/app/flows/fines/fines-mac/fines-mac-personal-details/constants/controls/fines-mac-personal-details-controls-address-line-three.ts
@@ -2,5 +2,5 @@ import { FormControl } from '@angular/forms';
 import { optionalMaxLengthValidator, specialCharactersValidator } from '@validators';
 
 export const FINES_MAC_PERSONAL_DETAILS_CONTROLS_ADDRESS_LINE_THREE = {
-  address_line_three: new FormControl(null, [optionalMaxLengthValidator(16), specialCharactersValidator()]),
+  address_line_3: new FormControl(null, [optionalMaxLengthValidator(16), specialCharactersValidator()]),
 };
