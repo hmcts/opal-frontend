9c2cf8a2702ada1dd9ac56c2c246eb4605db39ed
Frankie Moran <frankie.jack.moran@gmail.com>
2024-09-17 17:15:08 +0100

    Merge conflict fixes

--- src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/controls/fines-mac-payment-terms-controls-hold-enforcement-on-account.constant.ts
+++ src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/controls/fines-mac-payment-terms-controls-hold-enforcement-on-account.constant.ts
@@ -1,4 +1,4 @@
-import { IAbstractFormArrayControlValidation } from '@interfaces/components/abstract';
+import { IAbstractFormArrayControlValidation } from '@components/abstract/interfaces/abstract-form-array-control-validation.interface';
 
 export const FINES_MAC_PAYMENT_TERMS_CONTROLS_HOLD_ENFORCEMENT_ON_ACCOUNT: IAbstractFormArrayControlValidation = {
   controlName: 'hold_enforcement_on_account',
--- src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/controls/fines-mac-payment-terms-controls-hold-enforcement-reason.constant.ts
+++ src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/controls/fines-mac-payment-terms-controls-hold-enforcement-reason.constant.ts
@@ -1,6 +1,6 @@
 import { Validators } from '@angular/forms';
-import { IAbstractFormArrayControlValidation } from '@interfaces/components/abstract';
-import { alphabeticalTextValidator } from '@validators';
+import { IAbstractFormArrayControlValidation } from '@components/abstract/interfaces/abstract-form-array-control-validation.interface';
+import { alphabeticalTextValidator } from '@validators/alphabetical-text/alphabetical-text.validator';
 
 export const FINES_MAC_PAYMENT_TERMS_CONTROLS_REASON_ACCOUNT_IS_ON_NOENF: IAbstractFormArrayControlValidation = {
   controlName: 'reason_account_is_on_noenf',
