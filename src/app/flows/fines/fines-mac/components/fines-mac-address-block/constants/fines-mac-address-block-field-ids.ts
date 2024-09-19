ed52d1e9cf18734546ec816b0b1eb7f5e1d02132
Max <Max.Holland@HMCTS.NET>
2024-09-19 11:55:24 +0100

    refactor: update input IDs and names in fines-mac-address-block-field-ids.ts

--- src/app/flows/fines/fines-mac/components/fines-mac-address-block/constants/fines-mac-address-block-field-ids.ts
+++ src/app/flows/fines/fines-mac/components/fines-mac-address-block/constants/fines-mac-address-block-field-ids.ts
@@ -1,5 +1,7 @@
 import { IFinesMacAddressFieldIds } from '../interfaces';
 
+import { FINES_MAC_CONTROLS_ADDRESS_LINE_ONE } from '../../../constants/controls/fines-mac-controls-address-line-one';
+
 export const FINES_MAC_ADDRESS_BLOCK_FIELD_IDS: IFinesMacAddressFieldIds = {
   addressLineOneFieldId: 'address_line_1',
   addressLineTwoFieldId: 'address_line_2',
