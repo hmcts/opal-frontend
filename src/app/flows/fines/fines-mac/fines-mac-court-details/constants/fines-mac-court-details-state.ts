2e63447ed5ffa18f0432093442b74e491888cfe3
Max <Max.Holland@HMCTS.NET>
2024-09-25 17:09:38 +0100

    Court Details

--- src/app/flows/fines/fines-mac/fines-mac-court-details/constants/fines-mac-court-details-state.ts
+++ src/app/flows/fines/fines-mac/fines-mac-court-details/constants/fines-mac-court-details-state.ts
@@ -1,7 +1,11 @@
 import { IFinesMacCourtDetailsState } from '../interfaces/fines-mac-court-details-state.interface';
+import { FINES_MAC_COURT_DETAILS_CONTROLS_SENDING_COURT as F_M_COURT_DETAILS_SENDING_COURT } from '../constants/controls/fines-mac-court-details-controls-sending-court';
+import { FINES_MAC_COURT_DETAILS_CONTROLS_PROSECUTOR_COURT_REFERENCE as F_M_COURT_DETAILS_PROSECUTOR_COURT_REFERENCE } from '../constants/controls/fines-mac-court-details-controls-prosecutor-case-reference';
+import { FINES_MAC_COURT_DETAILS_CONTROLS_ENFORCING_COURT as F_M_COURT_DETAILS_ENFORCING_COURT } from '../constants/controls/fines-mac-court-details-controls-enforcing-court';
+import { IFinesMacFormState } from '../../interfaces/fines-mac-form-state';
 
-export const FINES_MAC_COURT_DETAILS_STATE: IFinesMacCourtDetailsState = {
-  sending_court: null,
-  prosecutor_case_reference: null,
-  enforcing_court: null,
+export const FINES_MAC_COURT_DETAILS_STATE: IFinesMacFormState = {
+  [F_M_COURT_DETAILS_SENDING_COURT.controlName]: null,
+  [F_M_COURT_DETAILS_PROSECUTOR_COURT_REFERENCE.controlName]: null,
+  [F_M_COURT_DETAILS_ENFORCING_COURT.controlName]: null,
 };
--- src/app/flows/fines/fines-mac/fines-mac-court-details/interfaces/fines-mac-court-details-form.interface.ts
+++ src/app/flows/fines/fines-mac/fines-mac-court-details/interfaces/fines-mac-court-details-form.interface.ts
@@ -1,9 +1,10 @@
 import { IAbstractFormBaseForm } from '@components/abstract/abstract-form-base/interfaces/abstract-form-base-form.interface';
 import { IFinesMacCourtDetailsState } from '../interfaces/fines-mac-court-details-state.interface';
 import { FinesMacStatus } from '../../types/fines-mac-status.type';
+import { IFinesMacFormState } from '../../interfaces/fines-mac-form-state';
 
-export interface IFinesMacCourtDetailsForm extends IAbstractFormBaseForm<IFinesMacCourtDetailsState> {
-  formData: IFinesMacCourtDetailsState;
+export interface IFinesMacCourtDetailsForm extends IAbstractFormBaseForm<IFinesMacFormState> {
+  formData: IFinesMacFormState;
   nestedFlow: boolean;
   status?: FinesMacStatus | null;
 }
