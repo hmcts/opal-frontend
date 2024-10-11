2635a27ffb198846f75a9b3516b598431debbfdc
Frankie Moran <frankie.jack.moran@gmail.com>
2024-10-11 14:39:03 +0100

    Add extra unit test on form array base and remove no longer needed constants without the constant.ts

--- src/app/flows/fines/fines-mac/fines-mac-offence-details/constants/fines-mac-offence-details-draft-state.ts
+++
@@ -1,6 +0,0 @@
-import { IFinesMacOffenceDetailsDraftState } from '../interfaces/fines-mac-offence-details-draft-state.interface';
-
-export const FINES_MAC_OFFENCE_DETAILS_DRAFT_STATE: IFinesMacOffenceDetailsDraftState = {
-  offenceDetailsDraft: [],
-  removeImposition: null,
-};
