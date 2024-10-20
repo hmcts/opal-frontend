b408af245a2f438548fc062b7a7e2e2ec5739fad
Frankie Moran <frankie.jack.moran@gmail.com>
2024-10-20 14:01:23 +0100

    Fix duplicate on remove impositions mock

--- src/app/flows/fines/fines-mac/fines-mac-offence-details/mocks/fines-mac-offence-details-remove-imposition.mock.ts
+++ src/app/flows/fines/fines-mac/fines-mac-offence-details/mocks/fines-mac-offence-details-remove-imposition.mock.ts
@@ -1,6 +1,39 @@
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { IAbstractFormArrayControls } from '@components/abstract/interfaces/abstract-form-array-controls.interface';
 
+const generateFormArrayControl = (index: number): IAbstractFormArrayControls => ({
+  fm_offence_details_result_code: {
+    inputId: `fm_offence_details_result_code_${index}`,
+    inputName: `fm_offence_details_result_code_${index}`,
+    controlName: `fm_offence_details_result_code_${index}`,
+  },
+  fm_offence_details_amount_imposed: {
+    inputId: `fm_offence_details_amount_imposed_${index}`,
+    inputName: `fm_offence_details_amount_imposed_${index}`,
+    controlName: `fm_offence_details_amount_imposed_${index}`,
+  },
+  fm_offence_details_amount_paid: {
+    inputId: `fm_offence_details_amount_paid_${index}`,
+    inputName: `fm_offence_details_amount_paid_${index}`,
+    controlName: `fm_offence_details_amount_paid_${index}`,
+  },
+  fm_offence_details_needs_creditor: {
+    inputId: `fm_offence_details_needs_creditor_${index}`,
+    inputName: `fm_offence_details_needs_creditor_${index}`,
+    controlName: `fm_offence_details_needs_creditor_${index}`,
+  },
+  fm_offence_details_creditor: {
+    inputId: `fm_offence_details_creditor_${index}`,
+    inputName: `fm_offence_details_creditor_${index}`,
+    controlName: `fm_offence_details_creditor_${index}`,
+  },
+  fm_offence_details_major_creditor: {
+    inputId: `fm_offence_details_major_creditor_${index}`,
+    inputName: `fm_offence_details_major_creditor_${index}`,
+    controlName: `fm_offence_details_major_creditor_${index}`,
+  },
+});
+
 export const FINES_MAC_OFFENCE_DETAILS_REMOVE_IMPOSITION_MOCK: {
   rowIndex: number;
   formArray: FormArray;
@@ -27,70 +60,5 @@ export const FINES_MAC_OFFENCE_DETAILS_REMOVE_IMPOSITION_MOCK: {
       fm_offence_details_major_creditor_1: new FormControl(null),
     }),
   ]),
-  formArrayControls: [
-    {
-      fm_offence_details_result_code: {
-        inputId: 'fm_offence_details_result_code_0',
-        inputName: 'fm_offence_details_result_code_0',
-        controlName: 'fm_offence_details_result_code_0',
-      },
-      fm_offence_details_amount_imposed: {
-        inputId: 'fm_offence_details_amount_imposed_0',
-        inputName: 'fm_offence_details_amount_imposed_0',
-        controlName: 'fm_offence_details_amount_imposed_0',
-      },
-      fm_offence_details_amount_paid: {
-        inputId: 'fm_offence_details_amount_paid_0',
-        inputName: 'fm_offence_details_amount_paid_0',
-        controlName: 'fm_offence_details_amount_paid_0',
-      },
-      fm_offence_details_needs_creditor: {
-        inputId: 'fm_offence_details_needs_creditor_0',
-        inputName: 'fm_offence_details_needs_creditor_0',
-        controlName: 'fm_offence_details_needs_creditor_0',
-      },
-      fm_offence_details_creditor: {
-        inputId: 'fm_offence_details_creditor_0',
-        inputName: 'fm_offence_details_creditor_0',
-        controlName: 'fm_offence_details_creditor_0',
-      },
-      fm_offence_details_major_creditor: {
-        inputId: 'fm_offence_details_major_creditor_0',
-        inputName: 'fm_offence_details_major_creditor_0',
-        controlName: 'fm_offence_details_major_creditor_0',
-      },
-    },
-    {
-      fm_offence_details_result_code: {
-        inputId: 'fm_offence_details_result_code_1',
-        inputName: 'fm_offence_details_result_code_1',
-        controlName: 'fm_offence_details_result_code_1',
-      },
-      fm_offence_details_amount_imposed: {
-        inputId: 'fm_offence_details_amount_imposed_1',
-        inputName: 'fm_offence_details_amount_imposed_1',
-        controlName: 'fm_offence_details_amount_imposed_1',
-      },
-      fm_offence_details_amount_paid: {
-        inputId: 'fm_offence_details_amount_paid_1',
-        inputName: 'fm_offence_details_amount_paid_1',
-        controlName: 'fm_offence_details_amount_paid_1',
-      },
-      fm_offence_details_needs_creditor: {
-        inputId: 'fm_offence_details_needs_creditor_1',
-        inputName: 'fm_offence_details_needs_creditor_1',
-        controlName: 'fm_offence_details_needs_creditor_1',
-      },
-      fm_offence_details_creditor: {
-        inputId: 'fm_offence_details_creditor_1',
-        inputName: 'fm_offence_details_creditor_1',
-        controlName: 'fm_offence_details_creditor_1',
-      },
-      fm_offence_details_major_creditor: {
-        inputId: 'fm_offence_details_major_creditor_1',
-        inputName: 'fm_offence_details_major_creditor_1',
-        controlName: 'fm_offence_details_major_creditor_1',
-      },
-    },
-  ],
+  formArrayControls: [generateFormArrayControl(0), generateFormArrayControl(1)],
 };
