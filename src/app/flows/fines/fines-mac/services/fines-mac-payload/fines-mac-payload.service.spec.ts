ff6a7630fcd382f5679569cebf46cd9aa6cefd6c
Max <Max.Holland@HMCTS.NET>
2024-10-23 07:17:17 +0100

    Payload service unit test

--- src/app/flows/fines/fines-mac/services/fines-mac-payload/fines-mac-payload.service.spec.ts
+++ src/app/flows/fines/fines-mac/services/fines-mac-payload/fines-mac-payload.service.spec.ts
@@ -31,59 +31,77 @@ describe('FinesMacPayloadService', () => {
   });
 
   it('should create an individual payload', () => {
-    const accountDetailsState: IFinesMacAccountDetailsState = {
-      ...FINES_MAC_ACCOUNT_DETAILS_STATE_MOCK,
-      fm_create_account_defendant_type: 'individual',
-    };
-
-    const personalDetailsState: IFinesMacPersonalDetailsState = {
-      ...FINES_MAC_PERSONAL_DETAILS_STATE_MOCK,
-      fm_personal_details_add_alias: false,
-      fm_personal_details_aliases: [],
-    };
-    const contactDetailsState: IFinesMacContactDetailsState = {
-      ...FINES_MAC_CONTACT_DETAILS_STATE_MOCK,
-    };
-    const employerDetailsState: IFinesMacEmployerDetailsState = {
-      ...FINES_MAC_EMPLOYER_DETAILS_STATE_MOCK,
-    };
-    const languagePreferencesState: IFinesMacLanguagePreferencesState = {
-      ...FINES_MAC_LANGUAGE_PREFERENCES_STATE_MOCK,
-    };
-    const companyDetailsState: IFinesMacCompanyDetailsState = { ...FINES_MAC_COMPANY_DETAILS_STATE_MOCK };
-
-    const parentGuardianDetailsState: IFinesMacParentGuardianDetailsState = {
-      ...FINES_MAC_PARENT_GUARDIAN_DETAILS_STATE_MOCK,
-    };
-
     const finesMacState: IFinesMacState = {
       ...FINES_MAC_STATE_MOCK,
-    };
-
-    finesMacState.accountDetails.formData = accountDetailsState;
-    finesMacState.personalDetails.formData = personalDetailsState;
-    finesMacState.contactDetails.formData = contactDetailsState;
-    finesMacState.employerDetails.formData = employerDetailsState;
-    finesMacState.languagePreferences.formData = languagePreferencesState;
-    finesMacState.companyDetails.formData = companyDetailsState;
-    finesMacState.parentGuardianDetails.formData = parentGuardianDetailsState;
-    finesMacState.accountCommentsNotes.formData = {
-      fm_account_comments_notes_comments: 'Test comment',
-      fm_account_comments_notes_notes: 'Test note',
-    };
-    finesMacState.paymentTerms.formData = {
-      fm_payment_terms_payment_terms: 'payInFull',
-      fm_payment_terms_payment_card_request: true,
-      fm_payment_terms_collection_order_made: true,
-      fm_payment_terms_has_days_in_default: true,
-      fm_payment_terms_add_enforcement_action: true,
-      fm_payment_terms_collection_order_date: '2024-10-21',
-      fm_payment_terms_pay_by_date: '2024-10-14',
-      fm_payment_terms_suspended_committal_date: '2024-10-11',
-      fm_payment_terms_default_days_in_jail: 11,
-      fm_payment_terms_enforcement_action: 'defendantIsInCustody',
-      fm_payment_terms_earliest_release_date: '2024-10-21',
-      fm_payment_terms_prison_and_prison_number: 'Test and test',
+      accountDetails: {
+        ...FINES_MAC_STATE_MOCK.accountDetails,
+        formData: {
+          ...FINES_MAC_ACCOUNT_DETAILS_STATE_MOCK,
+          fm_create_account_defendant_type: 'individual',
+        },
+      },
+      personalDetails: {
+        ...FINES_MAC_STATE_MOCK.personalDetails,
+        formData: {
+          ...FINES_MAC_PERSONAL_DETAILS_STATE_MOCK,
+          fm_personal_details_add_alias: false,
+          fm_personal_details_aliases: [],
+        },
+      },
+      contactDetails: {
+        ...FINES_MAC_STATE_MOCK.contactDetails,
+        formData: {
+          ...FINES_MAC_CONTACT_DETAILS_STATE_MOCK,
+        },
+      },
+      employerDetails: {
+        ...FINES_MAC_STATE_MOCK.employerDetails,
+        formData: {
+          ...FINES_MAC_EMPLOYER_DETAILS_STATE_MOCK,
+        },
+      },
+      languagePreferences: {
+        ...FINES_MAC_STATE_MOCK.languagePreferences,
+        formData: {
+          ...FINES_MAC_LANGUAGE_PREFERENCES_STATE_MOCK,
+        },
+      },
+      companyDetails: {
+        ...FINES_MAC_STATE_MOCK.companyDetails,
+        formData: {
+          ...FINES_MAC_COMPANY_DETAILS_STATE_MOCK,
+        },
+      },
+      parentGuardianDetails: {
+        ...FINES_MAC_STATE_MOCK.parentGuardianDetails,
+        formData: {
+          ...FINES_MAC_PARENT_GUARDIAN_DETAILS_STATE_MOCK,
+        },
+      },
+      accountCommentsNotes: {
+        ...FINES_MAC_STATE_MOCK.accountCommentsNotes,
+        formData: {
+          fm_account_comments_notes_comments: 'Test comment',
+          fm_account_comments_notes_notes: 'Test note',
+        },
+      },
+      paymentTerms: {
+        ...FINES_MAC_STATE_MOCK.accountCommentsNotes,
+        formData: {
+          fm_payment_terms_payment_terms: 'payInFull',
+          fm_payment_terms_payment_card_request: true,
+          fm_payment_terms_collection_order_made: true,
+          fm_payment_terms_has_days_in_default: true,
+          fm_payment_terms_add_enforcement_action: true,
+          fm_payment_terms_collection_order_date: '2024-10-21',
+          fm_payment_terms_pay_by_date: '2024-10-14',
+          fm_payment_terms_suspended_committal_date: '2024-10-11',
+          fm_payment_terms_default_days_in_jail: 11,
+          fm_payment_terms_enforcement_action: 'defendantIsInCustody',
+          fm_payment_terms_earliest_release_date: '2024-10-21',
+          fm_payment_terms_prison_and_prison_number: 'Test and test',
+        },
+      },
     };
 
     const result = service.buildPayload(finesMacState);
