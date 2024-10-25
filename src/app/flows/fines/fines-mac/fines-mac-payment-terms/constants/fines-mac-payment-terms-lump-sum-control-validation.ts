f63ed57e28f2c9e03a9b4ef3f2e255c2428d59d8
Max Holland <Max.Holland@HMCTS.NET>
2024-10-25 13:18:29 +0100

    Update fines-mac-payment-terms-lump-sum-control-validation.ts

--- src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/fines-mac-payment-terms-lump-sum-control-validation.ts
+++ src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/fines-mac-payment-terms-lump-sum-control-validation.ts
@@ -1,4 +1,4 @@
-import { FINES_MAC_PAYMENT_TERMS_INSTALMENTS_ONLY_CONTROL_VALIDATION as fm_payment_terms_instalment_amount_CONTROLS } from './fines-mac-payment-terms-instalments-only-control-validation';
+import { FINES_MAC_PAYMENT_TERMS_INSTALMENTS_ONLY_CONTROL_VALIDATION as FM_PAYMENT_TERMS_INSTALMENT_AMOUNT_CONTROLS } from './fines-mac-payment-terms-instalments-only-control-validation';
 import { Validators } from '@angular/forms';
 import { IAbstractFormArrayControlValidation } from '@components/abstract/interfaces/abstract-form-array-control-validation.interface';
 import { twoDecimalPlacesValidator } from '@validators/two-decimal-places/two-decimal-places.validator';
@@ -8,5 +8,5 @@ export const FINES_MAC_PAYMENT_TERMS_LUMP_SUM_CONTROL_VALIDATION: IAbstractFormA
     controlName: 'fm_payment_terms_lump_sum_amount',
     validators: [Validators.required, twoDecimalPlacesValidator()],
   },
-  ...fm_payment_terms_instalment_amount_CONTROLS,
+  ...FM_PAYMENT_TERMS_INSTALMENT_AMOUNT_CONTROLS,
 ];
