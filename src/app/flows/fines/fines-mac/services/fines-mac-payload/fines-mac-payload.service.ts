c54a4cafd60a7ac988ddf22501bb39806d3de311
Max <Max.Holland@HMCTS.NET>
2024-10-30 12:05:19 +0000

    WIP

--- src/app/flows/fines/fines-mac/services/fines-mac-payload/fines-mac-payload.service.ts
+++ src/app/flows/fines/fines-mac/services/fines-mac-payload/fines-mac-payload.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { IFinesMacState } from '../../interfaces/fines-mac-state.interface';
 
 import { IFinesMacAccountDetailsState } from '../../fines-mac-account-details/interfaces/fines-mac-account-details-state.interface';
@@ -12,11 +12,33 @@ import { buildDefendantPayload } from './utils/fines-mac-payload-defendant.utils
 import { buildPaymentTermsPayload } from './utils/fines-mac-payload-payment-terms.utils';
 import { buildAccountNotesPayload } from './utils/fines-mac-payload-account-notes.utils';
 import { IFinesMacPayload } from './interfaces/fines-mac-payload.interface';
+import { DateService } from '@services/date-service/date.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FinesMacPayloadService {
+  private readonly dateService = inject(DateService);
+
+  private formatDateValues(obj: any, keysToReplace: string[]): any {
+    if (typeof obj !== 'object' || obj === null) {
+      return obj;
+    }
+
+    for (let key in obj) {
+      if (keysToReplace.includes(key)) {
+        if (obj[key]) {
+          const parsedDate = this.dateService.getFromFormat(obj[key], 'dd/MM/yyyy');
+          obj[key] = this.dateService.toFormat(parsedDate, 'yyyy-MM-dd');
+        }
+      } else if (typeof obj[key] === 'object') {
+        obj[key] = this.formatDateValues(obj[key], keysToReplace); // Recursive call
+      }
+    }
+
+    return obj;
+  }
+
   /**
    * Builds the initial payload for fines MAC based on the provided state objects.
    *
@@ -82,6 +104,19 @@ export class FinesMacPayloadService {
     const { formData: parentGuardianDetailsState } = finesMacState.parentGuardianDetails;
     const { formData: accountCommentsNotesState } = finesMacState.accountCommentsNotes;
 
+    // Before we build the payload, transform the dates to the required format...
+    // const parentGuardianDateKeys = ['fm_parent_guardian_details_dob'];
+    // const personalDetailsDateKeys = ['fm_personal_details_dob'];
+    // const offenceDetailsDateKeys = ['fm_offence_details_offence_date'];
+    // const paymentTermsDateKeys = [
+    //   'fm_payment_terms_collection_order_date',
+    //   'fm_payment_terms_start_date',
+    //   'fm_payment_terms_pay_by_date',
+    //   'fm_payment_terms_earliest_release_date',
+    // ];
+
+    // console.log(this.formatDateValues(parentGuardianDetailsState, parentGuardianDateKeys));
+
     // Build the parts of our payload...
     const initialPayload = this.buildInitialPayload(accountDetailsState, courtDetailsState, paymentTermsState);
     const defendant = buildDefendantPayload(
@@ -97,7 +132,7 @@ export class FinesMacPayloadService {
     const accountNotes = buildAccountNotesPayload(accountCommentsNotesState);
 
     // Return our payload object
-    return {
+    const finesMacPayload: IFinesMacPayload = {
       ...initialPayload,
       defendant: defendant,
       offences: null,
@@ -105,5 +140,17 @@ export class FinesMacPayloadService {
       payment_terms: paymentTerms,
       account_notes: accountNotes,
     };
+
+    const dateKeys = [
+      'dob',
+      'effective_date',
+      'collection_order_date',
+      'suspended_committal_date',
+      'account_sentence_date',
+    ];
+
+    console.log(this.formatDateValues(finesMacPayload, dateKeys));
+
+    return finesMacPayload;
   }
 }
