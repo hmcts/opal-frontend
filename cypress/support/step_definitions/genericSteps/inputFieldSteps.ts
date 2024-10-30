6bbf5ac10ee1b4b2d149b37029315348d3457a10
CadeFaulkner <cade.faulkner@hmcts.net>
2024-10-30 17:08:55 +0000

    Refactor input field steps to improve readability and performance

--- cypress/support/step_definitions/genericSteps/inputFieldSteps.ts
+++ cypress/support/step_definitions/genericSteps/inputFieldSteps.ts
@@ -1,18 +1,31 @@
 import { Then, When } from '@badeball/cypress-cucumber-preprocessor';
+import { generateString } from '../../../support/utils/stringUtils';
 
 Then('I enter more than 30 characters into the {string} field', (fieldName: string) => {
-  cy.contains('app-govuk-text-input', fieldName, { matchCase: false }).find('input').clear().type('Test'.repeat(10));
+  cy.contains('app-govuk-text-input', fieldName, { matchCase: false })
+    .find('input')
+    .clear()
+    .type('Test'.repeat(10), { delay: 0 });
+});
+Then('I enter {int} alphanumeric characters into the {string} field', (numChars: number, fieldName: string) => {
+  cy.contains('app-govuk-text-input', fieldName, { matchCase: false })
+    .find('input')
+    .clear()
+    .type(generateString(numChars), { delay: 0 });
 });
 Then('I enter {string} into the {string} field', (value: string, fieldName: string) => {
-  cy.contains('app-govuk-text-input', fieldName, { matchCase: false }).find('input').clear().type(value);
+  cy.contains('app-govuk-text-input', fieldName, { matchCase: false }).find('input').clear().type(value, { delay: 0 });
 });
 
 Then('I enter {string} into the {string} text field', (value: string, fieldName: string) => {
-  cy.contains('app-govuk-text-area', fieldName, { matchCase: false }).find('textarea').clear().type(value);
+  cy.contains('app-govuk-text-area', fieldName, { matchCase: false })
+    .find('textarea')
+    .clear()
+    .type(value, { delay: 0 });
 });
 
 Then('I enter {string} into the {string} payment field', (value: string, fieldName: string) => {
-  cy.contains('.govuk-form-group', fieldName, { matchCase: false }).find('input').clear().type(value);
+  cy.contains('.govuk-form-group', fieldName, { matchCase: false }).find('input').clear().type(value, { delay: 0 });
 });
 
 Then('I see {string} in the {string} field', (value: string, fieldName: string) => {
@@ -30,16 +43,6 @@ Then('I see {string} in the {string} payment field', (value: string, fieldName: 
 When('I see {string} under the {string} field', (text: string, fieldName: string) => {
   cy.contains('app-govuk-text-input', fieldName).find('input').prev().invoke('text').should('contains', text);
 });
-
-function generateString(length: number): string {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    result += charset[randomIndex];
-  }
-  return result;
-}
 
 Then(
   'the characters remaining counter should show {int} after entering {int} characters into the {string} input field',
@@ -67,7 +70,7 @@ Then(
       .eq(index - 1)
       .contains('label', labelText)
       .nextUntil('input')
-      .type(value);
+      .type(value, { delay: 0 });
   },
 );
 Then('I clear the {string} field for imposition {int}', (labelText: string, index: number) => {
