d51d3e3bfbfafcfa574f54de9adc8cc69f74d818
Max Holland <Max.Holland@HMCTS.NET>
2024-11-01 12:07:27 +0000

    Po 905 (#995)

---
+++ src/app/flows/fines/fines-mac/services/fines-mac-payload/constants/fines-mac-transform-items-config.constant.ts
@@ -0,0 +1,40 @@
+import { ITransformItem } from '@services/transformation-service/interfaces/transform-item.interface';
+
+export const FINES_MAC_TRANSFORM_ITEMS_CONFIG: ITransformItem[] = [
+  {
+    key: 'dob',
+    transformType: 'date',
+    dateInputFormat: 'dd/MM/yyyy',
+    dateOutputFormat: 'yyyy-MM-dd',
+  },
+  {
+    key: 'effective_date',
+    transformType: 'date',
+    dateInputFormat: 'dd/MM/yyyy',
+    dateOutputFormat: 'yyyy-MM-dd',
+  },
+  {
+    key: 'collection_order_date',
+    transformType: 'date',
+    dateInputFormat: 'dd/MM/yyyy',
+    dateOutputFormat: 'yyyy-MM-dd',
+  },
+  {
+    key: 'suspended_committal_date',
+    transformType: 'date',
+    dateInputFormat: 'dd/MM/yyyy',
+    dateOutputFormat: 'yyyy-MM-dd',
+  },
+  {
+    key: 'account_sentence_date',
+    transformType: 'date',
+    dateInputFormat: 'dd/MM/yyyy',
+    dateOutputFormat: 'yyyy-MM-dd',
+  },
+  {
+    key: 'response',
+    transformType: 'date',
+    dateInputFormat: 'dd/MM/yyyy',
+    dateOutputFormat: 'yyyy-MM-dd',
+  },
+];
