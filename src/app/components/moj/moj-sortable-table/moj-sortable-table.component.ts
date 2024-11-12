417668e2fb2cb258d5dc268a2bbc5fc4a2f7b3a8
arnabsubediHMCTS <arnab.subedi@hmcts.net>
2024-11-12 11:38:54 +0000

    PO-563

--- src/app/components/moj/moj-sortable-table/moj-sortable-table.component.ts
+++ src/app/components/moj/moj-sortable-table/moj-sortable-table.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'app-moj-sortable-table',
   standalone: true,
