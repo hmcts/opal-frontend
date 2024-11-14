190110affacccb686c1f0af7f9d4a011b6b36fe3
Frankie Moran <frankie.jack.moran@gmail.com>
2024-11-14 09:12:52 +0000

    PO-983 (#1033)

---
+++ src/app/components/govuk/govuk-table/govuk-table-body-row/govuk-table-body-row.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-govuk-table-body-row, [app-govuk-table-body-row]',
+  standalone: true,
+  imports: [],
+  templateUrl: './govuk-table-body-row.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GovukTableBodyRowComponent {
+  @Input({ required: false }) public bodyRowClasses: string = '';
+  @HostBinding('class') get hostClass() {
+    return `govuk-table__row ${this.bodyRowClasses}`;
+  }
+}
--- src/app/components/govuk/govuk-table/govuk-tablecomponent.spec.ts
+++ src/app/components/govuk/govuk-table/govuk-tablecomponent.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GovukTableComponent } from './govuk-table.component';
 
-describe('GovukTableComponent', () => {
+describe('GovukTable1Component', () => {
   let component: GovukTableComponent;
   let fixture: ComponentFixture<GovukTableComponent>;
 
@@ -18,5 +18,15 @@ describe('GovukTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have default tableClasses as undefined', () => {
+    expect(component.tableClasses).toBeUndefined();
+  });
+
+  it('should accept tableClasses as input', () => {
+    component.tableClasses = 'test-class';
+    fixture.detectChanges();
+    expect(component.tableClasses).toBe('test-class');
   });
 });
