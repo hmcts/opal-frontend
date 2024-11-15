9c56f90eea68f435ef60f6679ad7b7abac53ddd6
Max <Max.Holland@HMCTS.NET>
2024-11-15 15:28:51 +0000

    Enhance tests for building account offences payload to include null values and improve structure

--- src/app/flows/fines/fines-mac/services/fines-mac-payload/utils/fines-mac-payload-account-offences.utils.spec.ts
+++ src/app/flows/fines/fines-mac/services/fines-mac-payload/utils/fines-mac-payload-account-offences.utils.spec.ts
@@ -48,7 +48,39 @@ describe('buildAccountOffencesPayload', () => {
     expect(results).toEqual(FINES_MAC_PAYLOAD_ACCOUNT_OFFENCES_WITH_MINOR_CREDITOR);
   });
 
-  it('should build payload with a minor creditor', () => {
+  // it('should build payload with a null response object', () => {
+  //   const offencesMockState: IFinesMacOffenceDetailsForm[] = [
+  //     {
+  //       ...FINES_MAC_PAYLOAD_OFFENCE_DETAILS_STATE,
+  //       formData: {
+  //         ...FINES_MAC_PAYLOAD_OFFENCE_DETAILS_STATE.formData,
+  //         fm_offence_details_id: 0,
+  //         fm_offence_details_date_of_sentence: null,
+  //         fm_offence_details_offence_id: null,
+  //         fm_offence_details_impositions: [],
+  //       },
+  //       childFormData: null,
+  //     },
+  //   ];
+
+  //   const courtDetailsState = {
+  //     fm_court_details_originator_id: null,
+  //     fm_court_details_originator_name: null,
+  //     fm_court_details_prosecutor_case_reference: null,
+  //     fm_court_details_imposing_court_id: null,
+  //   };
+  //   const results = buildAccountOffencesPayload(offencesMockState, courtDetailsState);
+  //   expect(results).toEqual([
+  //     {
+  //       date_of_sentence: null,
+  //       imposing_court_id: null,
+  //       offence_id: null,
+  //       impositions: null,
+  //     },
+  //   ]);
+  // });
+
+  it('should build payload with null values', () => {
     const offencesMockState: IFinesMacOffenceDetailsForm[] = [
       {
         ...FINES_MAC_PAYLOAD_OFFENCE_DETAILS_STATE,
@@ -57,9 +89,42 @@ describe('buildAccountOffencesPayload', () => {
           fm_offence_details_id: 0,
           fm_offence_details_date_of_sentence: null,
           fm_offence_details_offence_id: null,
-          fm_offence_details_impositions: [],
+          fm_offence_details_impositions: [
+            {
+              fm_offence_details_imposition_id: null,
+              fm_offence_details_result_id: null,
+              fm_offence_details_amount_imposed: null,
+              fm_offence_details_amount_paid: null,
+              fm_offence_details_balance_remaining: null,
+              fm_offence_details_needs_creditor: null,
+              fm_offence_details_creditor: null,
+              fm_offence_details_major_creditor_id: null,
+            },
+          ],
         },
-        childFormData: null,
+        childFormData: [
+          {
+            formData: {
+              fm_offence_details_minor_creditor_creditor_type: null,
+              fm_offence_details_minor_creditor_title: null,
+              fm_offence_details_minor_creditor_forenames: null,
+              fm_offence_details_minor_creditor_surname: null,
+              fm_offence_details_minor_creditor_company_name: null,
+              fm_offence_details_minor_creditor_address_line_1: null,
+              fm_offence_details_minor_creditor_address_line_2: null,
+              fm_offence_details_minor_creditor_address_line_3: null,
+              fm_offence_details_minor_creditor_post_code: null,
+              fm_offence_details_minor_creditor_pay_by_bacs: null,
+              fm_offence_details_minor_creditor_bank_account_name: null,
+              fm_offence_details_minor_creditor_bank_sort_code: null,
+              fm_offence_details_minor_creditor_bank_account_number: null,
+              fm_offence_details_minor_creditor_bank_account_ref: null,
+              fm_offence_details_imposition_position: 0,
+            },
+            nestedFlow: false,
+            status: 'Provided',
+          },
+        ],
       },
     ];
 
@@ -70,12 +135,21 @@ describe('buildAccountOffencesPayload', () => {
       fm_court_details_imposing_court_id: null,
     };
     const results = buildAccountOffencesPayload(offencesMockState, courtDetailsState);
+
     expect(results).toEqual([
       {
         date_of_sentence: null,
         imposing_court_id: null,
         offence_id: null,
-        impositions: null,
+        impositions: [
+          {
+            result_id: null,
+            amount_imposed: null,
+            amount_paid: null,
+            major_creditor_id: null,
+            minor_creditor: null,
+          },
+        ],
       },
     ]);
   });
