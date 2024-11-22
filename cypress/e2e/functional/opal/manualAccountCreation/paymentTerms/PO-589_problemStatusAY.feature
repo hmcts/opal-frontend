53cfb0434d49e513b5db60ebb6fa2ae6206b47df
Frankie Moran <frankie.jack.moran@gmail.com>
2024-11-22 17:53:48 +0000

    PO-796 (#1026)
    Co-authored-by: CadeFaulkner <cade.faulkner@hmcts.net>
    Co-authored-by: Max Holland <Max.Holland@HMCTS.NET>

---
+++ cypress/e2e/functional/opal/manualAccountCreation/paymentTerms/PO-589_problemStatusAY.feature
@@ -0,0 +1,81 @@
+Feature: PO-589 - Payment terms - There is a problem status - Adult or youth only
+  Scenario: AC1,2,3 - Status updates accordingly for sentence date changes
+    Given I am on the Opal Frontend and I sign in as "opal-test@HMCTS.NET"
+    And I am on the dashboard
+    And I navigate to Manual Account Creation
+
+    And I see "Business unit and defendant type" on the page header
+    And I enter "West London" into the business unit search box
+    And I select the "Fine" radio button
+    And I select the "Adult or youth only" radio button
+    And I click the "Continue" button
+
+    Then I see "Account details" on the page header
+
+    When I click on the "Personal details" link
+    And I see "Personal details" on the page header
+    And I select title "Mrs" from dropdown
+    And I enter "FNAME" into the "First names" field
+    And I enter "LNAME" into the "Last name" field
+    And I enter "ADDR1" into the "Address line 1" field
+    And I click the "Return to account details" button
+    And I click on the "Payment terms" link
+
+    Then I see "Payment terms" on the page header
+    And I select the "Yes" radio button under the "Has a collection order been made?" section
+    And I enter a date 15 weeks into the past into the "Date of collection order" date field
+    And I select the "Pay in full" radio button
+    And I enter a date 52 weeks into the future into the "Enter pay by date" date field
+
+    When I click the "Return to account details" button
+    Then I see the status of "Payment terms" is "Provided"
+
+    When I click on the "Offence details" link
+    Then I enter "TP11003" into the "Offence code" field
+    And I enter a date 9 weeks into the past into the "Date of sentence" date field
+    And I enter "Fine (FO)" into the "Result code" field for imposition 1
+    And I enter "200" into the "Amount imposed" field for imposition 1
+    And I enter "50" into the "Amount paid" field for imposition 1
+
+    And I click the "Review offence" button
+    And I click the "Return to account details" button
+
+    Then I see the status of "Payment terms" is "There is a problem"
+
+    Then I click on the "Offence details" link
+    And I click the "Change" link for offence "TP11003"
+    And I enter a date 20 weeks into the past into the "Date of sentence" date field
+    And I click the "Review offence" button
+    And I click the "Return to account details" button
+
+    Then I see the status of "Payment terms" is "Provided"
+
+    Then I click on the "Offence details" link
+    And I click the "Change" link for offence "TP11003"
+    And I enter a date 10 weeks into the past into the "Date of sentence" date field
+    And I click the "Review offence" button
+    And I click the "Return to account details" button
+
+    Then I see the status of "Payment terms" is "There is a problem"
+
+    When I click on the "Payment terms" link
+
+    Then I see the error message "Date cannot be before the date of sentence" at the top of the page
+    And I see the error message "Date cannot be before the date of sentence" above the "Date of collection order" date field
+
+    When I click the "Return to account details" button
+    Then I see the error message "Date cannot be before the date of sentence" at the top of the page
+    And I see the error message "Date cannot be before the date of sentence" above the "Date of collection order" date field
+
+    When I click the "Add account comments and notes" button
+    Then I see the error message "Date cannot be before the date of sentence" at the top of the page
+    And I see the error message "Date cannot be before the date of sentence" above the "Date of collection order" date field
+
+    When I enter a date 5 weeks into the past into the "Date of collection order" date field
+    And I click the "Add account comments and notes" button
+
+    Then I see "Account comments and notes" on the page header
+    And I click the "Return to account details" button
+
+    Then I see the status of "Payment terms" is "Provided"
+
--- src/app/flows/fines/fines-mac/fines-mac-offence-details/fines-mac-offence-details-add-an-offence/fines-mac-offence-details-add-an-offence.component.spec.ts
+++ src/app/flows/fines/fines-mac/fines-mac-offence-details/fines-mac-offence-details-add-an-offence/fines-mac-offence-details-add-an-offence.component.spec.ts
@@ -20,6 +20,11 @@ import { FinesMacOffenceDetailsService } from '../services/fines-mac-offence-det
 import { FINES_MAC_OFFENCE_DETAILS_DRAFT_STATE } from '../constants/fines-mac-offence-details-draft-state.constant';
 import { FINES_MAC_OFFENCE_DETAILS_DRAFT_STATE_MOCK } from '../mocks/fines-mac-offence-details-draft-state.mock';
 import { FINES_MAC_OFFENCE_DETAILS_MINOR_CREDITOR_FORM_MOCK } from '../fines-mac-offence-details-minor-creditor/mocks/fines-mac-offence-details-minor-creditor-form.mock';
+import { FINES_MAC_STATUS } from '../../constants/fines-mac-status';
+import { FINES_MAC_PAYMENT_TERMS_FORM } from '../../fines-mac-payment-terms/constants/fines-mac-payment-terms-form';
+import { FINES_MAC_PAYMENT_TERMS_STATE_MOCK } from '../../fines-mac-payment-terms/mocks/fines-mac-payment-terms-state.mock';
+import { FINES_MAC_PAYMENT_TERMS_FORM_MOCK } from '../../fines-mac-payment-terms/mocks/fines-mac-payment-terms-form.mock';
+import { FINES_MAC_PAYMENT_TERMS_STATE } from '../../fines-mac-payment-terms/constants/fines-mac-payment-terms-state';
 
 describe('FinesMacOffenceDetailsAddAnOffenceComponent', () => {
   let component: FinesMacOffenceDetailsAddAnOffenceComponent;
@@ -30,7 +35,7 @@ describe('FinesMacOffenceDetailsAddAnOffenceComponent', () => {
   let formSubmit: IFinesMacOffenceDetailsForm;
 
   beforeEach(async () => {
-    mockFinesService = jasmine.createSpyObj(FinesService, ['finesMacState']);
+    mockFinesService = jasmine.createSpyObj(FinesService, ['finesMacState', 'getEarliestDateOfSentence']);
     mockFinesService.finesMacState = FINES_MAC_STATE_MOCK;
 
     mockFinesMacOffenceDetailsService = jasmine.createSpyObj(FinesMacOffenceDetailsService, [
@@ -192,9 +197,163 @@ describe('FinesMacOffenceDetailsAddAnOffenceComponent', () => {
     expect(mockFinesService.finesMacState.offenceDetails).toEqual([]);
   });
 
-  it('should set the offenceCodeMessage', () => {
-    component['addOffenceCodeMessage']('TEST');
+  it('should create autocomplete items for results', () => {
+    const response = OPAL_FINES_RESULTS_REF_DATA_MOCK;
+    const result = component['createAutoCompleteItemsResults'](response);
+    expect(result.length).toBe(response.refData.length);
+    expect(result[0].value).toBe(response.refData[0].result_id);
+    expect(result[0].name).toBe(OPAL_FINES_RESULT_PRETTY_NAME_MOCK);
+  });
 
-    expect(mockFinesMacOffenceDetailsService.offenceCodeMessage).toBe(`Offence TEST added`);
+  it('should create autocomplete items for major creditors', () => {
+    const response = OPAL_FINES_MAJOR_CREDITOR_REF_DATA_MOCK;
+    const result = component['createAutoCompleteItemsMajorCreditors'](response);
+    expect(result.length).toBe(response.refData.length);
+    expect(result[0].value).toBe(response.refData[0].major_creditor_id);
+    expect(result[0].name).toBe(OPAL_FINES_MAJOR_CREDITOR_PRETTY_NAME_MOCK);
+  });
+
+  it('should update offence details index when form exists in the state', () => {
+    const form = { ...FINES_MAC_OFFENCE_DETAILS_FORM_MOCK };
+
+    const existingForm = { ...FINES_MAC_OFFENCE_DETAILS_FORM_MOCK };
+
+    mockFinesService.finesMacState.offenceDetails = [{ ...existingForm }];
+    mockFinesService.finesMacState.offenceDetails[0].childFormData = null;
+
+    component['updateOffenceDetailsIndex'](form);
+
+    expect(mockFinesService.finesMacState.offenceDetails.length).toBe(1);
+    expect(mockFinesService.finesMacState.offenceDetails[0].formData).toEqual(form.formData);
+    expect(mockFinesService.finesMacState.offenceDetails[0].childFormData).toBeNull();
+  });
+
+  it('should add offence details form to the state when form does not exist', () => {
+    const form = { ...FINES_MAC_OFFENCE_DETAILS_FORM_MOCK };
+
+    mockFinesService.finesMacState.offenceDetails = [];
+
+    component['updateOffenceDetailsIndex'](form);
+
+    expect(mockFinesService.finesMacState.offenceDetails.length).toBe(1);
+    expect(mockFinesService.finesMacState.offenceDetails[0]).toEqual(form);
+  });
+
+  it('should retrieve form data and set offenceIndex to 0 when offenceDetails is empty', () => {
+    mockFinesService.finesMacState.offenceDetails = [];
+
+    component['retrieveFormData']();
+
+    expect(component.offenceIndex).toBe(0);
+  });
+
+  it('should retrieve form data and set offenceIndex to offenceIndex from service when offenceDetails is not empty', () => {
+    mockFinesService.finesMacState.offenceDetails = [{ ...FINES_MAC_OFFENCE_DETAILS_FORM_MOCK }];
+    mockFinesMacOffenceDetailsService.offenceIndex = 1;
+
+    component['retrieveFormData']();
+
+    expect(component.offenceIndex).toBe(1);
+  });
+
+  it('should handle form submission and navigate to account details', () => {
+    const routerSpy = spyOn(component['router'], 'navigate');
+    mockFinesService.finesMacState.offenceDetails = [];
+
+    formSubmit.nestedFlow = false;
+
+    component.handleOffenceDetailsSubmit(formSubmit);
+
+    expect(mockFinesService.finesMacState.offenceDetails).toContain(formSubmit);
+    expect(routerSpy).toHaveBeenCalledWith([FINES_MAC_OFFENCE_DETAILS_ROUTING_PATHS.children.reviewOffences], {
+      relativeTo: component['activatedRoute'].parent,
+    });
+  });
+
+  it('should handle form submission and navigate to next route', () => {
+    const routerSpy = spyOn(component['router'], 'navigate');
+    mockFinesService.finesMacState.offenceDetails = [];
+    component.offenceIndex = 0;
+
+    formSubmit.nestedFlow = true;
+
+    component.handleOffenceDetailsSubmit(formSubmit);
+
+    expect(mockFinesService.finesMacState.offenceDetails).toContain(formSubmit);
+    expect(routerSpy).not.toHaveBeenCalled();
+    expect(component.showOffenceDetailsForm).toBeTruthy();
+    expect(component.offenceIndex).toBe(1);
+    expect(mockFinesMacOffenceDetailsService.emptyOffences).toBeFalsy();
+  });
+
+  it('should handle unsaved changes', () => {
+    component.handleUnsavedChanges(true);
+    expect(mockFinesService.finesMacState.unsavedChanges).toBeTruthy();
+    expect(component.stateUnsavedChanges).toBeTruthy();
+
+    component.handleUnsavedChanges(false);
+    expect(mockFinesService.finesMacState.unsavedChanges).toBeFalsy();
+    expect(component.stateUnsavedChanges).toBeFalsy();
+  });
+
+  it('should set minorCreditorAdded to false on destroy', () => {
+    component.ngOnDestroy();
+    expect(mockFinesMacOffenceDetailsService.minorCreditorAdded).toBeFalsy();
+  });
+
+  it('should get collection order date from payment terms', () => {
+    const date = new Date();
+    spyOn(component['dateService'], 'getDateFromFormat').and.returnValue(date);
+    mockFinesService.finesMacState.paymentTerms = {
+      ...FINES_MAC_PAYMENT_TERMS_FORM_MOCK,
+      formData: {
+        ...FINES_MAC_PAYMENT_TERMS_STATE_MOCK,
+        fm_payment_terms_collection_order_date: '01/01/2022',
+      },
+    };
+
+    const result = component['getCollectionOrderDate']();
+
+    expect(result).toBe(date);
+  });
+
+  it('should return null when collection order date is not available', () => {
+    mockFinesService.finesMacState.paymentTerms = {
+      ...FINES_MAC_PAYMENT_TERMS_FORM,
+      formData: { ...FINES_MAC_PAYMENT_TERMS_STATE, fm_payment_terms_collection_order_date: null },
+    };
+
+    const result = component['getCollectionOrderDate']();
+
+    expect(result).toBeNull();
+  });
+
+  it('should set payment terms status to INCOMPLETE when collection order date is earlier than earliest date of sentence', () => {
+    const earliestDateOfSentence = new Date('2022-02-01');
+    mockFinesService.finesMacState.paymentTerms = {
+      ...FINES_MAC_PAYMENT_TERMS_FORM_MOCK,
+      formData: {
+        ...FINES_MAC_PAYMENT_TERMS_STATE_MOCK,
+        fm_payment_terms_collection_order_date: '01/01/2022',
+      },
+    };
+    mockFinesService.getEarliestDateOfSentence.and.returnValue(earliestDateOfSentence);
+
+    component['checkPaymentTermsCollectionOrder']();
+
+    expect(mockFinesService.finesMacState.paymentTerms.status).toBe(FINES_MAC_STATUS.INCOMPLETE);
+  });
+
+  it('should not set payment terms status to INCOMPLETE when collection order date is not earlier than earliest date of sentence', () => {
+    const collectionOrderDate = new Date('2022-03-01');
+    const earliestDateOfSentence = new Date('2022-02-01');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    spyOn(component as any, 'getCollectionOrderDate').and.returnValue(collectionOrderDate);
+    mockFinesService.finesMacState.paymentTerms = { ...FINES_MAC_PAYMENT_TERMS_FORM };
+    mockFinesService.getEarliestDateOfSentence.and.returnValue(earliestDateOfSentence);
+
+    component['checkPaymentTermsCollectionOrder']();
+
+    expect(mockFinesService.finesMacState.paymentTerms.status).not.toBe(FINES_MAC_STATUS.INCOMPLETE);
   });
 });
