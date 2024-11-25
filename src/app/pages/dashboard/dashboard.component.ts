70f0233599000bed3ac21b293d199488d9753a79
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2024-11-25 17:03:58 +0000

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FINES_ROUTING_PATHS } from '@routing/fines/constants/fines-routing-paths.constant';
 import { GlobalStateService } from '@services/global-state-service/global-state.service';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
