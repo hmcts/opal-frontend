62b007db434e4a30c817ebade2b57f93b727b05e
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2024-11-25 17:05:33 +0000

    Update fines.service.spec.ts

--- src/app/flows/fines/services/fines-service/fines.service.spec.ts
+++ src/app/flows/fines/services/fines-service/fines.service.spec.ts
@@ -104,6 +104,7 @@ describe('FinesService', () => {
         },
       },
     };
+    
     service.finesMacState.accountDetails.formData.fm_create_account_defendant_type = 'adultOrYouthOnly';
     expect(service.checkMandatorySections()).toBeFalse();
 
