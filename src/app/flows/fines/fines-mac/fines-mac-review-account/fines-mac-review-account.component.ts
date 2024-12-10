27d02573208cc595ba96d828f8e08bf004e0e3b1
Frankie Moran <frankie.jack.moran@gmail.com>
2024-12-10 13:51:55 +0000

    PO-663 (#1076)
    Co-authored-by: CadeFaulkner <cade.faulkner@hmcts.net>

--- src/app/flows/fines/fines-mac/fines-mac-review-account/fines-mac-review-account.component.ts
+++ src/app/flows/fines/fines-mac/fines-mac-review-account/fines-mac-review-account.component.ts
@@ -25,6 +25,7 @@ import {
   IOpalFinesLocalJusticeAreaRefData,
 } from '@services/fines/opal-fines-service/interfaces/opal-fines-local-justice-area-ref-data.interface';
 import { FinesMacReviewAccountParentGuardianDetailsComponent } from './fines-mac-review-account-parent-guardian-details/fines-mac-review-account-parent-guardian-details.component';
+import { FinesMacReviewAccountCompanyDetailsComponent } from './fines-mac-review-account-company-details/fines-mac-review-account-company-details.component';
 
 @Component({
   selector: 'app-fines-mac-review-account',
@@ -42,6 +43,7 @@ import { FinesMacReviewAccountParentGuardianDetailsComponent } from './fines-mac
     FinesMacReviewAccountAccountCommentsAndNotesComponent,
     FinesMacReviewAccountOffenceDetailsComponent,
     FinesMacReviewAccountParentGuardianDetailsComponent,
+    FinesMacReviewAccountCompanyDetailsComponent,
   ],
   templateUrl: './fines-mac-review-account.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -99,6 +101,9 @@ export class FinesMacReviewAccountComponent {
     if (nonRelative) {
       this.router.navigate([route]);
     } else {
+      if (route === this.fineMacRoutes.children.deleteAccountConfirmation) {
+        this.finesService.finesMacState.deleteFromCheckAccount = true;
+      }
       this.router.navigate([route], { relativeTo: this.activatedRoute.parent });
     }
   }
