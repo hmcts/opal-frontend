c44945fc5600e2593808b1e5b7ec427e61ae197f
Frankie Moran <frankie.jack.moran@gmail.com>
2024-12-10 15:37:16 +0000

    PO-560 (#1074)
    Co-authored-by: CadeFaulkner <cade.faulkner@hmcts.net>

--- src/app/flows/fines/fines-mac/fines-mac-review-account/fines-mac-review-account.component.ts
+++ src/app/flows/fines/fines-mac/fines-mac-review-account/fines-mac-review-account.component.ts
@@ -3,11 +3,48 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { GovukBackLinkComponent } from '@components/govuk/govuk-back-link/govuk-back-link.component';
 import { GovukButtonComponent } from '@components/govuk/govuk-button/govuk-button.component';
 import { FINES_MAC_ROUTING_PATHS } from '../routing/constants/fines-mac-routing-paths';
+import { FinesService } from '@services/fines/fines-service/fines.service';
+import { FinesMacReviewAccountAccountDetailsComponent } from './fines-mac-review-account-account-details/fines-mac-review-account-account-details.component';
+import { FinesMacReviewAccountCourtDetailsComponent } from './fines-mac-review-account-court-details/fines-mac-review-account-court-details.component';
+import {
+  IOpalFinesCourt,
+  IOpalFinesCourtRefData,
+} from '@services/fines/opal-fines-service/interfaces/opal-fines-court-ref-data.interface';
+import { forkJoin, Observable, tap } from 'rxjs';
+import { OpalFines } from '@services/fines/opal-fines-service/opal-fines.service';
+import { CommonModule } from '@angular/common';
+import { FinesMacReviewAccountPersonalDetailsComponent } from './fines-mac-review-account-personal-details/fines-mac-review-account-personal-details.component';
+import { FinesMacReviewAccountContactDetailsComponent } from './fines-mac-review-account-contact-details/fines-mac-review-account-contact-details.component';
+import { FinesMacReviewAccountEmployerDetailsComponent } from './fines-mac-review-account-employer-details/fines-mac-review-account-employer-details.component';
+
+import { FinesMacReviewAccountPaymentTermsComponent } from './fines-mac-review-account-payment-terms/fines-mac-review-account-payment-terms.component';
+import { FinesMacReviewAccountAccountCommentsAndNotesComponent } from './fines-mac-review-account-account-comments-and-notes/fines-mac-review-account-account-comments-and-notes.component';
+import { FinesMacReviewAccountOffenceDetailsComponent } from './fines-mac-review-account-offence-details/fines-mac-review-account-offence-details.component';
+import {
+  IOpalFinesLocalJusticeArea,
+  IOpalFinesLocalJusticeAreaRefData,
+} from '@services/fines/opal-fines-service/interfaces/opal-fines-local-justice-area-ref-data.interface';
+import { FinesMacReviewAccountParentGuardianDetailsComponent } from './fines-mac-review-account-parent-guardian-details/fines-mac-review-account-parent-guardian-details.component';
+import { FinesMacReviewAccountCompanyDetailsComponent } from './fines-mac-review-account-company-details/fines-mac-review-account-company-details.component';
 
 @Component({
   selector: 'app-fines-mac-review-account',
   standalone: true,
-  imports: [GovukBackLinkComponent, GovukButtonComponent],
+  imports: [
+    CommonModule,
+    GovukBackLinkComponent,
+    GovukButtonComponent,
+    FinesMacReviewAccountAccountDetailsComponent,
+    FinesMacReviewAccountCourtDetailsComponent,
+    FinesMacReviewAccountPersonalDetailsComponent,
+    FinesMacReviewAccountContactDetailsComponent,
+    FinesMacReviewAccountEmployerDetailsComponent,
+    FinesMacReviewAccountPaymentTermsComponent,
+    FinesMacReviewAccountAccountCommentsAndNotesComponent,
+    FinesMacReviewAccountOffenceDetailsComponent,
+    FinesMacReviewAccountParentGuardianDetailsComponent,
+    FinesMacReviewAccountCompanyDetailsComponent,
+  ],
   templateUrl: './fines-mac-review-account.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -15,7 +52,34 @@ export class FinesMacReviewAccountComponent {
   private readonly router = inject(Router);
   private readonly activatedRoute = inject(ActivatedRoute);
 
+  private readonly opalFinesService = inject(OpalFines);
+  protected readonly finesService = inject(FinesService);
+
+  protected enforcementCourtsData!: IOpalFinesCourt[];
+  protected localJusticeAreasData!: IOpalFinesLocalJusticeArea[];
+
   protected readonly fineMacRoutes = FINES_MAC_ROUTING_PATHS;
+
+  private readonly enforcementCourtsData$: Observable<IOpalFinesCourtRefData> = this.opalFinesService
+    .getCourts(this.finesService.finesMacState.businessUnit.business_unit_id)
+    .pipe(
+      tap((response: IOpalFinesCourtRefData) => {
+        this.enforcementCourtsData = response.refData;
+      }),
+    );
+
+  private readonly localJusticeAreasData$: Observable<IOpalFinesLocalJusticeAreaRefData> = this.opalFinesService
+    .getLocalJusticeAreas()
+    .pipe(
+      tap((response: IOpalFinesLocalJusticeAreaRefData) => {
+        this.localJusticeAreasData = response.refData;
+      }),
+    );
+
+  protected groupLjaAndCourtData$ = forkJoin({
+    enforcementCourtsData: this.enforcementCourtsData$,
+    localJusticeAreasData: this.localJusticeAreasData$,
+  });
 
   /**
    * Navigates back to the previous page
@@ -37,6 +101,9 @@ export class FinesMacReviewAccountComponent {
     if (nonRelative) {
       this.router.navigate([route]);
     } else {
+      if (route === this.fineMacRoutes.children.deleteAccountConfirmation) {
+        this.finesService.finesMacState.deleteFromCheckAccount = true;
+      }
       this.router.navigate([route], { relativeTo: this.activatedRoute.parent });
     }
   }
