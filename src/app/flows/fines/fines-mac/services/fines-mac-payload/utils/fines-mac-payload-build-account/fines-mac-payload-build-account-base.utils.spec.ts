94aac3afb275b5849013faf2dc34ee048e6c0162
Max <Max.Holland@HMCTS.NET>
2024-12-19 15:22:42 +0000

    test: refactor finesMacPayloadBuildAccountBase tests for improved clarity and state management; add initial payload mock

--- src/app/flows/fines/fines-mac/services/fines-mac-payload/utils/fines-mac-payload-build-account/fines-mac-payload-build-account-base.utils.spec.ts
+++ src/app/flows/fines/fines-mac/services/fines-mac-payload/utils/fines-mac-payload-build-account/fines-mac-payload-build-account-base.utils.spec.ts
@@ -9,39 +9,7 @@ import { FINES_MAC_PAYLOAD_COURT_DETAILS_STATE_MOCK } from '../mocks/state/fines
 import { FINES_MAC_PAYLOAD_PAYMENT_TERMS_IN_FULL_MOCK } from '../mocks/state/fines-mac-payload-payment-terms-state.mock';
 import { FINES_MAC_PAYLOAD_OFFENCE_DETAILS_STATE } from '../mocks/state/fines-mac-payload-offence-details-state.mock';
 import { IFinesMacOffenceDetailsState } from '../../../../fines-mac-offence-details/interfaces/fines-mac-offence-details-state.interface';
-
-const EXPECTED_PAYLOAD: IFinesMacPayloadAccountAccountInitial = {
-  account_type: 'conditionalCaution',
-  defendant_type: 'adultOrYouthOnly',
-  originator_name: 'Crown Prosecution Service',
-  originator_id: '4821',
-  prosecutor_case_reference: 'P2BC305678',
-  enforcement_court_id: 'Magistrates Court Database (204)',
-  collection_order_made: true,
-  collection_order_made_today: null,
-  collection_order_date: '22/10/2024',
-  suspended_committal_date: '12/10/2024',
-  payment_card_request: true,
-  account_sentence_date: '01/09/2024',
-};
-
-const OFFENCE_MOCK: IFinesMacOffenceDetailsState = {
-  fm_offence_details_id: 0,
-  fm_offence_details_date_of_sentence: null,
-  fm_offence_details_offence_id: 'OFF1234',
-  fm_offence_details_impositions: [
-    {
-      fm_offence_details_imposition_id: 1,
-      fm_offence_details_result_id: 'FCOST',
-      fm_offence_details_amount_imposed: 300,
-      fm_offence_details_amount_paid: 500,
-      fm_offence_details_balance_remaining: 400,
-      fm_offence_details_needs_creditor: true,
-      fm_offence_details_creditor: 'major',
-      fm_offence_details_major_creditor_id: 9999,
-    },
-  ],
-};
+import { FINES_MAC_PAYLOAD_ACCOUNT_ACCOUNT_INITIAL_MOCK } from '../mocks/fines-mac-payload-account-account-initial.mock';
 
 describe('finesMacPayloadBuildAccountBase', () => {
   let expectedPayload: IFinesMacPayloadAccountAccountInitial;
@@ -51,12 +19,28 @@ describe('finesMacPayloadBuildAccountBase', () => {
   let paymentTermsState: IFinesMacPaymentTermsState;
   let offenceDetailsState: IFinesMacOffenceDetailsState[];
   beforeEach(() => {
-    expectedPayload = structuredClone(EXPECTED_PAYLOAD);
-    offenceMock = structuredClone(OFFENCE_MOCK);
+    expectedPayload = structuredClone(FINES_MAC_PAYLOAD_ACCOUNT_ACCOUNT_INITIAL_MOCK);
     courtDetailsState = structuredClone(FINES_MAC_PAYLOAD_COURT_DETAILS_STATE_MOCK);
     accountDetailsState = structuredClone(FINES_MAC_PAYLOAD_ACCOUNT_DETAILS_STATE_MOCK);
     paymentTermsState = structuredClone(FINES_MAC_PAYLOAD_PAYMENT_TERMS_IN_FULL_MOCK);
     offenceDetailsState = structuredClone([FINES_MAC_PAYLOAD_OFFENCE_DETAILS_STATE.formData]);
+    offenceMock = structuredClone({
+      fm_offence_details_id: 0,
+      fm_offence_details_date_of_sentence: null,
+      fm_offence_details_offence_id: 'OFF1234',
+      fm_offence_details_impositions: [
+        {
+          fm_offence_details_imposition_id: 1,
+          fm_offence_details_result_id: 'FCOST',
+          fm_offence_details_amount_imposed: 300,
+          fm_offence_details_amount_paid: 500,
+          fm_offence_details_balance_remaining: 400,
+          fm_offence_details_needs_creditor: true,
+          fm_offence_details_creditor: 'major',
+          fm_offence_details_major_creditor_id: 9999,
+        },
+      ],
+    });
   });
   it('should build the initial payload correctly', () => {
     const result = finesMacPayloadBuildAccountBase(
