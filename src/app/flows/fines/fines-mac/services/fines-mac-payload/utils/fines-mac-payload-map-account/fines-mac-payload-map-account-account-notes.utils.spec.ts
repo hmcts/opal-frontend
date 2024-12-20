e3e017783eabe44b8dbe0c4efff4c37a12d7e35e
Max <Max.Holland@HMCTS.NET>
2024-12-20 11:44:22 +0000

    test: enhance null checks and cleanup in finesMacPayloadMapAccount tests

--- src/app/flows/fines/fines-mac/services/fines-mac-payload/utils/fines-mac-payload-map-account/fines-mac-payload-map-account-account-notes.utils.spec.ts
+++ src/app/flows/fines/fines-mac/services/fines-mac-payload/utils/fines-mac-payload-map-account/fines-mac-payload-map-account-account-notes.utils.spec.ts
@@ -3,7 +3,7 @@ import { IFinesMacState } from '../../../../interfaces/fines-mac-state.interface
 import { IFinesMacPayloadAccountAccountNote } from '../interfaces/fines-mac-payload-account-account-note.interface';
 
 describe('finesMacPayloadMapAccountAccountNotesPayload', () => {
-  let initialState: IFinesMacState;
+  let initialState: IFinesMacState | null;
 
   beforeEach(() => {
     initialState = {
@@ -16,12 +16,24 @@ describe('finesMacPayloadMapAccountAccountNotesPayload', () => {
     } as IFinesMacState;
   });
 
+  afterAll(() => {
+    initialState = null;
+  });
+
   it('should return the initial state if payload is null', () => {
+    if (!initialState) {
+      fail('Initial state is not properly initialised');
+      return;
+    }
     const result = finesMacPayloadMapAccountAccountNotesPayload(initialState, null);
     expect(result).toEqual(initialState);
   });
 
   it('should map account note with note_type "AC" to fm_account_comments_notes_comments', () => {
+    if (!initialState) {
+      fail('Initial state is not properly initialised');
+      return;
+    }
     const payload: IFinesMacPayloadAccountAccountNote[] = [
       { account_note_text: 'Test comment', account_note_serial: 1, note_type: 'AC' },
     ];
@@ -30,6 +42,11 @@ describe('finesMacPayloadMapAccountAccountNotesPayload', () => {
   });
 
   it('should map account note with note_type "AA" and serial 2 to fm_account_comments_notes_notes', () => {
+    if (!initialState) {
+      fail('Initial state is not properly initialised');
+      return;
+    }
+
     const payload: IFinesMacPayloadAccountAccountNote[] = [
       { account_note_text: 'Test note', account_note_serial: 2, note_type: 'AA' },
     ];
@@ -38,6 +55,10 @@ describe('finesMacPayloadMapAccountAccountNotesPayload', () => {
   });
 
   it('should not map account note with note_type "AA" and serial not equal to 2', () => {
+    if (!initialState) {
+      fail('Initial state is not properly initialised');
+      return;
+    }
     const payload: IFinesMacPayloadAccountAccountNote[] = [
       { account_note_text: 'Test note', account_note_serial: 1, note_type: 'AA' },
     ];
