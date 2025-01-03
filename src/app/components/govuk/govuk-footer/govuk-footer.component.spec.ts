ffe3593b3902f0613008f00d91b867c883f106b0
Max Holland <Max.Holland@HMCTS.NET>
2025-01-03 10:58:17 +0000

    Govuk test optimisation (#1130)
    Co-authored-by: Frankie Moran <frankie.jack.moran@gmail.com>

--- src/app/components/govuk/govuk-footer/govuk-footer.component.spec.ts
+++ src/app/components/govuk/govuk-footer/govuk-footer.component.spec.ts
@@ -3,8 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GovukFooterComponent } from './govuk-footer.component';
 
 describe('GovukFooterComponent', () => {
-  let component: GovukFooterComponent;
-  let fixture: ComponentFixture<GovukFooterComponent>;
+  let component: GovukFooterComponent | null;
+  let fixture: ComponentFixture<GovukFooterComponent> | null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,6 +14,12 @@ describe('GovukFooterComponent', () => {
     fixture = TestBed.createComponent(GovukFooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterAll(() => {
+    fixture = null;
+    component = null;
+    TestBed.resetTestingModule();
   });
 
   it('should create', () => {
