4634d5ec89636f1c119fe00eb3d5a2cdf3eebfa4
Frankie Moran <frankie.jack.moran@gmail.com>
2025-01-03 09:53:30 +0000

    Delete unused files to prevent 0% coverage being reported

--- src/app/flows/fines/fines-mac/constants/controls/fines-mac-controls-surname.ts
+++
@@ -1,8 +0,0 @@
-import { Validators } from '@angular/forms';
-import { alphabeticalTextValidator } from '@validators/alphabetical-text/alphabetical-text.validator';
-import { IAbstractFormArrayControlValidation } from '@components/abstract/interfaces/abstract-form-array-control-validation.interface';
-
-export const FINES_MAC_CONTROLS_SURNAME: IAbstractFormArrayControlValidation = {
-  controlName: 'surname',
-  validators: [Validators.required, Validators.maxLength(30), alphabeticalTextValidator()],
-};
