aa3c1817ee3203fd2e86238420e72d71519be0c0
Frankie Moran <frankie.jack.moran@gmail.com>
2025-01-03 10:41:47 +0000

    Delete unused files to prevent 0% coverage being reported (#1141)

--- src/app/flows/fines/fines-mac/constants/controls/fines-mac-controls-add-alias.ts
+++
@@ -1,6 +0,0 @@
-import { IAbstractFormArrayControlValidation } from '@components/abstract/interfaces/abstract-form-array-control-validation.interface';
-
-export const FINES_MAC_CONTROLS_ADD_ALIAS: IAbstractFormArrayControlValidation = {
-  controlName: 'add_alias',
-  validators: [],
-};
