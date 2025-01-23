7ed66b4b9b27afcd760c0d7ae6199f94314b6c85
CadeFaulkner <cade.faulkner@hmcts.net>
2025-01-23 16:14:57 +0000

    Refactor Cypress configuration to modularize browser launch setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,36 @@ import { defineConfig } from 'cypress';
 import * as webpack from '@cypress/webpack-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions,
-): Promise<Cypress.PluginConfigOptions> {
-  await addCucumberPreprocessorPlugin(on, config);
+function setupBrowserLaunch(on) {
+  on('before:browser:launch', (browser, launchOptions) => {
+    console.log('launching browser %s is headless? %s', browser.name, browser.isHeadless);
+
+    const width = 3640;
+    const height = 2560;
+
+    console.log('setting the browser window size to %d x %d', width, height);
+
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push(`--window-size=${width},${height}`);
+      launchOptions.args.push('--force-device-scale-factor=1');
+    }
+
+    if (browser.name === 'electron' && browser.isHeadless) {
+      launchOptions.preferences.width = width;
+      launchOptions.preferences.height = height;
+    }
+
+    if (browser.name === 'firefox' && browser.isHeadless) {
+      launchOptions.args.push(`--width=${width}`);
+      launchOptions.args.push(`--height=${height}`);
+    }
+
+    return launchOptions;
+  });
+}
+
+function setupNodeEvents(on, config) {
+  addCucumberPreprocessorPlugin(on, config);
 
   on(
     'file:preprocessor',
@@ -47,36 +72,12 @@ async function setupNodeEvents(
       },
     }),
   );
-  on('before:browser:launch', (browser, launchOptions) => {
-    console.log('launching browser %s is headless? %s', browser.name, browser.isHeadless);
-
-    const width = 3640;
-    const height = 2560;
-
-    console.log('setting the browser window size to %d x %d', width, height);
-
-    if (browser.name === 'chrome' && browser.isHeadless) {
-      launchOptions.args.push(`--window-size=${width},${height}`);
-
-      launchOptions.args.push('--force-device-scale-factor=1');
-    }
-
-    if (browser.name === 'electron' && browser.isHeadless) {
-      launchOptions.preferences.width = width;
-      launchOptions.preferences.height = height;
-    }
-
-    if (browser.name === 'firefox' && browser.isHeadless) {
-      launchOptions.args.push(`--width=${width}`);
-      launchOptions.args.push(`--height=${height}`);
-    }
-
-    return launchOptions;
-  });
+  setupBrowserLaunch(on);
   config.env['messagesOutput'] =
     `${process.env.TEST_STAGE}-output/prod/cucumber/${process.env.TEST_MODE}-report-${process.env.CYPRESS_THREAD}.ndjson`;
-  return config;
+  return Promise.resolve(config);
 }
+
 export default defineConfig({
   viewportWidth: 2560,
   viewportHeight: 2560,
@@ -109,5 +110,11 @@ export default defineConfig({
     },
     specPattern: 'cypress/component/**/*.cy.ts',
     supportFile: false,
+    setupNodeEvents(on, config) {
+      setupBrowserLaunch(on);
+      config.env['messagesOutput'] =
+        `${process.env.TEST_STAGE}-output/prod/cucumber/${process.env.TEST_MODE}-report-${process.env.CYPRESS_THREAD}.ndjson`;
+      return Promise.resolve(config);
+    },
   },
 });
