0b1f221737a2b68f8efd542f0a19ff968e28d728
Max <Max.Holland@HMCTS.NET>
2025-01-23 10:01:25 +0000

    refactor: remove unused interfaces and mock data from opal fines service

--- src/app/flows/fines/services/opal-fines-service/interfaces/opal-fines-search-court-body.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IOpalFinesSearchCourtBody {
-  courtId: null | number;
-  courtCode: null | number;
-  parentCourtId: null | number;
-  localJusticeAreaId: null | number;
-  nationalCourtCode: null | string;
-}
