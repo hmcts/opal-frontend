ffc2a98ac683b52edf6bd30b03213a53a599f171
CadeFaulkner <cade.faulkner@hmcts.net>
2025-01-28 17:08:04 +0000

    test: enhance FinesMacCourtDetails component tests with additional scenarios and validations

--- cypress/component/manualAccountCreation/FinesMacCourtDetails/FinesMacCourtDetails.cy.ts
+++ cypress/component/manualAccountCreation/FinesMacCourtDetails/FinesMacCourtDetails.cy.ts
@@ -2,29 +2,23 @@ import { mount } from 'cypress/angular';
 import { OpalFines } from '../../../../src/app/flows/fines/services/opal-fines-service/opal-fines.service';
 import { ActivatedRoute } from '@angular/router';
 import { FINES_COURTS_DETAILS_MOCK } from './mocks/fines-mac-court-details-mock';
+
 import { FinesMacCourtDetailsComponent } from '../../../../src/app/flows/fines/fines-mac/fines-mac-court-details/fines-mac-court-details.component';
 import { FinesService } from '@services/fines/fines-service/fines.service';
 import { OPAL_FINES_LOCAL_JUSTICE_AREA_REF_DATA_MOCK } from '../../../../src/app/flows/fines/services/opal-fines-service/mocks/opal-fines-local-justice-area-ref-data.mock';
 import { OPAL_FINES_COURT_REF_DATA_MOCK } from '../../../../src/app/flows/fines/services/opal-fines-service/mocks/opal-fines-court-ref-data.mock';
-import { ERROR_MESSAGES, REQUIRED_FIELDS } from './constants/fines_mac_court_details_errors';
-import { DOM_ELEMENTS } from './constants/fines_mac_court_details_elements';
 import { of } from 'rxjs';
+import { DOM_ELEMENTS } from './constants/fines_mac_court_details_elements';
+import { INVALID_ERRORS, MISSING_ERRORS } from './constants/fines_mac_court_details_errors';
 
 describe('FinesMacParentGuardianDetailsComponent', () => {
   let mockFinesService = {
     finesMacState: { ...FINES_COURTS_DETAILS_MOCK },
   };
-  mockFinesService.finesMacState.businessUnit.business_unit_id = 73;
 
-  afterEach(() => {
-    mockFinesService.finesMacState.courtDetails.formData = {
-      fm_court_details_originator_id: '',
-      fm_court_details_originator_name: '',
-      fm_court_details_prosecutor_case_reference: '',
-      fm_court_details_imposing_court_id: '',
-    };
-  });
-  const setupComponent = (formSubmit: any) => {
+  const setupComponent = (formSubmit: any, defType?: string) => {
+    mockFinesService.finesMacState.businessUnit.business_unit_id = 73;
+
     const mockOpalFinesService = {
       getLocalJusticeAreas: () => of(OPAL_FINES_LOCAL_JUSTICE_AREA_REF_DATA_MOCK),
       getLocalJusticeAreaPrettyName: (item: any) => `${item.name} (${item.lja_code})`,
@@ -49,84 +43,153 @@ describe('FinesMacParentGuardianDetailsComponent', () => {
       ],
       componentProperties: {
         handleCourtDetailsSubmit: formSubmit,
+        defendantType: defType,
       },
     });
   };
-
-  it('should render the child component', () => {
-    setupComponent(null);
-    mockFinesService.finesMacState.accountDetails.formData.fm_create_account_defendant_type = 'adultOrYouthOnly';
-
-    // Verify the child component is rendered
-    cy.get(DOM_ELEMENTS['app']).should('exist');
+  //Clean up after each test
+  afterEach(() => {
+    mockFinesService.finesMacState.courtDetails.formData = {
+      fm_court_details_originator_id: '',
+      fm_court_details_originator_name: '',
+      fm_court_details_prosecutor_case_reference: '',
+      fm_court_details_imposing_court_id: '',
+    };
   });
 
-  it('should not allow form to be submitted with no input and should throw required fields errors', () => {
-    setupComponent(null);
+  it('should render the component correctly for adultOrYouthOnly', () => {
+    setupComponent(null, 'adultOrYouthOnly');
+    cy.get('app-fines-mac-court-details-form').should('exist');
 
-    cy.get('button[type = "submit"]').first().click();
+    cy.get('button[type="submit"]').should('contain', 'Add personal details');
+  });
+  it('should render the component correctly for parentOrGuardianToPay', () => {
+    setupComponent(null, 'parentOrGuardianToPay');
+    cy.get('app-fines-mac-court-details-form').should('exist');
 
-    for (const [, value] of Object.entries(REQUIRED_FIELDS)) {
-      cy.get(DOM_ELEMENTS['errorSummary']).should('contain', value);
-    }
+    cy.get('button[type="submit"]').should('contain', 'Add parent or guardian details');
+  });
+  it('should render the component correctly for company', () => {
+    setupComponent(null, 'company');
+    cy.get('app-fines-mac-court-details-form').should('exist');
+
+    cy.get('button[type="submit"]').should('contain', 'Add company details');
   });
 
-  it('should not allow length of prosecutor reference to be more than 30 characters', () => {
-    setupComponent(null);
-
-    mockFinesService.finesMacState.courtDetails.formData.fm_court_details_prosecutor_case_reference = 'a'.repeat(31);
-
-    cy.get('button[type = "submit"]').first().click();
-
-    cy.get(DOM_ELEMENTS['errorSummary']).should('contain', ERROR_MESSAGES['maxLengthProsecutorCaseReference']);
-  });
-
-  it('should not allow special characters in prosecutor reference', () => {
-    setupComponent(null);
-
-    mockFinesService.finesMacState.courtDetails.formData.fm_court_details_prosecutor_case_reference = '£';
-
-    cy.get('button[type = "submit"]').first().click();
-
-    cy.get(DOM_ELEMENTS['errorSummary']).should('contain', ERROR_MESSAGES['patternProsecutorCaseReference']);
-  });
-
-  it('should allow valid details and form submission', () => {
+  it('should have correct titles and allow inputs', () => {
     const mockFormSubmit = cy.spy().as('formSubmitSpy');
-
     setupComponent(mockFormSubmit);
 
-    cy.get(DOM_ELEMENTS['originatorAutoComplete']).type("Avon & Somerset Magistrates' Court (1450)", {
-      delay: 0,
-    });
+    // Verify the page text
+    //title
+    cy.get(DOM_ELEMENTS.pageTitle).should('contain', 'Court details');
+    //Lja
+    cy.get(DOM_ELEMENTS.ljaInputTitle).should('contain', 'Sending area or Local Justice Area (LJA)');
+    cy.get(DOM_ELEMENTS.ljaInputHint).should(
+      'contain',
+      'Search using the code or name of the area that sent the transfer',
+    );
+    //Pcr
+    cy.get(DOM_ELEMENTS.pcrInputTitle).should('contain', 'Prosecutor Case Reference (PCR)');
+    cy.get(DOM_ELEMENTS.pcrInputHint).should(
+      'contain',
+      "Enter the prosecutor's reference number or original account number",
+    );
+    //Enforcement Court
+    cy.get(DOM_ELEMENTS.enforcementCourtTitle).should('contain', 'Enforcement court');
+    cy.get(DOM_ELEMENTS.enforcementCourtHint).should('contain', 'Search using enforcement court or code');
 
-    cy.get(DOM_ELEMENTS['courtIdAutoComplete']).type('Historic Debt Database (101)', { delay: 0 });
+    //Verify working input fields
+    cy.get(DOM_ELEMENTS.ljaInput).focus().type('Asylum', { delay: 0 });
+    cy.get(DOM_ELEMENTS.ljaAutocomplete).find('li').first().click();
+    cy.get(DOM_ELEMENTS.ljaInput).should('have.value', 'Asylum & Immigration Tribunal (9985)');
 
-    mockFinesService.finesMacState.courtDetails.formData.fm_court_details_prosecutor_case_reference = '123';
+    cy.get(DOM_ELEMENTS.pcrInput).focus().type('1234', { delay: 0 });
+    cy.get(DOM_ELEMENTS.pcrInput).should('have.value', '1234');
 
-    cy.get('button[type = "submit"]').first().click();
-    cy.get('button[type = "submit"]').first().click();
+    cy.get(DOM_ELEMENTS.enforcementCourt).focus().type('Port', { delay: 0 });
+    cy.get(DOM_ELEMENTS.enforcementCourtAutocomplete).find('li').first().click();
+    cy.get(DOM_ELEMENTS.enforcementCourt).should('have.value', 'Port Talbot Justice Centre (999)');
 
+    //Verify form submit
+    cy.get(DOM_ELEMENTS.returnToACDetails).click();
     cy.get('@formSubmitSpy').should('have.been.calledOnce');
   });
 
-  it('should not allow invalid data to be submitted', () => {
+  it('should validate mandatory fields', () => {
     setupComponent(null);
 
-    cy.get(DOM_ELEMENTS['originatorAutoComplete']).type('random', {
-      delay: 0,
+    //Submit without input
+    cy.get(DOM_ELEMENTS.returnToACDetails).click();
+
+    //Verify error summary
+    cy.get(DOM_ELEMENTS.errorSummary).should('exist');
+
+    Object.values(MISSING_ERRORS).forEach((key) => {
+      cy.get(DOM_ELEMENTS.errorSummary).should('contain', key);
     });
 
-    cy.get(DOM_ELEMENTS['courtIdAutoComplete']).type('random', { delay: 0 });
+    //Verify input field error messages
+    cy.get(DOM_ELEMENTS.ljaErrorMessage).should('contain', MISSING_ERRORS.missingLJA);
+    cy.get(DOM_ELEMENTS.pcrErrorMessage).should('contain', MISSING_ERRORS.missingPCR);
+    cy.get(DOM_ELEMENTS.enforcementCourtErrorMessage).should('contain', MISSING_ERRORS.missingEnforcementCourt);
+  });
 
-    mockFinesService.finesMacState.courtDetails.formData.fm_court_details_prosecutor_case_reference = 'r@';
+  it('should validate PRC field length', () => {
+    mockFinesService.finesMacState.courtDetails.formData.fm_court_details_prosecutor_case_reference = 'a'.repeat(31);
 
-    cy.get('button[type = "submit"]').first().click();
-    cy.get('button[type = "submit"]').first().click();
+    setupComponent(null);
 
-    cy.get(DOM_ELEMENTS['errorSummary'])
-      .should('contain', ERROR_MESSAGES['patternProsecutorCaseReference'])
-      .should('contain', REQUIRED_FIELDS['emptySendingArea'])
-      .should('contain', REQUIRED_FIELDS['emptyEnforcementCourt']);
+    //Submit with invalid input
+    cy.get(DOM_ELEMENTS.returnToACDetails).click();
+
+    //Verify error message
+    cy.get(DOM_ELEMENTS.pcrErrorMessage).should('contain', INVALID_ERRORS.tooLongPCR);
+  });
+
+  it('should validate PCR field allowed characters', () => {
+    setupComponent(null);
+
+    const invalidInputs = ['1234!', '1@', 'test@', 'test1234@', 'abc#', '123$', 'abc%', '123^', 'abc&', '123*'];
+
+    invalidInputs.forEach((input) => {
+      cy.get(DOM_ELEMENTS.pcrInput).clear().type(input, { delay: 0 });
+
+      //Submit with invalid input
+      cy.get(DOM_ELEMENTS.returnToACDetails).click();
+
+      //Verify error message
+      cy.get(DOM_ELEMENTS.pcrErrorMessage).should('contain', INVALID_ERRORS.invalidPCR);
+    });
+  });
+  it('should clear errors when validation is met', () => {
+    const mockFormSubmit = cy.spy().as('formSubmitSpy');
+    setupComponent(mockFormSubmit);
+
+    //Submit without input
+    cy.get(DOM_ELEMENTS.returnToACDetails).click();
+
+    //Verify error summary
+    cy.get(DOM_ELEMENTS.errorSummary).should('exist');
+
+    //Verify input field error messages
+    cy.get(DOM_ELEMENTS.ljaErrorMessage).should('contain', MISSING_ERRORS.missingLJA);
+    cy.get(DOM_ELEMENTS.pcrErrorMessage).should('contain', MISSING_ERRORS.missingPCR);
+    cy.get(DOM_ELEMENTS.enforcementCourtErrorMessage).should('contain', MISSING_ERRORS.missingEnforcementCourt);
+
+    //Verify error summary is cleared
+    cy.get(DOM_ELEMENTS.ljaInput).focus().type('Asylum', { delay: 0 });
+    cy.get(DOM_ELEMENTS.ljaAutocomplete).find('li').first().click();
+
+    cy.get(DOM_ELEMENTS.pcrInput).focus().type('1234', { delay: 0 });
+
+    cy.get(DOM_ELEMENTS.enforcementCourt).focus().type('Port', { delay: 0 });
+    cy.get(DOM_ELEMENTS.enforcementCourtAutocomplete).find('li').first().click();
+
+    cy.get(DOM_ELEMENTS.returnToACDetails).click();
+    cy.get(DOM_ELEMENTS.errorSummary).should('not.exist');
+    cy.get(DOM_ELEMENTS.ljaErrorMessage).should('not.exist');
+    cy.get(DOM_ELEMENTS.pcrErrorMessage).should('not.exist');
+    cy.get(DOM_ELEMENTS.enforcementCourtErrorMessage).should('not.exist');
   });
 });
