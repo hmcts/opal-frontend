014fccd1edf0a038603746bc0cad58ba0eb1eef4
Frankie Moran <frankie.jack.moran@gmail.com>
2025-01-31 14:58:10 +0000

    PO-1173 (#1191)
    Co-authored-by: Max Holland <Max.Holland@HMCTS.NET>

--- src/app/services/app-initializer-service/app-initializer.service.spec.ts
+++ src/app/services/app-initializer-service/app-initializer.service.spec.ts
@@ -2,11 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { AppInitializerService } from './app-initializer.service';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { of } from 'rxjs';
-import { ISessionTokenExpiry } from '@services/session-service/interfaces/session-token-expiry.interface';
-import { SESSION_TOKEN_EXPIRY_MOCK } from '@services/session-service/mocks/session-token-expiry.mock';
-
-const tokenExpiry: ISessionTokenExpiry = SESSION_TOKEN_EXPIRY_MOCK;
 
 describe('AppInitializerService', () => {
   let service: AppInitializerService;
@@ -37,21 +32,13 @@ describe('AppInitializerService', () => {
     expect(service['transferStateService'].initializeSsoEnabled).toHaveBeenCalled();
   });
 
-  it('should initialize the session timeout', async () => {
-    spyOn(service['sessionService'], 'getTokenExpiry').and.returnValue(of(tokenExpiry));
-    await service['initializeSessionTimeout']();
-    expect(service['sessionService'].getTokenExpiry).toHaveBeenCalled();
-  });
-
   it('should initialize the SSO enabled, LaunchDarkly, and session timeout', async () => {
     spyOn(service['transferStateService'], 'initializeLaunchDarklyConfig');
     spyOn(service['transferStateService'], 'initializeSsoEnabled');
-    spyOn(service['sessionService'], 'getTokenExpiry').and.returnValue(of(tokenExpiry));
 
     await service.initializeApp();
 
     expect(service['transferStateService'].initializeLaunchDarklyConfig).toHaveBeenCalled();
     expect(service['transferStateService'].initializeSsoEnabled).toHaveBeenCalled();
-    expect(service['sessionService'].getTokenExpiry).toHaveBeenCalled();
   });
 });
--- src/app/services/app-initializer-service/app-initializer.service.ts
+++ src/app/services/app-initializer-service/app-initializer.service.ts
@@ -1,14 +1,11 @@
 import { Injectable, inject } from '@angular/core';
-import { SessionService } from '@services/session-service/session.service';
 import { TransferStateService } from '@services/transfer-state-service/transfer-state.service';
-import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppInitializerService {
   private readonly transferStateService = inject(TransferStateService);
-  private readonly sessionService = inject(SessionService);
 
   /**
    * Initializes the SSO (Single Sign-On) enabled state.
@@ -26,22 +23,11 @@ export class AppInitializerService {
   }
 
   /**
-   * Initializes the session timeout.
-   *
-   * @returns A promise that resolves when the session timeout is initialized.
-   */
-  private async initializeSessionTimeout(): Promise<void> {
-    await firstValueFrom(this.sessionService.getTokenExpiry());
-    return Promise.resolve();
-  }
-
-  /**
    * Initializes the application.
    * This method calls the necessary initialization functions.
    */
-  public async initializeApp(): Promise<void[]> {
+  public initializeApp(): void {
     this.initializeSsoEnabled();
     this.initializeLaunchDarkly();
-    return Promise.all([this.initializeSessionTimeout()]);
   }
 }
