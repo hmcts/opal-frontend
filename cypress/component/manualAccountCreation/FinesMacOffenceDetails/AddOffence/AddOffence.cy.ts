81253bda7577e7b5ba4f64710d5de21a99405360
CadeFaulkner <cade.faulkner@hmcts.net>
2025-01-31 14:47:43 +0000

    refactor: enhance FinesMacOffenceDetails tests with proper service mocks and HTTP intercepts

--- cypress/component/manualAccountCreation/FinesMacOffenceDetails/AddOffence/AddOffence.cy.ts
+++ cypress/component/manualAccountCreation/FinesMacOffenceDetails/AddOffence/AddOffence.cy.ts
@@ -1,70 +1,96 @@
-// import { mount } from 'cypress/angular';
-// import { FinesMacOffenceDetailsAddAnOffenceComponent } from 'src/app/flows/fines/fines-mac/fines-mac-offence-details/fines-mac-offence-details-add-an-offence/fines-mac-offence-details-add-an-offence.component';
-// import { OpalFines } from '../../../../../src/app/flows/fines/services/opal-fines-service/opal-fines.service';
-// import { ActivatedRoute } from '@angular/router';
-// import { FINES_MAC_STATE_MOCK } from '../../../../../src/app/flows/fines/fines-mac/mocks/fines-mac-state.mock';
-// import { FinesService } from '@services/fines/fines-service/fines.service';
-// import { OPAL_FINES_MAJOR_CREDITOR_REF_DATA_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-major-creditor-ref-data.mock';
-// import { OPAL_FINES_MAJOR_CREDITOR_PRETTY_NAME_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-major-creditor-pretty-name.mock';
-// import { OPAL_FINES_OFFENCES_REF_DATA_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-offences-ref-data.mock';
-// import { OPAL_FINES_RESULTS_REF_DATA_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-results-ref-data.mock';
-// import { OPAL_FINES_RESULT_PRETTY_NAME_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-result-pretty-name.mock';
-// import { FinesMacOffenceDetailsService } from 'src/app/flows/fines/fines-mac/fines-mac-offence-details/services/fines-mac-offence-details-service/fines-mac-offence-details.service';
-// import { ADD_OFFENCE_OFFENCE_MOCK } from './mocks/add-offence-draft-state-mock';
-// import { of } from 'rxjs';
+import { mount } from 'cypress/angular';
+import { FinesMacOffenceDetailsAddAnOffenceComponent } from 'src/app/flows/fines/fines-mac/fines-mac-offence-details/fines-mac-offence-details-add-an-offence/fines-mac-offence-details-add-an-offence.component';
+import { OpalFines } from '../../../../../src/app/flows/fines/services/opal-fines-service/opal-fines.service';
+import { ActivatedRoute } from '@angular/router';
+import { FINES_MAC_STATE_MOCK } from '../../../../../src/app/flows/fines/fines-mac/mocks/fines-mac-state.mock';
+import { FinesService } from '@services/fines/fines-service/fines.service';
+import { OPAL_FINES_MAJOR_CREDITOR_REF_DATA_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-major-creditor-ref-data.mock';
+import { OPAL_FINES_MAJOR_CREDITOR_PRETTY_NAME_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-major-creditor-pretty-name.mock';
+import { OPAL_FINES_OFFENCES_REF_DATA_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-offences-ref-data.mock';
+import { OPAL_FINES_RESULTS_REF_DATA_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-results-ref-data.mock';
+import { OPAL_FINES_RESULT_PRETTY_NAME_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-result-pretty-name.mock';
+import { FinesMacOffenceDetailsService } from 'src/app/flows/fines/fines-mac/fines-mac-offence-details/services/fines-mac-offence-details-service/fines-mac-offence-details.service';
+import { ADD_OFFENCE_OFFENCE_MOCK } from './mocks/add-offence-draft-state-mock';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { DateService } from '@services/date-service/date.service';
+import { mock } from 'node:test';
 
-// describe('FinesMacLanguagePreferenceComponent', () => {
-//   let mockFinesService = {
-//     finesMacState: { ...FINES_MAC_STATE_MOCK },
-//   };
+describe('FinesMacLanguagePreferenceComponent', () => {
+  let mockFinesService = new FinesService(new DateService());
+  mockFinesService.finesMacState = { ...FINES_MAC_STATE_MOCK };
 
-//   afterEach(() => {
-//     cy.then(() => {
-//       mockFinesService.finesMacState.languagePreferences.formData = {
-//         fm_language_preferences_document_language: '',
-//         fm_language_preferences_hearing_language: '',
-//       };
-//     });
-//   });
+  let mockOffenceDetailsService = new FinesMacOffenceDetailsService();
+  Object.assign(mockOffenceDetailsService, {
+    offenceIndex: 0,
+    addedOffenceCode: '',
+    finesMacOffenceDetailsDraftState: ADD_OFFENCE_OFFENCE_MOCK,
+    offenceCodeMessage: '',
+  });
 
-//   const setupComponent = (formSubmit: any) => {
-//     const mockOffenceDetailsService = {
-//       finesMacOffenceDetailsDraftState: { ...ADD_OFFENCE_OFFENCE_MOCK },
-//     };
+  beforeEach(() => {
+    cy.intercept('GET', '**/opal-fines-service/results**', {
+      statusCode: 200,
+      body: OPAL_FINES_RESULTS_REF_DATA_MOCK,
+    });
+    cy.intercept('GET', '**/opal-fines-service/major-creditors**', {
+      statusCode: 200,
+      body: OPAL_FINES_MAJOR_CREDITOR_REF_DATA_MOCK,
+    });
+    cy.intercept(
+      {
+        method: 'GET',
+        pathname: '/opal-fines-service/offences',
+      },
+      (req) => {
+        const requestedCjsCode = req.query.q;
+        const matchedOffences = OPAL_FINES_OFFENCES_REF_DATA_MOCK.refData.filter(
+          (offence) => offence.get_cjs_code === requestedCjsCode,
+        );
+        req.reply({
+          count: matchedOffences.length,
+          refData: matchedOffences,
+        });
+      },
+    );
+  });
+  afterEach(() => {
+    cy.then(() => {
+      mockFinesService.finesMacState.languagePreferences.formData = {
+        fm_language_preferences_document_language: '',
+        fm_language_preferences_hearing_language: '',
+      };
+    });
+  });
 
-//     const mockOpalFinesService = {
-//       getResults: () => of(OPAL_FINES_RESULTS_REF_DATA_MOCK),
-//       getResultPrettyName: () => OPAL_FINES_RESULT_PRETTY_NAME_MOCK,
-//       getMajorCreditors: () => of(OPAL_FINES_MAJOR_CREDITOR_REF_DATA_MOCK),
-//       getMajorCreditorPrettyName: () => OPAL_FINES_MAJOR_CREDITOR_PRETTY_NAME_MOCK,
-//       getOffenceByCjsCode: () => of(OPAL_FINES_OFFENCES_REF_DATA_MOCK),
-//     };
+  const setupComponent = (formSubmit: any) => {
+    mount(FinesMacOffenceDetailsAddAnOffenceComponent, {
+      providers: [
+        provideHttpClient(),
+        OpalFines,
+        { provide: FinesMacOffenceDetailsService, useValue: mockOffenceDetailsService },
+        { provide: FinesService, useValue: mockFinesService },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            parent: {
+              snapshot: {
+                url: [{ path: 'manual-account-creation' }],
+              },
+            },
+          },
+        },
+      ],
+      componentProperties: {
+        handleOffenceDetailsSubmit: formSubmit,
+      },
+    });
+  };
 
-//     mount(FinesMacOffenceDetailsAddAnOffenceComponent, {
-//       providers: [
-//         { provide: OpalFines, useValue: mockOpalFinesService },
-//         { provide: FinesService, useValue: mockFinesService },
-//         { provide: FinesMacOffenceDetailsService, useValue: mockOffenceDetailsService },
-//         {
-//           provide: ActivatedRoute,
-//           useValue: {
-//             parent: {
-//               snapshot: {
-//                 url: [{ path: 'manual-account-creation' }],
-//               },
-//             },
-//           },
-//         },
-//       ],
-//       componentProperties: {
-//         handleOffenceDetailsSubmit: formSubmit,
-//       },
-//     });
-//   };
+  it('should render the component', () => {
+    setupComponent(null);
 
-//   it('should render the component', () => {
-//     setupComponent(null);
-
-//     // Verify the component is rendered
-//   });
-// });
+    cy.get('app-fines-mac-offence-details-add-an-offence-form').should('exist');
+    cy.get('#fm_offence_details_offence_cjs_code').type('AK123456', { delay: 0 });
+  });
+});
