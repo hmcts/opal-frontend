85d4101772d4271b44bbd3738b79ef52885fdeac
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-02-03 09:05:51 +0000

    fines mac add offence constants and mocks.

---
+++ cypress/component/manualAccountCreation/FinesMacOffenceDetails/AddOffence/constants/fines_mac_offence_details_elements.ts
@@ -0,0 +1,12 @@
+export const DOM_ELEMENTS = {
+  app: 'app-fines-mac-offence-details-add-an-offence',
+  pageTitle: 'h1.govuk-heading-l',
+  legend: 'legend.govuk-fieldset__legend.govuk-fieldset__legend--m',
+
+  //Inputs
+  dateOfSentenceInput: 'input[id = "fm_offence_details_date_of_sentence"]',
+  offenceCodeInput: 'input[id = "fm_offence_details_offence_cjs_code"]',
+  resultCode: 'input[id = "fm_offence_details_result_id_0-autocomplete"]',
+  amountImposedInput: 'input[id = "fm_offence_details_amount_imposed_0"]',
+  amountPaidInput: 'input[id = "fm_offence_details_amount_paid_0"]',
+};
