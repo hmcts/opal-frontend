03797e4853dc4b05c4275ae421afe577643991f5
CadeFaulkner <cade.faulkner@hmcts.net>
2025-02-19 15:21:50 +0000

    refactor: remove unnecessary console logs from component support file

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -2,8 +2,6 @@ import '@cypress/grep';
 
 beforeEach(function () {
   const test = this.currentTest;
-  console.log('test:   ', test);
-  console.log('tags:  ', test._testConfig.unverifiedTestConfig.tags);
   const tags = test?._testConfig.unverifiedTestConfig.tags;
 
   if (tags && tags.length > 0) {
