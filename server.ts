21882f0ef211c074d3faddddeea80f256215face
Frankie Moran <frankie.jack.moran@gmail.com>
2025-04-04 12:19:35 +0100

    Changes to work with the opal-frontend-common-node library

--- server.ts
+++ server.ts
@@ -13,26 +13,42 @@ global['document'] = win.document;
 global['self'] = win;
 
 import 'zone.js/node';
-
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr/node';
 import * as express from 'express';
 import { existsSync } from 'node:fs';
-
 import { Logger } from '@hmcts/nodejs-logging';
-
-import { AppInsights, HealthCheck, Helmet, PropertiesVolume } from './server/modules';
-import { SessionStorage } from './server/session/index';
-import { LaunchDarkly } from './server/launch-darkly/index';
-import Routes from './server/routes';
-import { CSRFToken } from './server/csrf-token';
 import config from 'config';
-import TransferServerState from './server/interfaces/transferServerState';
 import bootstrap from './src/main.server';
-import AppInsightsConfiguration from './server/modules/appinsights/app-insights-configuration';
+
+import { AppInsights } from '@hmcts/opal-frontend-common-node/app-insights';
+import { LaunchDarkly } from '@hmcts/opal-frontend-common-node/launch-darkly';
+import {
+  TransferServerState,
+  ExpiryConfiguration,
+  SessionStorageConfiguration,
+  RoutesConfiguration,
+} from '@hmcts/opal-frontend-common-node/interfaces';
+import { HealthCheck } from '@hmcts/opal-frontend-common-node/health';
+import { Helmet } from '@hmcts/opal-frontend-common-node/helmet';
+import { PropertiesVolume } from '@hmcts/opal-frontend-common-node/properties-volume';
+import { CSRFToken } from '@hmcts/opal-frontend-common-node/csrf-token';
+import { Routes } from '@hmcts/opal-frontend-common-node/routes';
+import SessionStorage from '@hmcts/opal-frontend-common-node/session/session-storage';
 
 const env = process.env['NODE_ENV'] || 'development';
 const developmentMode = env === 'development';
+
+const sessionStorageConfig: SessionStorageConfiguration = {
+  secret: config.get('secrets.opal.opal-frontend-cookie-secret'),
+  prefix: config.get('session.prefix'),
+  maxAge: config.get('session.maxAge'),
+  sameSite: config.get('session.sameSite'),
+  secure: config.get('session.secure'),
+  domain: config.get('session.domain'),
+  redisEnabled: config.get('features.redis.enabled'),
+  redisConnectionString: config.get('secrets.opal.redis-connection-string'),
+};
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -47,19 +63,49 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
-  new PropertiesVolume().enableFor(server);
-  new SessionStorage().enableFor(server);
-  new CSRFToken().enableFor(server);
-  new HealthCheck().enableFor(server);
+  new PropertiesVolume().enableFor(server, config);
+  new SessionStorage().enableFor(server, sessionStorageConfig);
+  new CSRFToken().enableFor(
+    server,
+    config.get('secrets.opal.opal-frontend-csrf-secret'),
+    config.get('csrf.cookieName'),
+    config.get('csrf.sameSite'),
+    config.get('csrf.secure'),
+  );
+  new HealthCheck().enableFor(server, 'opal-frontend');
   // secure the application by adding various HTTP headers to its responses
-  new Helmet(developmentMode).enableFor(server);
+  new Helmet(developmentMode).enableFor(server, config.get('features.helmet.enabled'));
 
-  new Routes().enableFor(server);
+  const testMode = config.get<boolean>('expiry.testMode');
+  const expiryConfigPath = testMode ? 'expiry.test' : 'expiry.default';
+  const sessionExpiryConfiguration: ExpiryConfiguration = {
+    testMode: config.get<boolean>('expiry.testMode'),
+    expiryTimeInMilliseconds: config.get<number>(`${expiryConfigPath}.expiryTimeInMilliseconds`),
+    warningThresholdInMilliseconds: config.get<number>(`${expiryConfigPath}.warningThresholdInMilliseconds`),
+  };
+  const env = process.env['NODE_ENV'] || 'development';
+  const routesConfiguration: RoutesConfiguration = {
+    opalApiTarget: config.get('opal-api.url'),
+    opalFinesServiceTarget: config.get('opal-api.opal-fines-service'),
+    frontendHostname:
+      env === 'development' ? config.get('frontend-hostname.dev') : config.get('frontend-hostname.prod'),
+    prefix: config.get('session.prefix'),
+  };
 
-  new AppInsights().enable();
+  new Routes().enableFor(server, config.get('features.sso.enabled'), sessionExpiryConfiguration, routesConfiguration);
 
-  const launchDarkly = new LaunchDarkly().enableFor();
-  const appInsights = new AppInsightsConfiguration().enableFor();
+  const launchDarkly = new LaunchDarkly().enableFor(
+    config.get('features.launch-darkly.enabled'),
+    config.get('features.launch-darkly.stream'),
+    config.has('secrets.opal.launch-darkly-client-id') ? config.get('features.launch-darkly.clientId') : null,
+  );
+  const appInsights = new AppInsights().enable(
+    config.get('features.app-insights.enabled'),
+    config.has('features.app-insights.connection-string')
+      ? config.get('features.app-insights.connection-string')
+      : null,
+    config.has('features.app-insights.cloudRoleName') ? config.get('features.app-insights.cloudRoleName') : null,
+  );
   const serverTransferState: TransferServerState = {
     launchDarklyConfig: launchDarkly,
     ssoEnabled: config.get('features.sso.enabled'),
--- server/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-import UserState from './userState';
-import SecurityToken from './securityToken';
-import launchDarklyConfig from './launchDarklyConfig';
-import appInsightsConfig from './appInsightsConfig';
-
-export { UserState, SecurityToken, launchDarklyConfig, appInsightsConfig };
