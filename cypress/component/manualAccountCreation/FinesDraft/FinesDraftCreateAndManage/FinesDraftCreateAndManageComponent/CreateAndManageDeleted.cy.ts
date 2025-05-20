f4e38b2f8345d132fe97dc2efad7469997d399ce
CadeFaulkner <cade.faulkner@hmcts.net>
2025-05-20 15:27:22 +0100

    Refactor tests for deleted accounts: improve pagination and sorting assertions

--- cypress/component/manualAccountCreation/FinesDraft/FinesDraftCreateAndManage/FinesDraftCreateAndManageComponent/CreateAndManageDeleted.cy.ts
+++ cypress/component/manualAccountCreation/FinesDraft/FinesDraftCreateAndManage/FinesDraftCreateAndManageComponent/CreateAndManageDeleted.cy.ts
@@ -21,6 +21,8 @@ import {
 import { OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK } from './mocks/fines_draft_over_25_account_mock';
 
 describe('FinesDraftCheckAndManageDeletedComponent', () => {
+  const dateService = new DateService();
+
   const setupComponent = () => {
     cy.then(() => {
       mount(FinesDraftCheckAndManageTabsComponent, {
@@ -53,9 +55,7 @@ describe('FinesDraftCheckAndManageDeletedComponent', () => {
     interceptGetApprovedAccounts(200, noAccountsMockData);
 
     setupComponent();
-    cy.get(DOM_ELEMENTS.navigationLinks)
-      .contains('' + NAVIGATION_LINKS[3])
-      .click();
+    cy.get(DOM_ELEMENTS.navigationLinks).contains(NAVIGATION_LINKS[3]).click();
     cy.get(DOM_ELEMENTS.heading).should('exist').and('contain', 'Create accounts');
 
     for (const link of NAVIGATION_LINKS) {
@@ -88,14 +88,14 @@ describe('FinesDraftCheckAndManageDeletedComponent', () => {
         cy.get(DOM_ELEMENTS.businessUnit).contains('Business Unit B');
       });
   });
-  it.only(
+  it(
     '(AC.5)should have pagination enabled for over 25 draft accounts for deleted accounts',
     { tags: ['@PO-609'] },
     () => {
-      const deletedMockData = { count: 0, summaries: [] };
-      const inReviewMockData = { count: 0, summaries: OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK.summaries };
+      const rejectedMockData = { count: 0, summaries: [] };
+      const deletedMockData = { count: 0, summaries: OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK.summaries };
+      interceptGetRejectedAccounts(200, rejectedMockData);
       interceptGetDeletedAccounts(200, deletedMockData);
-      interceptGetInReviewAccounts(200, inReviewMockData);
 
       setupComponent();
       cy.get(DOM_ELEMENTS.navigationLinks).contains('Deleted').click();
@@ -117,52 +117,50 @@ describe('FinesDraftCheckAndManageDeletedComponent', () => {
         });
     },
   );
-  //       const deletedMockData = { count: 0, summaries: OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK.summaries };
-  //       interceptGetDeletedAccounts(200, deletedMockData);
 
-  //       setupComponent();
-
-  //       cy.get(DOM_ELEMENTS.navigationLinks)
-  //         .contains('' + NAVIGATION_LINKS[3])
-  //         .click();
-
-  //       cy.get('strong').contains('Showing 1 - 25 of 50 accounts').should('exist');
-  //       cy.get(DOM_ELEMENTS.paginationLinks).contains('1').should('exist');
-  //       cy.get(DOM_ELEMENTS.paginationLinks).contains('2').should('exist');
-  //       cy.get(DOM_ELEMENTS.paginationLinks).contains('Next').should('exist');
-  //       cy.get(DOM_ELEMENTS.defendant).contains('Robert Brown').should('exist');
-
-  //       cy.get(DOM_ELEMENTS.paginationLinks).contains('Next').click({ force: true });
-  //       cy.get(DOM_ELEMENTS.defendant).contains('Emma Gonzalez').should('exist');
-  //       cy.get(DOM_ELEMENTS.paginationLinks).contains('Previous').should('exist');
-
-  //       cy.get(DOM_ELEMENTS.defendant)
-  //         .its('length')
-  //         .then((count) => {
-  //           expect(count).to.be.eq(25);
-  //         });
-  //     },
-  //   );
   it(
     '(AC.5) should have default sort order for created accounts set to descending for Deleted',
     { tags: ['@PO-609'] },
     () => {
+      const rejectedMockData = { count: 0, summaries: [] };
+      const deletedMockData = { count: 2, summaries: OPAL_FINES_DRAFT_ACCOUNTS_MOCK.summaries };
+      interceptGetRejectedAccounts(200, rejectedMockData);
+      interceptGetDeletedAccounts(200, deletedMockData);
+
       setupComponent();
-      cy.get(DOM_ELEMENTS.navigationLinks)
-        .contains('' + NAVIGATION_LINKS[3])
-        .click();
-      cy.get(DOM_ELEMENTS.tableHeadings).contains('Created').should('exist');
-      cy.get(DOM_ELEMENTS.created)
-        .first()
-        .contains(
-          `${dateService.getDaysAgo(OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK.summaries[0].account_snapshot.created_date)}`,
-        );
-      cy.get(DOM_ELEMENTS.paginationLinks).contains('Next').click({ force: true });
-      cy.get(DOM_ELEMENTS.created)
-        .last()
-        .contains(
-          `${dateService.getDaysAgo(OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK.summaries[9].account_snapshot.created_date)}`,
-        );
+
+      cy.get(DOM_ELEMENTS.navigationLinks).contains('Deleted').click();
+
+      cy.get(DOM_ELEMENTS.tableHeadings).contains('th', 'Created').should('have.attr', 'aria-sort', 'ascending');
+      cy.get(DOM_ELEMENTS.tableRow)
+        .eq(0)
+        .within(() => {
+          cy.get(DOM_ELEMENTS.defendant).contains('SMITH, Jane');
+          cy.get(DOM_ELEMENTS.created).contains('4 days ago');
+        });
+      cy.get(DOM_ELEMENTS.tableRow)
+        .eq(1)
+        .within(() => {
+          cy.get(DOM_ELEMENTS.defendant).contains('DOE, John');
+          cy.get(DOM_ELEMENTS.created).contains('Today');
+        });
+
+      cy.get(DOM_ELEMENTS.tableHeadings).contains('Created').click();
+      cy.get(DOM_ELEMENTS.tableHeadings).contains('th', 'Created').should('have.attr', 'aria-sort', 'descending');
+
+      cy.get(DOM_ELEMENTS.tableRow)
+        .eq(0)
+        .within(() => {
+          cy.get(DOM_ELEMENTS.defendant).contains('DOE, John');
+          cy.get(DOM_ELEMENTS.created).contains('Today');
+        });
+
+      cy.get(DOM_ELEMENTS.tableRow)
+        .eq(1)
+        .within(() => {
+          cy.get(DOM_ELEMENTS.defendant).contains('SMITH, Jane');
+          cy.get(DOM_ELEMENTS.created).contains('4 days ago');
+        });
     },
   );
 });
