b66f62b2bab5a4f1cdfcbaa9a930bde9a32acb01
rajanigandra <Rajani Gandra>
2025-05-23 15:08:44 +0100

    PO-618 code added

---
+++ cypress/component/manualAccountCreation/FinesDraft/FinesDraftCreateAndManage/FinesDraftCreateAndManageComponent/FinesDraftViewAllRejected.cy.ts
@@ -0,0 +1,156 @@
+import { mount } from 'cypress/angular';
+import { provideRouter } from '@angular/router';
+import { FinesDraftCreateAndManageViewAllRejectedComponent } from 'src/app/flows/fines/fines-draft/fines-draft-create-and-manage/fines-draft-create-and-manage-view-all-rejected/fines-draft-create-and-manage-view-all-rejected.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OpalFines } from '@services/fines/opal-fines-service/opal-fines.service';
+import { FinesMacPayloadService } from 'src/app/flows/fines/fines-mac/services/fines-mac-payload/fines-mac-payload.service';
+import { GlobalStore } from '@hmcts/opal-frontend-common/stores/global';
+import { FinesDraftStore } from 'src/app/flows/fines/fines-draft/stores/fines-draft.store';
+import { DOM_ELEMENTS } from './constants/fines_draft_cam_inputter_elements';
+import { DRAFT_SESSION_USER_STATE_MOCK } from './mocks/fines-draft-session-mock';
+import { interceptGetAllRejectedAccounts } from './intercepts/fines-draft-intercepts';
+import { OPAL_FINES_DRAFT_ACCOUNTS_MOCK } from './mocks/fines-draft-account.mock';
+import { OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK } from './mocks/fines_draft_over_25_account_mock';
+//import { FinesDraftCheckAndManageTabsComponent } from 'src/app/flows/fines/fines-draft/fines-draft-create-and-manage/fines-draft-create-and-manage-tabs/fines-draft-create-and-manage-tabs.component';
+
+describe('FinesDraftCreateAndManageViewAllRejectedComponent', () => {
+  const setupComponent = () => {
+    mount(FinesDraftCreateAndManageViewAllRejectedComponent, {
+      providers: [
+        FinesDraftStore,
+        Router,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            parent: {
+              snapshot: {
+                url: ['check-and-manage'],
+                data: {
+                  allRejectedAccounts: OPAL_FINES_DRAFT_ACCOUNTS_MOCK,
+                },
+                fragment: 'rejected',
+              },
+            },
+          },
+        },
+      ],
+
+      componentProperties: {},
+    });
+  };
+
+  it('AC.2 Should show all the headings as per the design artifact', { tags: ['@PO-618'] }, () => {
+    const AllRejectedMockData = { count: 2, summaries: OPAL_FINES_DRAFT_ACCOUNTS_MOCK.summaries };
+    interceptGetAllRejectedAccounts(200, AllRejectedMockData);
+
+    setupComponent();
+    cy.get(DOM_ELEMENTS.heading).should('exist').and('contain', 'All rejected accounts');
+  });
+
+  it('AC.3 verify the table of headers in review tab', { tags: ['@PO-618'] }, () => {
+    const toReviewMockData = { count: 0, summaries: OPAL_FINES_DRAFT_ACCOUNTS_MOCK.summaries };
+
+    interceptGetAllRejectedAccounts(200, toReviewMockData);
+
+    setupComponent();
+
+    cy.get(DOM_ELEMENTS.navigationLinks).contains('To review').click();
+
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('Defendant').should('exist');
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('Date of birth').should('exist');
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('Created').should('exist');
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('Account type').should('exist');
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('Business unit').should('exist');
+
+    //Check table row data in row 1
+    cy.get(DOM_ELEMENTS.tableRow)
+      .eq(0)
+      .within(() => {
+        cy.get(DOM_ELEMENTS.defendant).contains('SMITH, Jane');
+        cy.get(DOM_ELEMENTS.dob).contains('—');
+        cy.get(DOM_ELEMENTS.created).contains('4 days ago');
+        cy.get(DOM_ELEMENTS.accountType).contains('Fixed Penalty');
+        cy.get(DOM_ELEMENTS.businessUnit).contains('Business Unit B');
+      });
+
+    //Check table row data in row 2
+    cy.get(DOM_ELEMENTS.tableRow)
+      .eq(1)
+      .within(() => {
+        cy.get(DOM_ELEMENTS.defendant).contains('DOE, John');
+        cy.get(DOM_ELEMENTS.dob).contains('15 May 1990');
+        cy.get(DOM_ELEMENTS.created).contains('Today');
+        cy.get(DOM_ELEMENTS.accountType).contains('Fine');
+        cy.get(DOM_ELEMENTS.businessUnit).contains('Business Unit A');
+      });
+  });
+  it('(AC.4a) The table should have the correct default ordering', { tags: ['@PO-618'] }, () => {
+    const rejectedMockData = { count: 0, summaries: [] };
+    const toReviewMockData = { count: 0, summaries: OPAL_FINES_DRAFT_ACCOUNTS_MOCK.summaries };
+    interceptGetAllRejectedAccounts(200, rejectedMockData);
+
+    setupComponent();
+
+    cy.get(DOM_ELEMENTS.navigationLinks).contains('In review').click();
+
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('th', 'Created').should('have.attr', 'aria-sort', 'ascending');
+    cy.get(DOM_ELEMENTS.tableRow)
+      .eq(0)
+      .within(() => {
+        cy.get(DOM_ELEMENTS.defendant).contains('SMITH, Jane');
+        cy.get(DOM_ELEMENTS.created).contains('4 days ago');
+      });
+    cy.get(DOM_ELEMENTS.tableRow)
+      .eq(1)
+      .within(() => {
+        cy.get(DOM_ELEMENTS.defendant).contains('DOE, John');
+        cy.get(DOM_ELEMENTS.created).contains('Today');
+      });
+
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('Created').click();
+    cy.get(DOM_ELEMENTS.tableHeadings).contains('th', 'Created').should('have.attr', 'aria-sort', 'descending');
+
+    cy.get(DOM_ELEMENTS.tableRow)
+      .eq(0)
+      .within(() => {
+        cy.get(DOM_ELEMENTS.defendant).contains('DOE, John');
+        cy.get(DOM_ELEMENTS.created).contains('Today');
+      });
+
+    cy.get(DOM_ELEMENTS.tableRow)
+      .eq(1)
+      .within(() => {
+        cy.get(DOM_ELEMENTS.defendant).contains('SMITH, Jane');
+        cy.get(DOM_ELEMENTS.created).contains('4 days ago');
+      });
+  });
+
+  it(
+    '(AC.4b)should have pagination enabled for over 25 draft accounts for In Review accounts',
+    { tags: ['@PO-618'] },
+    () => {
+      const rejectedMockData = { count: 0, summaries: [] };
+      const toReviewMockData = { count: 0, summaries: OPAL_FINES_OVER_25_DRAFT_ACCOUNTS_MOCK.summaries };
+      interceptGetAllRejectedAccounts(200, rejectedMockData);
+
+      setupComponent();
+      cy.get(DOM_ELEMENTS.navigationLinks).contains('In review').click();
+
+      cy.get('strong').contains('Showing 1 - 25 of 50 accounts').should('exist');
+      cy.get(DOM_ELEMENTS.paginationLinks).contains('1').should('exist');
+      cy.get(DOM_ELEMENTS.paginationLinks).contains('2').should('exist');
+      cy.get(DOM_ELEMENTS.paginationLinks).contains('Next').should('exist');
+      cy.get(DOM_ELEMENTS.defendant).contains('Robert Brown').should('exist');
+
+      cy.get(DOM_ELEMENTS.paginationLinks).contains('Next').click({ force: true });
+      cy.get(DOM_ELEMENTS.defendant).contains('Emma Gonzalez').should('exist');
+      cy.get(DOM_ELEMENTS.paginationLinks).contains('Previous').should('exist');
+
+      cy.get(DOM_ELEMENTS.defendant)
+        .its('length')
+        .then((count) => {
+          expect(count).to.be.eq(25);
+        });
+    },
+  );
+});
