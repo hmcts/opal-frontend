c311eef1c07cdd7a69a73bdcd1a3d92534e8f684
Frankie Moran <frankie.jack.moran@gmail.com>
2025-05-30 11:32:03 +0100

    Linting

--- cypress/component/manualAccountCreation/FinesDraft/FinesDraftCheckAndValidate/FinesDraftCheckAndValidate/constants/fines_draft_cam_checker_tableConstants.ts
+++ cypress/component/manualAccountCreation/FinesDraft/FinesDraftCheckAndValidate/FinesDraftCheckAndValidate/constants/fines_draft_cam_checker_tableConstants.ts
@@ -1,2 +1,9 @@
 export const NAVIGATION_LINKS = ['To review', 'Rejected', 'Deleted'];
-export const TABLE_HEADINGS = ['Defendant', 'Date of birth', 'Created', 'Account type', 'Business unit', 'Submitted by'];
+export const TABLE_HEADINGS = [
+  'Defendant',
+  'Date of birth',
+  'Created',
+  'Account type',
+  'Business unit',
+  'Submitted by',
+];
