e0a79a04f6d3d59d6559495b7239daeb61c9c509
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-06-10 17:10:33 +0100

    Lint fix

--- src/app/flows/fines/fines-mac/fines-mac-offence-details/fines-mac-offence-details-add-an-offence/fines-mac-offence-details-add-an-offence-form/fines-mac-offence-details-add-an-offence-form.component.ts
+++ src/app/flows/fines/fines-mac/fines-mac-offence-details/fines-mac-offence-details-add-an-offence/fines-mac-offence-details-add-an-offence-form/fines-mac-offence-details-add-an-offence-form.component.ts
@@ -29,9 +29,9 @@ import { FINES_MAC_OFFENCE_DETAILS_RESULTS_CODES } from '../../constants/fines-m
 import { MojDatePickerComponent } from '@hmcts/opal-frontend-common/components/moj/moj-date-picker';
 import { MojTicketPanelComponent } from '@hmcts/opal-frontend-common/components/moj/moj-ticket-panel';
 import { MojAlertComponent } from '@hmcts/opal-frontend-common/components/moj/moj-alert';
-import { MojAlertContentComponent} from '@hmcts/opal-frontend-common/components/moj/moj-alert';
-import { MojAlertTextComponent} from '@hmcts/opal-frontend-common/components/moj/moj-alert';
-import { MojAlertIconComponent} from '@hmcts/opal-frontend-common/components/moj/moj-alert';
+import { MojAlertContentComponent } from '@hmcts/opal-frontend-common/components/moj/moj-alert';
+import { MojAlertTextComponent } from '@hmcts/opal-frontend-common/components/moj/moj-alert';
+import { MojAlertIconComponent } from '@hmcts/opal-frontend-common/components/moj/moj-alert';
 import { CommonModule } from '@angular/common';
 import { FINES_MAC_OFFENCE_DETAILS_STATE } from '../../constants/fines-mac-offence-details-state.constant';
 import { FINES_ROUTING_PATHS } from '@routing/fines/constants/fines-routing-paths.constant';
