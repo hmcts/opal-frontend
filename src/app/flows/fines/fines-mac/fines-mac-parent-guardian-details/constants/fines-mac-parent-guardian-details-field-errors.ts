336259b7d9b10bb66ef40e1867e6bcded203588b
Frankie Moran <frankie.jack.moran@gmail.com>
2025-06-17 09:57:20 +0100

    First lot of changes to error messages

--- src/app/flows/fines/fines-mac/fines-mac-parent-guardian-details/constants/fines-mac-parent-guardian-details-field-errors.ts
+++ src/app/flows/fines/fines-mac/fines-mac-parent-guardian-details/constants/fines-mac-parent-guardian-details-field-errors.ts
@@ -31,141 +31,141 @@ export const FINES_MAC_PARENT_GUARDIAN_DETAILS_FIELD_ERRORS: IFinesMacParentGuar
   },
   fm_parent_guardian_details_alias_forenames_0: {
     required: {
-      message: `Enter first name(s) for alias 1`,
+      message: `Enter alias 1 first name(s)`,
       priority: 1,
     },
     maxlength: {
-      message: `The first name(s) must be 20 characters or fewer for alias 1`,
+      message: `Alias 1 first name(s) must be 20 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The first name(s) must only contain alphabetical text for alias 1`,
+      message: `Alias 1 first name(s) must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_surname_0: {
     required: {
-      message: `Enter last name for alias 1`,
+      message: `Enter alias 1 last name`,
       priority: 1,
     },
     maxlength: {
-      message: `The last name must be 30 characters or fewer for alias 1`,
+      message: `alias 1 last name must be 30 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The last name must only contain alphabetical text for alias 1`,
+      message: `alias 1 last name must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_forenames_1: {
     required: {
-      message: `Enter first name(s) for alias 2`,
+      message: `Enter alias 2 first name(s)`,
       priority: 1,
     },
     maxlength: {
-      message: `The first name(s) must be 20 characters or fewer for alias 2`,
+      message: `Alias 2 first name(s) must be 20 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The first name(s) must only contain alphabetical text for alias 2`,
+      message: `Alias 2 first name(s) must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_surname_1: {
     required: {
-      message: `Enter last name for alias 2`,
+      message: `Enter alias 2 last name`,
       priority: 1,
     },
     maxlength: {
-      message: `The last name must be 30 characters or fewer for alias 2`,
+      message: `Alias 2 last name must be 30 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The last name must only contain alphabetical text for alias 2`,
+      message: `Alias 2 last name must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_forenames_2: {
     required: {
-      message: `Enter first name(s) for alias 3`,
+      message: `Enter alias 3 first name(s)`,
       priority: 1,
     },
     maxlength: {
-      message: `The first name(s) must be 20 characters or fewer for alias 3`,
+      message: `Alias 3 first name(s) must be 20 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The first name(s) must only contain alphabetical text for alias 3`,
+      message: `Alias 3 first name(s) must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_surname_2: {
     required: {
-      message: `Enter last name for alias 3`,
+      message: `Enter alias 3 last name`,
       priority: 1,
     },
     maxlength: {
-      message: `The last name must be 30 characters or fewer for alias 3`,
+      message: `Alias 3 last name must be 30 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The last name must only contain alphabetical text for alias 3`,
+      message: `Alias 3 last name must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_forenames_3: {
     required: {
-      message: `Enter first name(s) for alias 4`,
+      message: `Enter alias 4 first name(s)`,
       priority: 1,
     },
     maxlength: {
-      message: `The first name(s) must be 20 characters or fewer for alias 4`,
+      message: `Alias 4 first name(s) must be 20 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The first name(s) must only contain alphabetical text for alias 4`,
+      message: `Alias 4first name(s) must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_surname_3: {
     required: {
-      message: `Enter last name for alias 4`,
+      message: `Enter alias 4 last name`,
       priority: 1,
     },
     maxlength: {
-      message: `The last name must be 30 characters or fewer for alias 4`,
+      message: `Alias 4 last name must be 30 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The last name must only contain alphabetical text for alias 4`,
+      message: `Alias 4 last name must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_forenames_4: {
     required: {
-      message: `Enter first name(s) for alias 5`,
+      message: `Enter alias 5 first name(s)`,
       priority: 1,
     },
     maxlength: {
-      message: `The first name(s) must be 20 characters or fewer for alias 5`,
+      message: `Alias 5 first name(s) must be 20 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The first name(s) must only contain alphabetical text for alias 5`,
+      message: `Alias 5 first name(s) must only contain letters`,
       priority: 3,
     },
   },
   fm_parent_guardian_details_alias_surname_4: {
     required: {
-      message: `Enter last name for alias 5`,
+      message: `Enter alias 5 last name`,
       priority: 1,
     },
     maxlength: {
-      message: `The last name must be 30 characters or fewer for alias 5`,
+      message: `Alias 5 last name must be 30 characters or fewer`,
       priority: 2,
     },
     alphabeticalTextPattern: {
-      message: `The last name must only contain alphabetical text for alias 5`,
+      message: `Alias 5 last name must only contain letters`,
       priority: 3,
     },
   },
@@ -195,49 +195,49 @@ export const FINES_MAC_PARENT_GUARDIAN_DETAILS_FIELD_ERRORS: IFinesMacParentGuar
       priority: 1,
     },
     maxlength: {
-      message: 'The address line 1 must be 25 characters or fewer',
+      message: 'Address line 1 must be 25 characters or fewer',
       priority: 2,
     },
     specialCharactersPattern: {
-      message: 'The address line 1 must not contain special characters',
+      message: 'Address line 1 must only contain letters or numbers',
       priority: 3,
     },
   },
   fm_parent_guardian_details_address_line_2: {
     maxlength: {
-      message: 'The address line 2 must be 25 characters or fewer',
+      message: 'Address line 2 must be 25 characters or fewer',
       priority: 1,
     },
     specialCharactersPattern: {
-      message: 'The address line 2 must not contain special characters',
+      message: 'Address line 2 must only contain letters or numbers',
       priority: 2,
     },
   },
   fm_parent_guardian_details_address_line_3: {
     maxlength: {
-      message: `The address line 3 must be 13 characters or fewer`,
+      message: `Address line 3 must be 13 characters or fewer`,
       priority: 1,
     },
     specialCharactersPattern: {
-      message: 'The address line 3 must not contain special characters',
+      message: 'Address line 3 must only contain letters or numbers',
       priority: 2,
     },
   },
   fm_parent_guardian_details_post_code: {
     maxlength: {
-      message: `The postcode must be 8 characters or fewer`,
+      message: `Postcode must be 8 characters or fewer`,
       priority: 1,
     },
   },
   fm_parent_guardian_details_vehicle_make: {
     maxlength: {
-      message: `The make of car must be 30 characters or fewer`,
+      message: `Make and model must be 30 characters or fewer`,
       priority: 1,
     },
   },
   fm_parent_guardian_details_vehicle_registration_mark: {
     maxlength: {
-      message: `The registration number must be 11 characters or fewer`,
+      message: `Registration number must be 11 characters or fewer`,
       priority: 1,
     },
   },
