3b605a6daa4482ae159e7ce9d799829262b1a89f
stephen.magill <stephen.magill@cgi.com>
2025-07-09 16:50:53 +0100

    g

---
+++ cypress/component/fineAccountEnquiry/searchAndMatches/SearchAndMatchesCompany.cy.ts
@@ -0,0 +1,150 @@
+import { mount } from 'cypress/angular';
+import { FinesSaSearchAccountComponent } from '../../../../src/app/flows/fines/fines-sa/fines-sa-search/fines-sa-search-account/fines-sa-search-account.component';
+import { FinesSaStore } from '../../../../src/app/flows/fines/fines-sa/stores/fines-sa.store';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { DOM_ELEMENTS } from './constants/search_and_matches_company_elements';
+import { COMPANY_SEARCH_STATE_MOCK } from './mocks/search_and_matches_company_mock';
+import { delay } from 'cypress/types/bluebird';
+
+describe('Search Account Component - Company', () => {
+  let companySearchMock = structuredClone(COMPANY_SEARCH_STATE_MOCK);
+
+  const setupComponent = (formSubmit: any = null) => {
+    mount(FinesSaSearchAccountComponent, {
+      providers: [
+        provideHttpClient(),
+        {
+          provide: FinesSaStore,
+          useFactory: () => {
+            const store = new FinesSaStore();
+            store.setSearchAccount(companySearchMock);
+
+            return store;
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            fragment: of('companies'),
+            parent: {
+              snapshot: {
+                url: [{ path: 'search' }],
+              },
+            },
+          },
+        },
+      ],
+      componentProperties: {
+        handleSearchAccountSubmit: formSubmit,
+      },
+    });
+  };
+  beforeEach(() => {
+    companySearchMock = structuredClone(COMPANY_SEARCH_STATE_MOCK);
+  });
+
+  it.only('AC1a-b. should render the search for an account screen and companies tab', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+    companySearchMock.fsa_search_account_companies_search_criteria!.fsa_search_account_company_company_name='test';
+
+
+    cy.get(DOM_ELEMENTS.companiesTab).click();
+    cy.get(DOM_ELEMENTS.app).should('exist');
+    cy.get(DOM_ELEMENTS.heading).should('contain', 'Search for an account');
+    cy.get(DOM_ELEMENTS.tabs).should('exist');
+    cy.get(DOM_ELEMENTS.tabsList).should('exist');
+    cy.get(DOM_ELEMENTS.individualsTab).should('exist');
+    cy.get(DOM_ELEMENTS.companiesTab).should('exist');
+    cy.get(DOM_ELEMENTS.minorCreditorsTab).should('exist');
+    cy.get(DOM_ELEMENTS.majorCreditorsTab).should('exist');
+    cy.get(DOM_ELEMENTS.companiesPanel).should('exist');
+    cy.get(DOM_ELEMENTS.companiesHeading).should('contain', 'Companies');
+    cy.get(DOM_ELEMENTS.companyNameLabel).should('exist').contains('Company name');
+    cy.get(DOM_ELEMENTS.companyNameInput).should('exist');
+    cy.get(DOM_ELEMENTS.companyNameExactMatchCheckbox).should('exist').and('not.be.checked');
+    cy.get(DOM_ELEMENTS.includeAliasCheckbox).should('exist').and('not.be.checked');
+    cy.get(DOM_ELEMENTS.addressLine1Label).should('exist').contains('Address line 1');
+    cy.get(DOM_ELEMENTS.addressLine1Input).should('exist');
+    cy.get(DOM_ELEMENTS.postcodeLabel).should('exist').and('contain', 'Postcode');
+    cy.get(DOM_ELEMENTS.postcodeInput).should('exist');
+    cy.get(DOM_ELEMENTS.accountNumberLabel).should('exist').and('contain', 'Account number');
+    cy.get(DOM_ELEMENTS.referenceNumberLabel).should('exist').and('contain', 'Reference or case number');
+    cy.get(DOM_ELEMENTS.referenceNumberInput).should('exist');
+    cy.get(DOM_ELEMENTS.activeAccountsOnlyCheckbox).should('be.checked');
+    cy.get(DOM_ELEMENTS.searchButton).should('exist').and('contain', 'Search');
+  });
+
+  it('AC3a. should show error for non-alphabetical company name', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+    
+cy.get(DOM_ELEMENTS.companyNameInput).type('Company123');
+cy.get(DOM_ELEMENTS.searchButton).click();
+
+cy.get(DOM_ELEMENTS.errorSummary).should('contain', 'Company name must only include letters a to z');
+cy.get(DOM_ELEMENTS.companyNameError).should('contain', 'Company name must only include letters a to z');
+
+cy.get(DOM_ELEMENTS.companyNameInput).clear();
+  });
+
+  it('AC3b. should show error for non-alphabetical address line 1', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+
+cy.get(DOM_ELEMENTS.addressLine1Input).type('Address123?');
+cy.get(DOM_ELEMENTS.searchButton).click();
+
+cy.get(DOM_ELEMENTS.errorSummary).should('contain', 'Address line 1 must only include letters a to z, numbers, hyphens, spaces and apostrophes');
+cy.get(DOM_ELEMENTS.addressLine1Error).should('contain', 'Address line 1 must only include letters a to z, numbers, hyphens, spaces and apostrophes');
+
+cy.get(DOM_ELEMENTS.addressLine1Input).clear();
+  });
+
+  it('AC3c. should show error for non-alphabetical post code', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+    
+cy.get(DOM_ELEMENTS.postcodeInput).type('POSTCODE?');
+cy.get(DOM_ELEMENTS.searchButton).click();
+
+cy.get(DOM_ELEMENTS.errorSummary).should('contain', 'Post code must only include letters a to z, numbers, hyphens, spaces and apostrophes');
+cy.get(DOM_ELEMENTS.postcodeError).should('contain', 'Post code must only include letters a to z, numbers, hyphens, spaces and apostrophes');
+
+cy.get(DOM_ELEMENTS.postcodeInput).clear();
+  });
+
+  it('AC4a. should validate company name maximum field length', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+
+cy.get(DOM_ELEMENTS.companyNameInput).type('abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijs');
+cy.get(DOM_ELEMENTS.searchButton).click();
+
+cy.get(DOM_ELEMENTS.errorSummary).should('contain', 'Company name must be 50 characters or fewer');
+cy.get(DOM_ELEMENTS.companyNameError).should('contain', 'Company name must be 50 characters or fewer');
+
+cy.get(DOM_ELEMENTS.companyNameInput).clear();
+  });
+
+  it('AC4b. should validate address line 1 maximum field length', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+
+cy.get(DOM_ELEMENTS.addressLine1Input).type('Address1234Address1234Address12345');
+cy.get(DOM_ELEMENTS.searchButton).click();
+
+cy.get(DOM_ELEMENTS.errorSummary).should('contain', 'Address line 1 must be 30 characters or fewer');
+cy.get(DOM_ELEMENTS.addressLine1Error).should('contain', 'Address line 1 must be 30 characters or fewer');
+
+cy.get(DOM_ELEMENTS.addressLine1Input).clear();
+  });
+
+  it('AC4c. hould validate post code maximum field length', { tags: ['PO-712'] }, () => {
+    setupComponent(null);
+    
+cy.get(DOM_ELEMENTS.postcodeInput).type('POSTCODE?');
+cy.get(DOM_ELEMENTS.searchButton).click();
+
+cy.get(DOM_ELEMENTS.errorSummary).should('contain', 'Post code must only include letters a to z, numbers, hyphens, spaces and apostrophes');
+cy.get(DOM_ELEMENTS.postcodeError).should('contain', 'Post code must only include letters a to z, numbers, hyphens, spaces and apostrophes');
+
+cy.get(DOM_ELEMENTS.postcodeInput).clear();
+  });
+});
---
+++ cypress/component/fineAccountEnquiry/searchAndMatches/constants/search_and_matches_company_elements.ts
@@ -0,0 +1,50 @@
+export const DOM_ELEMENTS = {
+  app: 'app-fines-sa-search-account-form',
+  heading: '.govuk-heading-l',
+  tabs: 'opal-lib-govuk-tabs',
+  tabsList: '.govuk-tabs__list',
+  individualsTab: '[id="tab-individuals"]',
+  companiesTab: '[id="tab-companies"]',
+  minorCreditorsTab: '[id="tab-minor-creditors"]',
+  majorCreditorsTab: '[id="tab-major-creditors"]',
+  companiesPanel: '[id="companies"]',
+  companiesHeading: '[id="companies"] .govuk-heading-l',
+  searchButton: '[buttonId="submitForm"]',
+
+  // Filter by business unit section
+  businessUnitSummaryList: '[summaryListId="accountDetails"]',
+  businessUnitLink: '[summaryListRowId="languagePreferences"] a',
+  businessUnitBadge: '.business-unit-badge',
+
+  // Account number field
+  accountNumberLabel: '[for="fsa_search_account_number"]',
+  accountNumberInput: '#fsa_search_account_number',
+  accountNumberError: '#fsa_search_account_number-error-message',
+
+  // Reference or case number field
+  referenceNumberLabel: '[for="fsa_search_account_reference_case_number"]',
+  referenceNumberInput: '#fsa_search_account_reference_case_number',
+  referenceNumberError: '#fsa_search_account_reference_case_number-error-message',
+
+  // Company tab fields
+  companyNameLabel: '[for="fsa_search_account_companies_company_name"]',
+  companyNameInput: '#fsa_search_account_companies_company_name',
+  companyNameExactMatchCheckbox: '#fsa_search_account_companies_company_name_exact_match',
+  companyNameError: '#fsa_search_account_companies_company_name-error-message',
+
+  includeAliasCheckbox: '#fsa_search_account_companies_include_aliases',
+
+  addressLine1Label: '[for="fsa_search_account_companies_address_line_1"]',
+  addressLine1Input: '#fsa_search_account_companies_address_line_1',
+  addressLine1Error: '#fsa_search_account_companies_address_line_1-error-message',
+
+  postcodeLabel: '[for="fsa_search_account_companies_post_code"]',
+  postcodeInput: '#fsa_search_account_companies_post_code',
+  postcodeError: '#fsa_search_account_companies_post_code-error-message',
+
+  // Active accounts only checkbox   
+  activeAccountsOnlyCheckbox: '#fsa_search_account_active_accounts_only',
+
+  // Generic error message
+  errorSummary: '.govuk-error-summary',
+};
