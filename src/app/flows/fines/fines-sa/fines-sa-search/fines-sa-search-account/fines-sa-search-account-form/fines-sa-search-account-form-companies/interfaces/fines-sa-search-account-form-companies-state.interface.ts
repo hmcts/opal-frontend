cc80719882f13a99bb5d4b6e297e3886d9d6a7bb
Frankie Moran <frankie.jack.moran@gmail.com>
2025-07-10 09:10:44 +0100

    Update interface and remove unused constant

--- src/app/flows/fines/fines-sa/fines-sa-search/fines-sa-search-account/fines-sa-search-account-form/fines-sa-search-account-form-companies/interfaces/fines-sa-search-account-form-companies-state.interface.ts
+++ src/app/flows/fines/fines-sa/fines-sa-search/fines-sa-search-account/fines-sa-search-account-form/fines-sa-search-account-form-companies/interfaces/fines-sa-search-account-form-companies-state.interface.ts
@@ -1,7 +1,7 @@
 export interface IFinesSaSearchAccountFormCompaniesState {
-  fsa_search_account_company_company_name: string | null;
-  fsa_search_account_company_company_name_exact_match: boolean;
-  fsa_search_account_company_include_aliases: boolean;
-  fsa_search_account_company_address_line_1: string | null;
-  fsa_search_account_company_post_code: string | null;
+  fsa_search_account_companies_company_name: string | null;
+  fsa_search_account_companies_company_name_exact_match: boolean;
+  fsa_search_account_companies_include_aliases: boolean;
+  fsa_search_account_companies_address_line_1: string | null;
+  fsa_search_account_companies_post_code: string | null;
 }
