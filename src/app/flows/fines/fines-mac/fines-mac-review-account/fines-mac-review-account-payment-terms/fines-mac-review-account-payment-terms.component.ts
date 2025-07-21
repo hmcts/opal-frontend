471de7bdc64a3dc706570fc06204b77e8cf70ca4
Frankie Moran <frankie.jack.moran@gmail.com>
2025-07-21 10:57:35 +0100

    Update the permission service usage (#1597)

--- src/app/flows/fines/fines-mac/fines-mac-review-account/fines-mac-review-account-payment-terms/fines-mac-review-account-payment-terms.component.ts
+++ src/app/flows/fines/fines-mac/fines-mac-review-account/fines-mac-review-account-payment-terms/fines-mac-review-account-payment-terms.component.ts
@@ -40,7 +40,7 @@ import { FINES_MAC_DEFENDANT_TYPES_KEYS } from '../../constants/fines-mac-defend
 export class FinesMacReviewAccountPaymentTermsComponent implements OnInit {
   private readonly globalStore = inject(GlobalStore);
   private readonly dateService = inject(DateService);
-  private readonly hasPermissionAccess = inject(PermissionsService).hasPermissionAccess;
+  private readonly hasBusinessUnitPermissionAccess = inject(PermissionsService).hasBusinessUnitPermissionAccess;
   private userStateRoles: ISessionUserStateRole[] = [];
   private readonly frequencyOptions = FINES_MAC_PAYMENT_TERMS_FREQUENCY_OPTIONS;
 
@@ -81,7 +81,7 @@ export class FinesMacReviewAccountPaymentTermsComponent implements OnInit {
     this.userStateRoles = this.globalStore.userState()?.business_unit_user || [];
     const { business_unit_id: businessUnitId } = this.businessUnit;
     if (this.userStateRoles && this.userStateRoles.length > 0) {
-      this.permissions[this.permissionsMap.collectionOrder] = this.hasPermissionAccess(
+      this.permissions[this.permissionsMap.collectionOrder] = this.hasBusinessUnitPermissionAccess(
         this.permissionsMap.collectionOrder,
         businessUnitId,
         this.userStateRoles,
