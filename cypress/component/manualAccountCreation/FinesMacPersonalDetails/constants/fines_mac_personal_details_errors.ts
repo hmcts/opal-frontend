1ef6ec3a1116b8500dec3a44d69485c0840ea295
Alfred-Gillingham2 <Alfred.Gillingham@HMCTS.NET>
2025-07-24 10:55:36 +0100

    WIP - Changes to tests for new wording, waiting on 1922 to finish

--- cypress/component/manualAccountCreation/FinesMacPersonalDetails/constants/fines_mac_personal_details_errors.ts
+++ cypress/component/manualAccountCreation/FinesMacPersonalDetails/constants/fines_mac_personal_details_errors.ts
@@ -1,9 +1,9 @@
 export const FORMAT_CHECK: { [key: string]: string } = {
   dateOfBirthInFuture: 'Enter a valid date of birth in the past',
   dateOfBirthInvalid: 'Enter date of birth in the format DD/MM/YYYY',
-  addressLine1ContainsSpecialCharacters: 'The address line 1 must not contain special characters',
-  addressLine2ContainsSpecialCharacters: 'The address line 2 must not contain special characters',
-  addressLine3ContainsSpecialCharacters: 'The address line 3 must not contain special characters',
+  addressLine1ContainsSpecialCharacters: 'Address line 1 must only contain letters or numbers',
+  addressLine2ContainsSpecialCharacters: 'Address line 2 must only contain letters or numbers',
+  addressLine3ContainsSpecialCharacters: 'Address line 3 must only contain letters or numbers',
   validNationalInsuranceNumber: 'Enter a National Insurance number in the format AANNNNNNA',
 };
 
@@ -15,26 +15,34 @@ export const MAIN_PERSONAL_DETAILS = {
 };
 
 export const ALIAS_PERSONAL_DETAILS = {
-  missingAliasFirstName: 'Enter first name(s) for alias',
-  missingAliasLastName: 'Enter last name for alias',
+  missingAliasFirstNameOne: 'Enter alias 1 first name(s)',
+  missingAliasFirstNameTwo: 'Enter alias 2 first name(s)',
+  missingAliasFirstNameThree: 'Enter alias 3 first name(s)',
+  missingAliasFirstNameFour: 'Enter alias 4 first name(s)',
+  missingAliasFirstNameFive: 'Enter alias 5 first name(s)',
+  missingAliasLastNameOne: 'Enter alias 1 last name',
+  missingAliasLastNameTwo: 'Enter alias 2 last name',
+  missingAliasLastNameThree: 'Enter alias 3 last name',
+  missingAliasLastNameFour: 'Enter alias 4 last name',
+  missingAliasLastNameFive: 'Enter alias 5 last name',
 };
 
 export const LENGTH_VALIDATION = {
-  firstNameTooLong: "The defendant's first name(s) must be 20 characters or fewer",
-  lastNameTooLong: "The defendant's last name must be 30 characters or fewer",
-  addressLine1TooLong: 'The address line 1 must be 30 characters or fewer',
-  addressLine2TooLong: 'The address line 2 must be 30 characters or fewer',
-  addressLine3TooLong: 'The address line 3 must be 16 characters or fewer',
+  firstNameTooLong: "Defendant's first name(s) must be 20 characters or fewer",
+  lastNameTooLong: "Defendant's last name must be 30 characters or fewer",
+  addressLine1TooLong: 'Address line 1 must be 30 characters or fewer',
+  addressLine2TooLong: 'Address line 2 must be 30 characters or fewer',
+  addressLine3TooLong: 'Address line 3 must be 16 characters or fewer',
 };
 
 export const CORRECTION_TEST_MESSAGES = {
   missingTitle: 'Select a title',
-  firstNameTooLong: "The defendant's first name(s) must be 20 characters or fewer",
-  lastNameTooLong: "The defendant's last name must be 30 characters or fewer",
-  addressLine1ContainsSpecialCharacters: 'The address line 1 must not contain special characters',
+  firstNameTooLong: "Defendant's first name(s) must be 20 characters or fewer",
+  lastNameTooLong: "Defendant's last name must be 30 characters or fewer",
+  addressLine1ContainsSpecialCharacters: 'Address line 1 must only contain letters or numbers',
 };
 
 export const VEHICLE_DETAILS_ERRORS = {
-  vehicleMake: 'The make of car must be 30 characters or fewer',
-  vehicleRegistration: 'The registration number must be 11 characters or fewer',
+  vehicleMake: 'Make and model must be 30 characters or fewer',
+  vehicleRegistration: 'Registration number must be 11 characters or fewer',
 };
