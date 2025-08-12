901e40328247ce5b208656c5629fd38ce9f521cb
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-08-12 15:50:45 +0100

    PO-1922 (#1607)
    Co-authored-by: Alfred-Gillingham2 <135228412+Alfred-Gillingham2@users.noreply.github.com>
    Co-authored-by: Alfred-Gillingham2 <Alfred.Gillingham@HMCTS.NET>
    Co-authored-by: Frankie Moran <frankie.jack.moran@gmail.com>
    Co-authored-by: Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>

--- src/app/flows/fines/fines-mac/fines-mac-personal-details/fines-mac-personal-details-form/fines-mac-personal-details-form.component.ts
+++ src/app/flows/fines/fines-mac/fines-mac-personal-details/fines-mac-personal-details-form/fines-mac-personal-details-form.component.ts
@@ -33,17 +33,22 @@ import {
 import { GovukErrorSummaryComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-error-summary';
 
 import { DateService } from '@hmcts/opal-frontend-common/services/date-service';
-import { alphabeticalTextValidator } from '@hmcts/opal-frontend-common/validators/alphabetical-text';
 import { dateOfBirthValidator } from '@hmcts/opal-frontend-common/validators/date-of-birth';
 import { nationalInsuranceNumberValidator } from '@hmcts/opal-frontend-common/validators/national-insurance-number';
 import { optionalMaxLengthValidator } from '@hmcts/opal-frontend-common/validators/optional-max-length';
 import { optionalValidDateValidator } from '@hmcts/opal-frontend-common/validators/optional-valid-date';
-import { specialCharactersValidator } from '@hmcts/opal-frontend-common/validators/special-characters';
 import { GovukSelectComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-select';
 import { GovukTextInputComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-text-input';
 import { IGovUkSelectOptions } from '@hmcts/opal-frontend-common/components/govuk/govuk-select/interfaces';
 import { CapitalisationDirective } from '@hmcts/opal-frontend-common/directives/capitalisation';
+import { patternValidator } from '@hmcts/opal-frontend-common/validators/pattern-validator';
+import { LETTERS_WITH_SPACES_PATTERN, SPECIAL_CHARACTERS_PATTERN } from '@hmcts/opal-frontend-common/constants';
 import { FINES_MAC_DEFENDANT_TYPES_KEYS } from '../../constants/fines-mac-defendant-types-keys';
+
+// regex pattern validators for the form controls
+const LETTERS_WITH_SPACES_PATTERN_VALIDATOR = patternValidator(LETTERS_WITH_SPACES_PATTERN, 'alphabeticalTextPattern');
+const SPECIAL_CHARACTERS_PATTERN_VALIDATOR = patternValidator(SPECIAL_CHARACTERS_PATTERN, 'specialCharactersPattern');
+
 @Component({
   selector: 'app-fines-mac-personal-details-form',
   imports: [
@@ -91,12 +96,12 @@ export class FinesMacPersonalDetailsFormComponent extends AbstractFormAliasBaseC
       fm_personal_details_forenames: new FormControl(null, [
         Validators.required,
         Validators.maxLength(20),
-        alphabeticalTextValidator(),
+        LETTERS_WITH_SPACES_PATTERN_VALIDATOR,
       ]),
       fm_personal_details_surname: new FormControl(null, [
         Validators.required,
         Validators.maxLength(30),
-        alphabeticalTextValidator(),
+        LETTERS_WITH_SPACES_PATTERN_VALIDATOR,
       ]),
       fm_personal_details_aliases: new FormArray([]),
       fm_personal_details_add_alias: new FormControl(null),
@@ -105,15 +110,15 @@ export class FinesMacPersonalDetailsFormComponent extends AbstractFormAliasBaseC
       fm_personal_details_address_line_1: new FormControl(null, [
         Validators.required,
         Validators.maxLength(30),
-        specialCharactersValidator(),
+        SPECIAL_CHARACTERS_PATTERN_VALIDATOR,
       ]),
       fm_personal_details_address_line_2: new FormControl(null, [
         optionalMaxLengthValidator(30),
-        specialCharactersValidator(),
+        SPECIAL_CHARACTERS_PATTERN_VALIDATOR,
       ]),
       fm_personal_details_address_line_3: new FormControl(null, [
         optionalMaxLengthValidator(16),
-        specialCharactersValidator(),
+        SPECIAL_CHARACTERS_PATTERN_VALIDATOR,
       ]),
       fm_personal_details_post_code: new FormControl(null, [optionalMaxLengthValidator(8)]),
     });
