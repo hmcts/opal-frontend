e6ae8df3fadbf23be5bce16508eab02143a9e015
stephen.magill <stephen.magill@cgi.com>
2025-08-13 10:03:25 +0100

    Import

--- cypress/component/fineAccountEnquiry/searchAndMatches/SearchAndMatchesIndividuals.cy.ts
+++ cypress/component/fineAccountEnquiry/searchAndMatches/SearchAndMatchesIndividuals.cy.ts
@@ -7,7 +7,6 @@ import { provideHttpClient } from '@angular/common/http';
 import { DOM_ELEMENTS } from './constants/search_and_matches_individuals_elements';
 import { INDIVIDUAL_SEARCH_STATE_MOCK } from './mocks/search_and_matches_individual_mock';
 import { delay } from 'cypress/types/bluebird';
-import { only } from 'node:test';
 
 describe('Search Account Component - Individuals', () => {
   let individualSearchMock = structuredClone(INDIVIDUAL_SEARCH_STATE_MOCK);
@@ -391,38 +390,34 @@ describe('Search Account Component - Individuals', () => {
     cy.get(DOM_ELEMENTS.errorSummary).should('exist');
     cy.get(DOM_ELEMENTS.lastNameError).should('exist').and('contain', 'Enter last name');
   });
-
+  
   it('AC1a. Should validate last name field when alias checkbox selected', { tags: ['PO-1969'] }, () => {
     setupComponent(null);
 
-    cy.get(DOM_ELEMENTS.includeAliasesCheckbox).check().should('be.checked');
+    cy.get(DOM_ELEMENTS.includeAliasesCheckbox).check().should('be.checked');;
     cy.get(DOM_ELEMENTS.searchButton).click();
 
     cy.get(DOM_ELEMENTS.lastNameError).should('exist').and('contain', 'Enter last name');
   });
 
-  it(
-    'AC1b. Should validate last name field when "Search exact match" for last name is selected',
-    { tags: ['PO-1969'] },
-    () => {
-      setupComponent(null);
+it('AC1b. Should validate last name field when "Search exact match" for last name is selected', { tags: ['PO-1969'] }, () => {
+  setupComponent(null);
 
-      cy.get(DOM_ELEMENTS.lastNameExactMatchCheckbox).check().should('be.checked');
-      cy.get(DOM_ELEMENTS.searchButton).click();
+  cy.get(DOM_ELEMENTS.lastNameExactMatchCheckbox).check().should('be.checked');
+  cy.get(DOM_ELEMENTS.searchButton).click();
 
-      cy.get(DOM_ELEMENTS.lastNameError).should('exist').and('contain', 'Enter last name');
-    },
-  );
-  it(
-    'AC1c. Should validate first name field when "Search exact match" for first name is selected',
-    { tags: ['PO-1969'] },
-    () => {
-      setupComponent(null);
+  cy.get(DOM_ELEMENTS.lastNameError)
+    .should('exist')
+    .and('contain', 'Enter last name');
+});
+it('AC1c. Should validate first name field when "Search exact match" for first name is selected', { tags: ['PO-1969'] }, () => {
+  setupComponent(null);
 
-      cy.get(DOM_ELEMENTS.firstNamesExactMatchCheckbox).check().should('be.checked');
-      cy.get(DOM_ELEMENTS.searchButton).click();
+  cy.get(DOM_ELEMENTS.firstNamesExactMatchCheckbox).check().should('be.checked');
+  cy.get(DOM_ELEMENTS.searchButton).click();
 
-      cy.get(DOM_ELEMENTS.firstNamesError).should('exist').and('contain', 'Enter first name');
-    },
-  );
+  cy.get(DOM_ELEMENTS.firstNamesError)
+    .should('exist')
+    .and('contain', 'Enter first name');
+});
 });
