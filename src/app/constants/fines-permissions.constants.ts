59e125c09c764dee3dab9af52b1497dea7e03206
louisbriggs <louis.briggs@gmail.com>
2025-08-29 10:53:30 +0100

    Re-ordering fines_persmission

--- src/app/constants/fines-permissions.constants.ts
+++ src/app/constants/fines-permissions.constants.ts
@@ -1,11 +1,11 @@
 import { IFinesPermissions } from '../interfaces/fines-permissions.interfaces';
 
 export const FINES_PERMISSIONS: IFinesPermissions = {
-  'collection-order': 4,
   'create-and-manage-draft-accounts': 1,
-  'search-and-view-accounts': 6,
-  'check-and-validate-draft-accounts': 5,
-  'account-notes': 2,
+  'account-notes': 2, // assumed, TBC
   'account-enquiry': 3,
-  'account-maintenance': 7,
+  'collection-order': 4,
+  'check-and-validate-draft-accounts': 5,
+  'search-and-view-accounts': 6,
+  'account-maintenance': 7, // assumed, TBC
 };
