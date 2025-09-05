0bf402976386cf435f17ac50d184917694baecc6
louisbriggs <louis.briggs@gmail.com>
2025-09-05 09:59:45 +0100

    Adding interface for defendant details tab

--- src/app/flows/fines/services/opal-fines-service/interfaces/opal-fines-account-defendant-details-defendant-tab-ref-data.interface.ts
+++ src/app/flows/fines/services/opal-fines-service/interfaces/opal-fines-account-defendant-details-defendant-tab-ref-data.interface.ts
@@ -1,3 +1,16 @@
+import { IOpalFinesDefendantAccountAddress, IOpalFinesDefendantAccountContactDetails, IOpalFinesDefendantAccountEmployerDetails, IOpalFinesDefendantAccountLanguagePreferences, IOpalFinesDefendantAccountPartyDetails, IOpalFinesDefendantAccountVehicleDetails } from "./opal-fines-defendant-account.interface";
+
 export interface IOpalFinesAccountDefendantDetailsDefendantTabRefData {
   version: number | undefined;
+  defendant_account_id: string;
+  defendant_account_party: {
+    defendant_account_party_type: string;
+    is_debtor: boolean;
+    party_details: IOpalFinesDefendantAccountPartyDetails;
+    address: IOpalFinesDefendantAccountAddress;
+    contact_details: IOpalFinesDefendantAccountContactDetails | null;
+    vehicle_details: IOpalFinesDefendantAccountVehicleDetails | null;
+    employer_details: IOpalFinesDefendantAccountEmployerDetails | null;
+    language_preferences: IOpalFinesDefendantAccountLanguagePreferences | null
+  }
 }
