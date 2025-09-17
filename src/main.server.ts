662cc89e5553b0d3f6ed51dfd8db29df36bf8453
Frankie Moran <frankie.jack.moran@gmail.com>
2025-09-17 10:23:40 +0100

    Update main.server.ts to match the new Angular upgrade

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { ApplicationRef } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+export function render(context: any): Promise<ApplicationRef> {
+  return bootstrapApplication(
+    AppComponent,
+    {
+      ...config,
+      providers: config.providers || [],
+    },
+    context,
+  );
+}
 
-export default bootstrap;
+export default render;
