dc6d5479656e6be890ce0aa7afce966f2cdfab86
Frankie Moran <frankie.jack.moran@gmail.com>
2025-09-17 10:27:48 +0100

    Linting

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication, BootstrapContext } from '@angular/platform-browse
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
