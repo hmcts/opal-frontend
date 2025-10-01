69ba08b6685dab5df154f12b622ba7cddf4772a6
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-10-01 14:24:57 +0100

    slight change

--- src/app/flows/fines/fines-acc/routing/resolvers/defendant-account-party.resolver.ts
+++ src/app/flows/fines/fines-acc/routing/resolvers/defendant-account-party.resolver.ts
@@ -30,7 +30,7 @@ export const defendantAccountPartyResolver: ResolveFn<IFinesAccDebtorAddAmendFor
 
   /**
    * Fetches the defendant account Party data from cache, transforms it to debtor form structure.
-   * This data should already be cached from the "tDefendant" tab visit.
+   * This data should already be cached from the "Defendant" tab visit.
    * @param accountId - The ID of the defendant account.
    * @returns An observable that emits the transformed debtor form data or redirects to Account Enquiry on error.
    */
