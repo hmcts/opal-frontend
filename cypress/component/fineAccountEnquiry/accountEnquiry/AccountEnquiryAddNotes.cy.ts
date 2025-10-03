a0977c4aaaa646735555a943d9afad7dbfa0dff3
Nipa1410 <nipa.patel@hmcts.net>
2025-10-03 13:37:01 +0100

    Addressed review comments

--- cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryAddNotes.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryAddNotes.cy.ts
@@ -129,7 +129,6 @@ describe('FinesAccNoteAddFormComponent', () => {
   );
 
   it('(AC.4c, 4ci, 4cii)click submit button after entering valid data', { tags: ['@PO-771', '@807', '@809'] }, () => {
-    //const formSubmit = cy.spy().as('formSubmitSpy');
     setupComponent(null);
     cy.get(DOM_ELEMENTS.addNoteTextBox).clear().type('a'.repeat(10), { delay: 0 });
     cy.intercept('POST', '**/opal-fines-service/notes/add', { statusCode: 200 }).as('addNote');
