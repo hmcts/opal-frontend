9e906d46d42d844cb09a5685fc6e46dcf0a54e3e
CadeFaulkner <cade.faulkner@hmcts.net>
2025-10-15 16:29:39 +0100

    Refactor defendant details tests and intercepts; add new AccountEnquiry component

--- cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiriesDefendantDetails.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiriesDefendantDetails.cy.ts
@@ -1,11 +1,12 @@
 import { mount } from 'cypress/angular';
 import { provideHttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, provideRouter, Router } from '@angular/router';
 import { of } from 'rxjs';
 
 import { FinesAccDefendantDetailsDefendantTabComponent } from '../../../../src/app/flows/fines/fines-acc/fines-acc-defendant-details/fines-acc-defendant-details-defendant-tab/fines-acc-defendant-details-defendant-tab.component';
 import { FinesAccountStore } from '../../../../src/app/flows/fines/fines-acc/stores/fines-acc.store';
 import { OpalFines } from '../../../../src/app/flows/fines/services/opal-fines-service/opal-fines.service';
+import { FinesAccComponent } from '../../../../src/app/flows/fines/fines-acc/fines-acc.component';
 import { DateService } from '@hmcts/opal-frontend-common/services/date-service';
 import { UtilsService } from '@hmcts/opal-frontend-common/services/utils-service';
 import { GlobalStore } from '@hmcts/opal-frontend-common/stores/global';
@@ -16,10 +17,16 @@ import { FinesAccDefendantDetailsComponent } from '../../../../src/app/flows/fin
 import { OPAL_FINES_ACCOUNT_DEFENDANT_ACCOUNT_PARTY_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-account-defendant-account-party.mock';
 import { OPAL_FINES_ACCOUNT_DEFENDANT_TAB_MOCK, MOCK_FINES_ACCOUNT_STATE } from './mocks/defendant_details_tab_mock';
 import { IOpalUserState } from '@hmcts/opal-frontend-common/services/opal-user-service/interfaces';
-import { interceptDefendantDetails } from './intercept/defendant_details_tab_intercept';
-import { get } from 'https';
+import { interceptDefendantDetails } from './intercept/interceptDefendantDetails';
+import { IFinesAccountState } from 'src/app/flows/fines/fines-acc/interfaces/fines-acc-state-interface';
+import { FinesAccPayloadService } from 'src/app/flows/fines/fines-acc/services/fines-acc-payload.service';
+import { url } from 'inspector';
+import { IOpalFinesAccountDefendantDetailsHeader } from 'src/app/flows/fines/fines-acc/fines-acc-defendant-details/interfaces/fines-acc-defendant-details-header.interface';
 
 describe('Defendant Details Tab (Component)', () => {
+  let payloadService: FinesAccPayloadService;
+  let transformedHeadingData: IFinesAccountState;
+
   const setupComponent = (
     prefilledData = DEFENDANT_HEADER_MOCK,
     language = MOCK_FINES_ACCOUNT_STATE,
@@ -29,35 +36,46 @@ describe('Defendant Details Tab (Component)', () => {
       mount(FinesAccDefendantDetailsComponent, {
         providers: [
           provideHttpClient(),
+          provideRouter([
+            {
+              path: '**',
+              component: FinesAccDefendantDetailsComponent,
+              resolve: {
+                defendantAccountHeadingData: prefilledData,
+              },
+              runGuardsAndResolvers: 'always',
+            },
+          ]),
           OpalFines,
+          FinesAccPayloadService,
+          {
+            provide: FinesAccPayloadService,
+            useFactory: () => {
+              payloadService = new FinesAccPayloadService();
+              transformedHeadingData = payloadService.transformAccountHeaderForStore(6000000001, prefilledData);
+              cy.log('Transformed Data in PayloadService: ' + JSON.stringify(transformedHeadingData));
+              return payloadService;
+            },
+          },
           {
             provide: FinesAccountStore,
             useFactory: () => {
               const store = new FinesAccountStore();
-              const mockState = structuredClone(MOCK_FINES_ACCOUNT_STATE);
-
-              if (prefilledData.party_details.organisation_flag) {
-                mockState.party_name = prefilledData.party_details.organisation_details?.organisation_name ?? '';
-                mockState.party_type = 'Organisation';
-              } else {
-                mockState.party_name =
-                  `${prefilledData.party_details.individual_details?.forenames ?? ''} ${prefilledData.party_details.individual_details?.surname ?? ''}`.trim();
-                mockState.party_type = 'Individual';
-              }
-
-              store.setAccountState(mockState);
+              cy.log('account state', store.getAccountState());
+              //transformedHeadingData = payloadService.transformAccountHeaderForStore(6000000001, prefilledData);
+              store.setAccountState(transformedHeadingData);
+              cy.log('Base version: ' + store.getAccountState().base_version);
               return store;
             },
           },
           {
             provide: ActivatedRoute,
             useValue: {
-              fragment: of('defendant-account-parties'),
+              fragment: of('defendant'),
               snapshot: {
                 data: {
                   defendantAccountHeadingData: prefilledData,
                 },
-                fragment: 'defendant-account-parties',
                 parent: { snapshot: { url: [{ path: 'defendant' }] } },
               },
             },
@@ -82,6 +100,11 @@ describe('Defendant Details Tab (Component)', () => {
     });
   };
 
+  beforeEach(() => {
+    payloadService = new FinesAccPayloadService();
+    transformedHeadingData = payloadService.transformAccountHeaderForStore(6000000001, DEFENDANT_HEADER_MOCK);
+  });
+
   it('AC1a, AC1b, AC1d. Defendant details tab layout, debtor flag true', { tags: ['PO-784'] }, () => {
     interceptDefendantDetails();
     setupComponent(DEFENDANT_HEADER_MOCK, MOCK_FINES_ACCOUNT_STATE, USER_STATE_MOCK_PERMISSION_BU77);
@@ -100,7 +123,7 @@ describe('Defendant Details Tab (Component)', () => {
     setupComponent(DEFENDANT_HEADER_MOCK, MOCK_FINES_ACCOUNT_STATE, USER_STATE_MOCK_PERMISSION_BU77);
   });
 
-  it('AC2a. Account maintenance permission', { tags: ['PO-784'] }, () => {
+  it.only('AC2a. Account maintenance permission', { tags: ['PO-784'] }, () => {
     interceptDefendantDetails();
     setupComponent(DEFENDANT_HEADER_MOCK, MOCK_FINES_ACCOUNT_STATE, USER_STATE_MOCK_PERMISSION_BU77);
   });
