6b06aa64ba9db10de4ee0cf75d0b7711a2059d6c
CadeFaulkner <cade.faulkner@hmcts.net>
2025-10-20 12:08:56 +0100

    refactor: update import paths and rename DOM_ELEMENTS to ACCOUNT_ENQUIRY_HEADER_ELEMENTS

--- cypress/component/fineAccountEnquiry/accountEnquiry/constants/account_enquiry_header_elements.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/constants/account_enquiry_header_elements.ts
@@ -1,6 +1,6 @@
 // Stable selectors / visible text hooks for the Defendant Details page.
 
-export const DOM_ELEMENTS = {
+export const ACCOUNT_ENQUIRY_HEADER_ELEMENTS = {
   headingWithCaption: 'opal-lib-govuk-heading-with-caption',
   headingName: 'h1.govuk-heading-l',
   pageHeader: 'opal-lib-custom-page-header',
