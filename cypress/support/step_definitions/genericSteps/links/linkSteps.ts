39a165a64d9a5e2769ef00f88a0afe418931ad7b
Timothy Gonella <timothy.gonella@hmcts.net>
2025-10-21 09:42:09 +0100

    Updates based on Review comment

---
+++ cypress/support/step_definitions/genericSteps/links/linkSteps.ts
@@ -0,0 +1,79 @@
+import { Then, When } from '@badeball/cypress-cucumber-preprocessor';
+import { clickLinkAcrossPages } from '../../../step_definitions/genericSteps/links/linkHelpers';
+
+/* ------------------------------------------------------------------------------------------------
+ * Cucumber steps
+ * ----------------------------------------------------------------------------------------------*/
+
+Then('I click on the {string} link', (linkText: string) => {
+  clickLinkAcrossPages(linkText, {
+    waitAfterPageMs: 200,
+    maxPages: 30,
+    paginationRetries: 5,
+    paginationRetryDelayMs: 500,
+    singlePageRetries: 6,
+    singlePageRetryDelayMs: 500,
+    // For tabs like “Rejected/Failed”, refreshing the current tab can help on slow renders:
+    singlePageRefreshSelector: '.moj-sub-navigation__link[aria-current="page"]',
+  });
+});
+
+When('{string} is clicked', (linkText: string) => {
+  cy.get('a').contains(linkText).click();
+});
+
+When('I click the {string} link in the {string} section', (linkText: string, _section: string) => {
+  cy.contains('a', linkText).first().click();
+});
+
+When('{string} is clicked, nothing happens', (linkText: string) => {
+  let initialUrl: string;
+  cy.url().then((url) => {
+    initialUrl = url.toString();
+    cy.get('a').contains(linkText).click();
+    cy.url().should('eq', initialUrl);
+  });
+});
+
+When('the link with text {string} should not be present', (linkText: string) => {
+  cy.contains('a', linkText).should('not.exist');
+});
+
+Then('I click on the {string} link for imposition {int}', (linkText: string, index: number) => {
+  cy.contains('legend', 'Impositions')
+    .parent()
+    .find('opal-lib-moj-ticket-panel')
+    .eq(index - 1)
+    .contains('a', linkText)
+    .click();
+});
+
+Then('I see the {string} link for imposition {int}', (linkText: string, index: number) => {
+  cy.contains('legend', 'Impositions')
+    .parent()
+    .find('opal-lib-moj-ticket-panel')
+    .eq(index - 1)
+    .contains('a', linkText)
+    .should('exist');
+});
+
+Then('I do not see the {string} link for imposition {int}', (linkText: string, index: number) => {
+  cy.contains('legend', 'Impositions')
+    .parent()
+    .find('opal-lib-moj-ticket-panel')
+    .eq(index - 1)
+    .contains('a', linkText)
+    .should('not.exist');
+});
+
+Then('I click the {string} link for offence {string}', (linkText: string, offence: string) => {
+  cy.contains('app-fines-mac-offence-details-review-offence', offence).find('a').contains(linkText).click();
+});
+
+Then('I see the {string} link for offence {string}', (linkText: string, offence: string) => {
+  cy.contains('app-fines-mac-offence-details-review-offence', offence).find('a').contains(linkText).should('exist');
+});
+
+Then('I open the {string} link in the same tab', (linkText: string) => {
+  cy.get('a').contains(linkText).invoke('removeAttr', 'target').click();
+});
