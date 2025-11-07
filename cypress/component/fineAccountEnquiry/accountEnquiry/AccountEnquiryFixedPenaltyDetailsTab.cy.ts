1a0498cd9372d56825bfc7a3f518e0aae392db4d
Alfred-Gillingham2 <Alfred.Gillingham@HMCTS.NET>
2025-11-07 10:12:36 +0000

    WIP - Component tests for PO-994 and PO-1818

---
+++ cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryFixedPenaltyDetailsTab.cy.ts
@@ -0,0 +1,227 @@
+import { interceptAuthenticatedUser, interceptUserState } from '../../CommonIntercepts/CommonIntercepts';
+import {
+  DEFENDANT_HEADER_MOCK,
+  USER_STATE_MOCK_PERMISSION_BU77,
+  createDefendantHeaderMockWithName,
+} from './mocks/defendant_details_mock';
+import { interceptDefendantHeader, interceptFixedPenaltyDetails } from './intercept/defendantAccountIntercepts';
+import { OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-account-defendant-details-fixed-penalty.mock';
+import {
+  ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS,
+  ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS as DOM,
+} from './constants/account_enquiry_fixed_penalty_elements';
+import { setupAccountEnquiryComponent } from './setup/SetupComponent';
+import { IComponentProperties } from './setup/setupComponent.interface';
+
+const componentProperties: IComponentProperties = {
+  accountId: '77',
+  fragments: 'fixed-penalty',
+  interceptedRoutes: [
+    '/access-denied',
+    '../note/add',
+    '../individual/amend',
+    '../company/amend',
+    // Add more routes here as needed
+  ],
+};
+describe('Account Enquiry Fixed Penalty', () => {
+  it(
+    'AC1a: Adult/Youth only - Fixed Penalty details tab, vehicle fixed penalty, all fields shown',
+    { tags: ['@PO-994'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+      // TODO: Change to company for other tests, change above to add company name
+      headerMock.debtor_type = 'individual';
+      headerMock.account_type = 'Fixed Penalty';
+      let fixedPenaltyDetailsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK);
+      fixedPenaltyDetailsMock.vehicle_fixed_penalty_flag = true;
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptFixedPenaltyDetails(accountId, fixedPenaltyDetailsMock, '123');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.tableTitle)
+        .should('exist')
+        .and('contain.text', 'Fixed Penalty details');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelIssuingAuthority)
+        .should('exist')
+        .and('contain.text', 'City of Metropolis');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTicketNumber).should('exist').and('contain.text', 'FP-123456');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelRegistrationNumber)
+        .should('exist')
+        .and('contain.text', 'XY21 ABC');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelDrivingLicence)
+        .should('exist')
+        .and('contain.text', 'LIC-789012');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeNumber).should('exist').and('contain.text', 'NT-345678');
+      //cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeDate).should('exist').and('contain.text', '01/05/2023');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTimeOfOffence).should('exist').and('contain.text', '14:30');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelPlaceOfOffence)
+        .should('exist')
+        .and('contain.text', 'Main Street, Metropolis');
+    },
+  );
+
+  it(
+    'AC1b: Adult/Youth only - Fixed Penalty details tab, non-vehicle fixed penalty, partial view',
+    { tags: ['@PO-994'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+      headerMock.debtor_type = 'individual';
+      headerMock.account_type = 'Fixed Penalty';
+      let fixedPenaltyDetailsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptFixedPenaltyDetails(accountId, fixedPenaltyDetailsMock, '123');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.tableTitle)
+        .should('exist')
+        .and('contain.text', 'Fixed Penalty details');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelIssuingAuthority)
+        .should('exist')
+        .and('contain.text', 'City of Metropolis');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTicketNumber).should('exist').and('contain.text', 'FP-123456');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelRegistrationNumber).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelDrivingLicence).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeNumber).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeDate).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTimeOfOffence).should('exist').and('contain.text', '14:30');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelPlaceOfOffence)
+        .should('exist')
+        .and('contain.text', 'Main Street, Metropolis');
+    },
+  );
+
+  it(
+    'AC1c: Adult/Youth only - Fixed Penalty details tab, show em-dash for missing fields',
+    { tags: ['@PO-994'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+      headerMock.debtor_type = 'individual';
+      headerMock.account_type = 'Fixed Penalty';
+      let fixedPenaltyDetailsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK);
+      fixedPenaltyDetailsMock.vehicle_fixed_penalty_flag = true;
+      //fixedPenaltyDetailsMock.vehicle_fixed_penalty_details?.notice_number = null;
+      //fixedPenaltyDetailsMock.vehicle_fixed_penalty_details.vehicle_registration_number = null;
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptFixedPenaltyDetails(accountId, fixedPenaltyDetailsMock, '123');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.tableTitle)
+        .should('exist')
+        .and('contain.text', 'Fixed Penalty details');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelRegistrationNumber).should('exist').and('contain.text', '—');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeNumber).should('exist').and('contain.text', '—');
+    },
+  );
+
+  it(
+    'AC1a: Company - Fixed Penalty details tab, vehicle fixed penalty, all fields shown',
+    { tags: ['@PO-994'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+      // TODO: Change to company for other tests, change above to add company name
+      headerMock.debtor_type = 'individual';
+      headerMock.account_type = 'Fixed Penalty';
+      let fixedPenaltyDetailsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK);
+      //headerMock.party_details.organisation_flag = true;
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptFixedPenaltyDetails(accountId, fixedPenaltyDetailsMock, '123');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.tableTitle)
+        .should('exist')
+        .and('contain.text', 'Fixed Penalty details');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelIssuingAuthority)
+        .should('exist')
+        .and('contain.text', 'City of Metropolis');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTicketNumber).should('exist').and('contain.text', 'FP-123456');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelRegistrationNumber)
+        .should('exist')
+        .and('contain.text', 'XY21 ABC');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelDrivingLicence)
+        .should('exist')
+        .and('contain.text', 'LIC-789012');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeNumber).should('exist').and('contain.text', 'NT-345678');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeDate).should('exist').and('contain.text', '01/05/2023');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTimeOfOffence).should('exist').and('contain.text', '14:30');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelPlaceOfOffence)
+        .should('exist')
+        .and('contain.text', 'Main Street, Metropolis');
+    },
+  );
+
+  it(
+    'AC1b: Company - Fixed Penalty details tab, non-vehicle fixed penalty, partial view',
+    { tags: ['@PO-994'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+      // TODO: Change to company for other tests, change above to add company name
+      headerMock.debtor_type = 'individual';
+      headerMock.account_type = 'Fixed Penalty';
+      let fixedPenaltyDetailsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK);
+      //headerMock.party_details.organisation_flag = true;
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptFixedPenaltyDetails(accountId, fixedPenaltyDetailsMock, '123');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.tableTitle)
+        .should('exist')
+        .and('contain.text', 'Fixed Penalty details');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelIssuingAuthority)
+        .should('exist')
+        .and('contain.text', 'City of Metropolis');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTicketNumber).should('exist').and('contain.text', 'FP-123456');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelRegistrationNumber).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelDrivingLicence).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeNumber).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelNoticeDate).should('not.exist');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTimeOfOffence).should('exist').and('contain.text', '14:30');
+      cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelPlaceOfOffence)
+        .should('exist')
+        .and('contain.text', 'Main Street, Metropolis');
+    },
+  );
+
+  it('AC1c: Company - Fixed Penalty details tab, show em-dash for missing fields', { tags: ['@PO-994'] }, () => {
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+    headerMock.debtor_type = 'COMPANY';
+
+    let fixedPenaltyDetailsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_FIXED_PENALTY_MOCK);
+
+    const accountId = headerMock.defendant_account_party_id;
+
+    setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+    cy.get('router-outlet').should('exist');
+
+    cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.tableTitle)
+      .should('exist')
+      .and('contain.text', 'Fixed Penalty details');
+    cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelRegistrationNumber).should('exist').and('contain.text', ' - ');
+    cy.get(ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS.labelTimeOfOffence).should('exist').and('contain.text', ' - ');
+  });
+});
---
+++ cypress/component/fineAccountEnquiry/accountEnquiry/constants/account_enquiry_fixed_penalty_elements.ts
@@ -0,0 +1,25 @@
+// Stable selectors / visible text hooks for the Defendant Details page.
+
+export const ACCOUNT_ENQUIRY_FIXED_PENALTY_ELEMENTS = {
+  headingWithCaption: 'opal-lib-govuk-heading-with-caption',
+  headingName: 'h1.govuk-heading-l',
+  pageHeader: 'opal-lib-custom-page-header',
+
+  // Buttons
+  addNoteButton: 'button#addAccountNote',
+
+  // Info sections
+  summaryMetricBar: 'opal-lib-custom-summary-metric-bar',
+  accountInfo: 'opal-lib-custom-account-information',
+
+  // Visible labels used in assertions
+  tableTitle: '.govuk-summary-card__title',
+  labelIssuingAuthority: '#fixedPenaltyDetailsIssuing_authorityValue',
+  labelTicketNumber: '#fixedPenaltyDetailsTicket_numberValue',
+  labelRegistrationNumber: '#fixedPenaltyDetailsRegistration_numberValue',
+  labelDrivingLicence: '#fixedPenaltyDetailsLicence_numberValue',
+  labelNoticeNumber: '#fixedPenaltyDetailsNotice_numberValue',
+  labelNoticeDate: '#fixedPenaltyDetailsIssued_dateValue',
+  labelTimeOfOffence: '#fixedPenaltyDetailsTime_of_offenceValue',
+  labelPlaceOfOffence: '#fixedPenaltyDetailsPlace_of_offenceValue',
+};
