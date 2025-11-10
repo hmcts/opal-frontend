4444bdc7458375782114eab58a3cec44ce1f797e
Nipa1410 <nipa.patel@hmcts.net>
2025-11-10 17:43:29 +0000

    Completed coding

---
+++ cypress/component/fineAccountEnquiry/versionControl/constants/global_version_control_elements.ts
@@ -0,0 +1,23 @@
+export const DOM_ELEMENTS = {
+  // Warning Banner Elements
+  warningBanner: '.moj-alert',
+  warningBannerIcon: 'opal-lib-moj-alert-icon[type="warning"]',
+  warningBannerText: 'opal-lib-moj-alert-content-text',
+  refreshLink: 'opal-lib-moj-alert-content-text > a',
+
+  // Success Banner Elements
+  successBanner: 'opal-lib-moj-alert',
+  successBannerIcon: 'opal-lib-moj-alert-icon',
+  successBannerText: 'opal-lib-moj-alert-content-text',
+  successBannerDismiss: '.moj-alert__dismiss',
+
+  // Tab Elements
+  atAGlanceTab: '[subnavitemid="at-a-glance-tab"] > .moj-sub-navigation__link',
+  impositionsTab: '[subnavitemid="impositions-tab"] > .moj-sub-navigation__link',
+  paymentHistoryTab: '[subnavitemid="payment-history-tab"] > .moj-sub-navigation__link',
+  historyNotesTab: '[subnavitemid="history-and-notes-tab"] > .moj-sub-navigation__link',
+  defendantTab: '[subnavitemid="defendant-tab"] > .moj-sub-navigation__link',
+  parentGuardianTab: '[subnavitemid="parent-or-guardian-tab"] > .moj-sub-navigation__link',
+  paymentTermsTab: '[subnavitemid="payment-terms-tab"] > .moj-sub-navigation__link',
+  enforcementTab: '[subnavitemid="enforcement-tab"] > .moj-sub-navigation__link',
+};
