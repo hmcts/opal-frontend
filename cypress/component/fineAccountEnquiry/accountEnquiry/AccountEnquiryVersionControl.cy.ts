3ccabb716ea57d7288a6dddbfd5bcd7ee4f6cf81
Nipa1410 <nipa.patel@hmcts.net>
2025-11-14 11:27:41 +0000

    Pipeline Fixes

--- cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryVersionControl.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryVersionControl.cy.ts
@@ -1,8 +1,13 @@
 import { OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-account-defendant-at-a-glance.mock';
+import { OPAL_FINES_ACCOUNT_DEFENDANT_ACCOUNT_PARTY_MOCK } from '@services/fines/opal-fines-service/mocks/opal-fines-account-defendant-account-party.mock';
 import { DOM_ELEMENTS } from './constants/global_version_control_elements';
 import { setupAccountEnquiryComponent } from './setup/SetupComponent';
 import { IComponentProperties } from './setup/setupComponent.interface';
-import { interceptAtAGlance, interceptDefendantHeader } from './intercept/defendantAccountIntercepts';
+import {
+  interceptAtAGlance,
+  interceptDefendantDetails,
+  interceptDefendantHeader,
+} from './intercept/defendantAccountIntercepts';
 import { interceptAuthenticatedUser, interceptUserState } from 'cypress/component/CommonIntercepts/CommonIntercepts';
 import { DEFENDANT_HEADER_MOCK, USER_STATE_MOCK_PERMISSION_BU77 } from './mocks/defendant_details_mock';
 
@@ -31,6 +36,14 @@ describe('Global Version Control Mechanism - Component Tests', () => {
       const etag = 'W/"version-1"';
 
       interceptDefendantHeader(77, DEFENDANT_HEADER_MOCK, etag);
+      interceptDefendantDetails(
+        77,
+        {
+          version: DEFENDANT_HEADER_MOCK.version,
+          defendant_account_party: OPAL_FINES_ACCOUNT_DEFENDANT_ACCOUNT_PARTY_MOCK.defendant_account_party,
+        },
+        etag,
+      );
       interceptAtAGlance(77, OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK, etag);
 
       setupAccountEnquiryComponent(componentProperties);
@@ -65,6 +78,14 @@ describe('Global Version Control Mechanism - Component Tests', () => {
       const atAGlanceEtag = 'W/"version-2"';
 
       interceptDefendantHeader(77, DEFENDANT_HEADER_MOCK, headerEtag);
+      interceptDefendantDetails(
+        77,
+        {
+          version: DEFENDANT_HEADER_MOCK.version, // Use same version as header to show they match
+          defendant_account_party: OPAL_FINES_ACCOUNT_DEFENDANT_ACCOUNT_PARTY_MOCK.defendant_account_party,
+        },
+        headerEtag, // Use headerEtag to match the header version
+      );
       interceptAtAGlance(77, OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK, atAGlanceEtag);
 
       setupAccountEnquiryComponent(componentProperties);
@@ -102,8 +123,15 @@ describe('Global Version Control Mechanism - Component Tests', () => {
       const atAGlanceEtag = 'W/"version-2"';
 
       interceptDefendantHeader(77, DEFENDANT_HEADER_MOCK, headerEtag);
+      interceptDefendantDetails(
+        77,
+        {
+          version: DEFENDANT_HEADER_MOCK.version,
+          defendant_account_party: OPAL_FINES_ACCOUNT_DEFENDANT_ACCOUNT_PARTY_MOCK.defendant_account_party,
+        },
+        atAGlanceEtag,
+      );
       interceptAtAGlance(77, OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK, atAGlanceEtag);
-
       setupAccountEnquiryComponent(componentProperties);
 
       cy.get(DOM_ELEMENTS.warningBanner).should('exist');
@@ -114,6 +142,14 @@ describe('Global Version Control Mechanism - Component Tests', () => {
 
       const newEtag = 'W/"version-2"';
       interceptDefendantHeader(77, DEFENDANT_HEADER_MOCK, newEtag);
+      interceptDefendantDetails(
+        77,
+        {
+          version: DEFENDANT_HEADER_MOCK.version,
+          defendant_account_party: OPAL_FINES_ACCOUNT_DEFENDANT_ACCOUNT_PARTY_MOCK.defendant_account_party,
+        },
+        newEtag,
+      );
       interceptAtAGlance(77, OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK, newEtag);
       // Click refresh link
       cy.get(DOM_ELEMENTS.refreshLink).click();
