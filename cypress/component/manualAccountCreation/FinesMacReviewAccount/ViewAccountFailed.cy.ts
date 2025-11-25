965e26ffc199970085f2f9faa771c844e878d0de
Frankie Moran <frankie.jack.moran@gmail.com>
2025-11-25 09:50:18 +0000

    refactor: rename error handling methods to use bannerError for consistency

--- cypress/component/manualAccountCreation/FinesMacReviewAccount/ViewAccountFailed.cy.ts
+++ cypress/component/manualAccountCreation/FinesMacReviewAccount/ViewAccountFailed.cy.ts
@@ -52,7 +52,7 @@ describe('FinesMacReviewAccountComponent - View Failed Account', () => {
           useFactory: () => {
             let globalStore = new GlobalStore();
             globalStore.setUserState(ACCOUNT_SESSION_USER_STATE_MOCK);
-            globalStore.setError({
+            globalStore.setBannerError({
               error: false,
               message: '',
             });
