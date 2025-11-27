dceac85da04cc81faf2f691282fc76a2f82a3d90
Timothy Gonella <timothy.gonella@hmcts.net>
2025-11-27 11:49:37 +0000

    Included fixes to stop “Maximum call stack size exceeded” from the sourcemap

---
+++ cypress/support/stubs/cucumber-source-map.js
@@ -0,0 +1,6 @@
+// Stubbed source-map helpers to avoid sourcemap parsing in headed Cypress runs.
+module.exports = {
+  createSourceMapConsumer: () => undefined,
+  cachedCreateSourceMapConsumer: () => undefined,
+  maybeRetrievePositionFromSourceMap: () => undefined,
+};
