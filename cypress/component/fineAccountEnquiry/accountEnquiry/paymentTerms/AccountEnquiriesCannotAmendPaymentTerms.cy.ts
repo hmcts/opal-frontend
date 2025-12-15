63a14719226016b12d45511db5877e613df71dd3
Nipa1410 <nipa.patel@hmcts.net>
2025-12-15 09:40:05 +0000

    Parent/Guardian tests added

--- cypress/component/fineAccountEnquiry/accountEnquiry/paymentTerms/AccountEnquiriesCannotAmendPaymentTerms.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/paymentTerms/AccountEnquiriesCannotAmendPaymentTerms.cy.ts
@@ -66,7 +66,7 @@ describe('Account Enquiry Payment Terms', () => {
   );
 
   it('AC1.2ai: Display appropriate message based on defendant account status (CS)', { tags: ['PO-1801'] }, () => {
-    let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
     headerMock.debtor_type = 'individual';
     headerMock.account_status_reference.account_status_code = 'CS';
     let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
@@ -85,7 +85,7 @@ describe('Account Enquiry Payment Terms', () => {
   });
 
   it('AC1.2ai: Display appropriate message based on defendant account status (WO)', { tags: ['PO-1801'] }, () => {
-    let headerMock = structuredClone(createDefendantHeaderMockWithName('Alice', 'Smith'));
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
     headerMock.debtor_type = 'individual';
     headerMock.account_status_reference.account_status_code = 'WO';
     let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
@@ -104,7 +104,7 @@ describe('Account Enquiry Payment Terms', () => {
   });
 
   it('AC1.2ai: Display appropriate message based on defendant account status (TO)', { tags: ['PO-1801'] }, () => {
-    let headerMock = structuredClone(createDefendantHeaderMockWithName('Alice', 'Smith'));
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
     headerMock.debtor_type = 'individual';
     headerMock.account_status_reference.account_status_code = 'TO';
     let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
@@ -123,7 +123,7 @@ describe('Account Enquiry Payment Terms', () => {
   });
 
   it('AC1.2ai: Display appropriate message based on defendant account status (TA)', { tags: ['PO-1801'] }, () => {
-    let headerMock = structuredClone(createDefendantHeaderMockWithName('Alice', 'Smith'));
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
     headerMock.debtor_type = 'individual';
     headerMock.account_status_reference.account_status_code = 'TA';
     let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
@@ -142,7 +142,7 @@ describe('Account Enquiry Payment Terms', () => {
   });
 
   it('AC1.2ai: Display appropriate message based on defendant account status (TS)', { tags: ['PO-1801'] }, () => {
-    let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
     headerMock.debtor_type = 'individual';
     headerMock.account_status_reference.account_status_code = 'TS';
     let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
@@ -161,7 +161,7 @@ describe('Account Enquiry Payment Terms', () => {
   });
 
   it('AC1.2aii: If the account has a zero balance,', { tags: ['PO-1801'] }, () => {
-    let headerMock = structuredClone(createDefendantHeaderMockWithName('Alice', 'Smith'));
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
     headerMock.payment_state_summary.account_balance = 0.0;
     headerMock.debtor_type = 'individual';
     headerMock.account_status_reference.account_status_code = 'TS';
@@ -185,7 +185,7 @@ describe('Account Enquiry Payment Terms', () => {
     'AC1.2b: Navigate to error screen if user lacks Amend Payment Terms permission in account BU',
     { tags: ['PO-1801'] },
     () => {
-      let headerMock = structuredClone(createDefendantHeaderMockWithName('Alice', 'Smith'));
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
       headerMock.debtor_type = 'individual';
       headerMock.account_status_reference.account_status_code = 'TA';
       let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
@@ -402,6 +402,192 @@ describe('Account Enquiry Payment Terms', () => {
         organisation_name: 'Acme Corporation',
         organisation_aliases: [],
       };
+      headerMock.account_status_reference.account_status_code = 'TA';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU17);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('REM');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p')
+        .contains(
+          'You do not have the required permissions to make changes to this account as it is outside your business unit.',
+        )
+        .should('exist');
+    },
+  );
+
+  it(
+    'AC1: Display Change link for users with Amend Payment Terms permission and show error screen if extend_ttp_disallow is TRUE for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
+      headerMock.parent_guardian_party_id = '1770000001';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+      paymentTermsMock.last_enforcement = 'DW';
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('DW');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p').contains('This account has an enforcement action outstanding: DW.').should('exist');
+    },
+  );
+
+  it(
+    'AC1.2ai: Display appropriate message based on defendant account status (CS) for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
+      headerMock.account_status_reference.account_status_code = 'CS';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('REM');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p').contains('This account has been consolidated.').should('exist');
+    },
+  );
+
+  it(
+    'AC1.2ai: Display appropriate message based on defendant account status (WO) for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
+      headerMock.account_status_reference.account_status_code = 'WO';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('REM');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p').contains('This account has been written-off.').should('exist');
+    },
+  );
+
+  it(
+    'AC1.2ai: Display appropriate message based on defendant account status (TO) for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
+      headerMock.account_status_reference.account_status_code = 'TO';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('REM');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p').contains('This account has been transferred out.').should('exist');
+    },
+  );
+
+  it(
+    'AC1.2ai: Display appropriate message based on defendant account status (TA) for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
+      headerMock.account_status_reference.account_status_code = 'TA';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('REM');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p').contains('This account has been transferred out.').should('exist');
+    },
+  );
+
+  it(
+    'AC1.2ai: Display appropriate message based on defendant account status (TS) for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
+      headerMock.account_status_reference.account_status_code = 'TS';
+      let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+      const accountId = headerMock.defendant_account_party_id;
+      interceptAuthenticatedUser();
+      interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+      interceptDefendantHeader(accountId, headerMock, '123');
+      interceptPaymentTerms(accountId, paymentTermsMock, '123');
+      interceptResultByCode('REM');
+      setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+      cy.get('router-outlet').should('exist');
+      cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+      cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+      cy.get('p').contains('This account has been transferred out.').should('exist');
+    },
+  );
+
+  it('AC1.2aii: If the account has a zero balance for Parent/Guardian', { tags: ['PO-1801'] }, () => {
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+    headerMock.payment_state_summary.account_balance = 0.0;
+    headerMock.debtor_type = 'Parent/Guardian';
+    headerMock.account_status_reference.account_status_code = 'TS';
+    let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+
+    const accountId = headerMock.defendant_account_party_id;
+    interceptAuthenticatedUser();
+    interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+    interceptDefendantHeader(accountId, headerMock, '123');
+
+    interceptPaymentTerms(accountId, paymentTermsMock, '123');
+    interceptResultByCode('REM');
+    setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+    cy.get('router-outlet').should('exist');
+    cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Change').click();
+    cy.get('h1').contains(' You cannot amend the payment terms of this account.').should('exist');
+    cy.get('p').contains('The account has a zero balance.').should('exist');
+  });
+
+  it(
+    'AC1.2b: Navigate to error screen if user lacks Amend Payment Terms permission in account BU for Parent/Guardian',
+    { tags: ['PO-1801'] },
+    () => {
+      let headerMock = structuredClone(createDefendantHeaderMockWithName('John', 'Smith'));
+      headerMock.debtor_type = 'Parent/Guardian';
       headerMock.account_status_reference.account_status_code = 'TA';
       let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
 
