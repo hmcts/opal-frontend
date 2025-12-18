b9f923be45e5acb513aac0a8df77c08ad7061279
louisbriggs <louis.briggs@gmail.com>
2025-12-18 13:42:23 +0000

    Removing unused constants

--- src/app/flows/fines/fines-acc/fines-acc-request-payment-card-access-denied/fines-acc-request-payment-card-access-denied.component.ts
+++ src/app/flows/fines/fines-acc/fines-acc-request-payment-card-access-denied/fines-acc-request-payment-card-access-denied.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FinesAccountStore } from '../stores/fines-acc.store';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GovukHeadingWithCaptionComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-heading-with-caption';
-import { FINES_ACC_PAYMENT_TERMS_AMEND_DENIED_ACCOUNT_STATUS_MAP } from './constants/fines-acc-payment-terms-amend-denied-account-status-map.constant';
 
 @Component({
   selector: 'app-fines-acc-request-payment-card-access-denied',
@@ -14,11 +13,7 @@ export class FinesAccRequestPaymentCardAccessDeniedComponent {
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
   public readonly accountStore = inject(FinesAccountStore);
-  public readonly accountStatusCode: keyof typeof FINES_ACC_PAYMENT_TERMS_AMEND_DENIED_ACCOUNT_STATUS_MAP =
-    this.router.currentNavigation()?.extras.state?.['accountStatusCode'];
   public readonly deniedType = this.route.snapshot.paramMap.get('type');
-  public readonly accountStatusDescription: string =
-    FINES_ACC_PAYMENT_TERMS_AMEND_DENIED_ACCOUNT_STATUS_MAP[this.accountStatusCode];
   public readonly lastEnforcement = this.router.currentNavigation()?.extras.state?.['lastEnforcement'];
 
   /**
