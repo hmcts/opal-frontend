64b0c770b2452d9b3c5c2ea563eef9acd07db95b
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-12-24 14:15:12 +0000

    deleting not required mock file

--- src/app/flows/fines/fines-acc/routing/resolvers/mocks/defendant-account-payment-terms-latest.mocks.ts
+++
@@ -1,3 +0,0 @@
-export { MOCK_PAYMENT_TERMS_DATA } from './defendant-account-payment-terms-data.mock';
-export { MOCK_RESULT_DATA } from './defendant-account-payment-terms-result-data.mock';
-export { MOCK_TRANSFORMED_FORM } from './defendant-account-payment-terms-transformed-form.mock';
