c6961e88ac5e9f0f20cd33a79a41b03431463b73
Frankie Moran <frankie.jack.moran@gmail.com>
2026-01-06 15:41:21 +0000

    Fix constant as wording had changed

--- src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/fines-mac-payment-terms-system-generated-notes.constant.ts
+++ src/app/flows/fines/fines-mac/fines-mac-payment-terms/constants/fines-mac-payment-terms-system-generated-notes.constant.ts
@@ -6,5 +6,5 @@ interface FinesMacPaymentTermsSystemGeneratedNotes {
 export const FINES_MAC_PAYMENT_TERMS_SYSTEM_GENERATED_NOTES: FinesMacPaymentTermsSystemGeneratedNotes = {
   COLLECTION_ORDER_MADE:
     'A collection order was previously made on {collectionOrderDate} prior to this account creation',
-  COLLECTION_ORDER_MADE_TODAY: 'A collection order was made today by {user}',
+  COLLECTION_ORDER_MADE_TODAY: 'A collection order has been made by {user} using Authorised Functions',
 };
