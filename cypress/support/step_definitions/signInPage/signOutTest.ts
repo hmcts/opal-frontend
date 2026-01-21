2ed2c82c6cf9bfb43b95e7770ce0aca1e4314dca
TimGonella <timothy.gonella@hmcts.net>
2026-01-21 11:44:51 +0000

    Archive old steps definitions and features (#2140)
    Co-authored-by: Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>

--- cypress/support/step_definitions/signInPage/signOutTest.ts
+++
@@ -1,8 +0,0 @@
-import { When } from '@badeball/cypress-cucumber-preprocessor';
-
-When('I attempt to get back to the dashboard by changing the url', () => {
-  cy.visit('/dashboard');
-});
-When('I attempt to get back to the manual account creation, create account screen by changing the url', () => {
-  cy.visit('/fines/manual-account-creation/create-account');
-});
