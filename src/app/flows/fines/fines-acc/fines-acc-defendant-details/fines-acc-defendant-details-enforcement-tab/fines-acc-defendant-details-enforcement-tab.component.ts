f17f46ecb11f0dc6f5faf87992f7acb29ed0f00e
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2026-01-26 11:34:03 +0000

    PO-1650

--- src/app/flows/fines/fines-acc/fines-acc-defendant-details/fines-acc-defendant-details-enforcement-tab/fines-acc-defendant-details-enforcement-tab.component.ts
+++ src/app/flows/fines/fines-acc/fines-acc-defendant-details/fines-acc-defendant-details-enforcement-tab/fines-acc-defendant-details-enforcement-tab.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { IFinesAccSummaryTabsContentStyles } from '../interfaces/fines-acc-summary-tabs-content-styles.interface';
 import { FINES_ACC_SUMMARY_TABS_CONTENT_STYLES } from '../../constants/fines-acc-summary-tabs-content-styles.constant';
-import { GovukSummaryCardListComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-summary-card-list';
+import { GovukSummaryCardListComponent,GovukSummaryCardActionComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-summary-card-list';
 import { IOpalFinesAccountDefendantDetailsEnforcementTabRefData } from '@services/fines/opal-fines-service/interfaces/opal-fines-account-defendant-details-enforcement-tab-ref-data.interface';
 import {
   GovukSummaryListComponent,
   GovukSummaryListRowComponent,
+  
 } from '@hmcts/opal-frontend-common/components/govuk/govuk-summary-list';
 import { MojBadgeComponent } from '@hmcts/opal-frontend-common/components/moj/moj-badge';
 import { FinesNotProvidedComponent } from '../../../components/fines-not-provided/fines-not-provided.component';
@@ -18,13 +19,14 @@ import { GovukDetailsComponent } from '@hmcts/opal-frontend-common/components/go
     GovukSummaryCardListComponent,
     GovukSummaryListComponent,
     GovukSummaryListRowComponent,
+    GovukSummaryCardActionComponent,
     MojBadgeComponent,
     FinesNotProvidedComponent,
     DatePipe,
     TitleCasePipe,
     GovukTagComponent,
     GovukDetailsComponent,
-  ],
+],
   templateUrl: './fines-acc-defendant-details-enforcement-tab.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
