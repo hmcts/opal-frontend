7ff69b2b1f1f2dd94780c895740c10eb9c997212
Jonathan.Duffy <Jonathan.Duffy@cgi.com>
2026-01-27 14:23:24 +0000

    implement request payment card functionality related tests

--- cypress/component/fineAccountEnquiry/accountEnquiry/paymentTerms/AccountEnquiriesCannotRequestPaymentCard.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/paymentTerms/AccountEnquiriesCannotRequestPaymentCard.cy.ts
@@ -15,7 +15,7 @@ import {
 import { IComponentProperties } from '../setup/setupComponent.interface';
 import { setupAccountEnquiryComponent } from '../setup/SetupComponent';
 
-describe('Account Enquiry Payment Terms - Cannot add payment card', () => {
+describe('Account Enquiry Payment Terms - Payment card', () => {
   beforeEach(() => {
     interceptAuthenticatedUser();
   });
@@ -79,5 +79,55 @@ describe('Account Enquiry Payment Terms - Cannot add payment card', () => {
     cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Request payment card').click();
     cy.get('h1').contains('You cannot request a payment card.');
     cy.get('p').contains('Your business unit does not allow payment card requests.');
+  });
+
+  it('AC1a: User can request a payment card and sees the confirmation screen', { tags: ['PO-1700'] }, () => {
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+    headerMock.debtor_type = 'individual';
+    let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+    paymentTermsMock.last_enforcement = 'REM';
+
+    const accountId = headerMock.defendant_account_party_id;
+    interceptAuthenticatedUser();
+    interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+    interceptDefendantHeader(accountId, headerMock, '123');
+    interceptPaymentTerms(accountId, paymentTermsMock, '123');
+    interceptResultByCode('REM');
+    setupAccountEnquiryComponent({
+      ...componentProperties,
+      accountId: accountId,
+    });
+    cy.get('router-outlet').should('exist');
+
+    cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Request payment card').click();
+    cy.get('h1').should('contain.text', 'Do you want to request a payment card for the defendant?');
+    cy.contains('button', 'Yes - request a payment card').should('exist');
+    cy.contains('a', 'No - cancel').should('exist');
+  });
+
+  it('AC1bi: Cancel returns to Payment terms with no changes made', { tags: ['PO-1700'] }, () => {
+    let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+    headerMock.debtor_type = 'individual';
+    let paymentTermsMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_PAYMENT_TERMS_LATEST_MOCK);
+    paymentTermsMock.last_enforcement = 'REM';
+
+    const accountId = headerMock.defendant_account_party_id;
+    interceptAuthenticatedUser();
+    interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+    interceptDefendantHeader(accountId, headerMock, '123');
+    interceptPaymentTerms(accountId, paymentTermsMock, '123');
+    interceptResultByCode('REM');
+    setupAccountEnquiryComponent({
+      ...componentProperties,
+      accountId: accountId,
+    });
+    cy.get('router-outlet').should('exist');
+
+    cy.get(PAYMENT_TERMS_TAB.paymentTermsLink).contains('Request payment card').click();
+    cy.contains('a', 'No - cancel').click();
+
+    cy.get(PAYMENT_TERMS_TAB.tabName).should('contain.text', 'Payment terms');
+    cy.get('opal-lib-moj-alert[type="success"]').should('not.exist');
+    cy.get(PAYMENT_TERMS_TAB.paymentCardRequested).should('contain.text', '11 October 2025');
   });
 });
---
+++ cypress/e2e/functional/opal/features/fineAccountEnquiry/accountEnquiry/AccountEnquiriesRequestPaymentCard.feature
@@ -0,0 +1,42 @@
+Feature: Account Enquiries – Request Payment Card
+  As an Opal user
+  I want to request a payment card for a defendant
+  So that I can confirm or cancel the request
+
+  Background:
+    Given I am logged in with email "opal-test@HMCTS.NET"
+    And I clear all approved accounts
+
+  Rule: Eligible account baseline
+    Background:
+      Given I create a "adultOrYouthOnly" draft account with the following details and set status "Publishing Pending":
+        | Account_status                          | Submitted                       |
+        | account.defendant.forenames             | Jamie                           |
+        | account.defendant.surname               | PayCardRequest{uniq}            |
+        | account.defendant.email_address_1       | Jamie.PayCardRequest{uniq}@test.com |
+        | account.defendant.telephone_number_home | 02078259314                     |
+        | account.account_type                    | Fine                            |
+        | account.prosecutor_case_reference       | PCR-AUTO-013                    |
+        | account.collection_order_made           | false                           |
+        | account.collection_order_made_today     | false                           |
+        | account.payment_card_request            | false                           |
+        | account.defendant.dob                   | 2002-05-15                      |
+      When I search for the account by last name "PayCardRequest{uniq}" and open the latest result
+      Then I should see the page header contains "Mr Jamie PAYCARDREQUEST{uniqUpper}"
+      When I go to the Payment terms section
+
+    @PO-1803
+    Scenario: Confirming a payment card request shows success and updates the last requested date
+      When I start a payment card request
+      And I confirm the payment card request
+      Then I should see the payment card request success message
+      And the payment card last requested date should be today date
+
+    @PO-1803
+    Scenario: Requesting a payment card when one already exists shows an error
+      When I start a payment card request
+      And I confirm the payment card request
+      Then I should see the payment card request success message
+      When I start a payment card request
+      And I confirm the payment card request
+      Then I should see the payment card request already exists error
