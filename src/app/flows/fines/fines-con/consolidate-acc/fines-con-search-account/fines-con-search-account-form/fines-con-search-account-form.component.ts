e6ffeaa2fb34d3d9093e73f26b45707adc9d3b52
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2026-03-02 09:50:20 +0000

    storing form data on tab switch.

--- src/app/flows/fines/fines-con/consolidate-acc/fines-con-search-account/fines-con-search-account-form/fines-con-search-account-form.component.ts
+++ src/app/flows/fines/fines-con/consolidate-acc/fines-con-search-account/fines-con-search-account-form/fines-con-search-account-form.component.ts
@@ -130,4 +130,13 @@ export class FinesConSearchAccountFormComponent extends AbstractFormBaseComponen
     this.initialFormSetup();
     super.ngOnInit();
   }
+
+  /**
+   * Persists in-progress search criteria when the search tab is unmounted
+   * (for example when switching to another tab).
+   */
+  public override ngOnDestroy(): void {
+    this.setSearchAccountTemporary();
+    super.ngOnDestroy();
+  }
 }
