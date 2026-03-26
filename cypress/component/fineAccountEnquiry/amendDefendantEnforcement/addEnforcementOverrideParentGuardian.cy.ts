531e4261b921734977cb943f0859b8486c28d48d
cadefaulkner <cade.faulkner@hmcts.net>
2026-03-26 16:02:45 +0000

    feat: refactor enforcement override tests to use shared locators and common setup - wip

--- cypress/component/fineAccountEnquiry/amendDefendantEnforcement/addEnforcementOverrideParentGuardian.cy.ts
+++ cypress/component/fineAccountEnquiry/amendDefendantEnforcement/addEnforcementOverrideParentGuardian.cy.ts
@@ -1,159 +1,143 @@
-import { mount } from 'cypress/angular';
-import { of } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { FinesAccEnfOverrideAddChangeComponent } from 'src/app/flows/fines/fines-acc/fines-acc-enf-override-add-change/fines-acc-enf-override-add-change.component';
-import { FinesAccEnfOverrideAddChangeFormComponent } from 'src/app/flows/fines/fines-acc/fines-acc-enf-override-add-change/fines-acc-enf-override-add-change-form/fines-acc-enf-override-add-change-form.component';
-import { FinesAccountStore } from 'src/app/flows/fines/fines-acc/stores/fines-acc.store';
-import { FinesAccPayloadService } from 'src/app/flows/fines/fines-acc/services/fines-acc-payload.service';
-import { OpalFines } from 'src/app/flows/fines/services/opal-fines-service/opal-fines.service';
-import { UtilsService } from '@hmcts/opal-frontend-common/services/utils-service';
-import { DOM_ELEMENTS } from './constants/add_enforcement_override_elements';
+import { DOM_ELEMENTS as ENF_OVR } from '../../../shared/selectors/account-enquiry/account.enquiry.enforcement-override-add.locators';
+import { ACCOUNT_ENQUIRY_ENFORCEMENT_STATUS_ELEMENTS as ENF } from '../../../shared/selectors/account-enquiry/account.enquiry.enforcement.locators';
+import { setupAccountEnquiryComponent } from '../accountEnquiry/setup/SetupComponent';
+import { IComponentProperties } from '../accountEnquiry/setup/setupComponent.interface';
+import {
+  interceptAuthenticatedUser,
+  interceptUserState,
+  interceptResultByCode,
+  interceptEnforcementOverrideResults,
+  interceptLocalJusticeAreas,
+  interceptEnforcers,
+} from 'cypress/component/CommonIntercepts/CommonIntercepts';
+import { USER_STATE_MOCK_PERMISSION_BU77 } from 'cypress/component/CommonIntercepts/CommonUserState.mocks';
+import {
+  interceptDefendantHeader,
+  interceptEnforcementStatus,
+  interceptPatchDefendantAccount,
+} from '../accountEnquiry/intercept/defendantAccountIntercepts';
+import { createDefendantHeaderMockWithName } from '../accountEnquiry/mocks/defendant_details_mock';
+import { OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_ENFORCEMENT_TAB_REF_DATA_MOCK } from '@app/flows/fines/services/opal-fines-service/mocks/opal-fines-account-defendant-details-enforcement-tab-ref-data.mock';
 
-const mockRoute: ActivatedRoute = {
-  snapshot: {
-    data: {
-      title: 'Add enforcement override',
-      enforcersRefData: {
-        refData: [
-          { enforcer_id: 'E1', enforcer_code: 'EC1', name: 'Enforcer One' },
-          { enforcer_id: 'E2', enforcer_code: 'EC2', name: 'Enforcer Two' },
-        ],
-      },
-      localJusticeAreasRefData: {
-        refData: [
-          { local_justice_area_id: 'L1', name: 'LJA One' },
-          { local_justice_area_id: 'L2', name: 'LJA Two' },
-        ],
-      },
-      resultsRefData: {
-        refData: [
-          { result_id: 'R1', result_title: 'Result One', requires_enforcer: true },
-          { result_id: 'R2', result_title: 'Result Two' },
-        ],
-      },
-    },
-  },
-} as any;
-
-const mockStore = {
-  party_name: () => 'Test Person',
-  getAccountNumber: () => '123456',
-  account_id: () => 1001,
-  base_version: () => '1',
-  business_unit_id: () => '2002',
-  setSuccessMessage: () => {},
+const componentProperties: IComponentProperties = {
+  accountId: '77',
+  fragments: 'enforcement',
+  interceptedRoutes: [
+    '/access-denied',
+    '../note/add',
+    '../debtor/individual/amend',
+    '../debtor/parentGuardian/amend',
+    '../enforcement/amend',
+    '../enforcement/amend-denied',
+    // Add more routes here as needed
+  ],
 };
+function commonSetup() {
+  let headerMock = structuredClone(createDefendantHeaderMockWithName('Robert', 'Thomson'));
+  headerMock.debtor_type = 'individual';
+  let enforcementMock = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_ENFORCEMENT_TAB_REF_DATA_MOCK);
+  enforcementMock.enforcement_override!.enforcement_override_result = {
+    enforcement_override_result_id: null,
+    enforcement_override_result_name: null,
+  };
 
-const mockPayloadService = {
-  buildEnforcementOverrideFormPayload: () => ({ enforcement_override: {} }),
-};
+  const accountId = headerMock.defendant_account_party_id;
+  interceptAuthenticatedUser();
+  interceptUserState(USER_STATE_MOCK_PERMISSION_BU77);
+  interceptDefendantHeader(accountId, headerMock, '123');
+  interceptEnforcementStatus(accountId, enforcementMock, '123');
 
-const mockOpalFinesService = {
-  getEnforcerPrettyName: (e: { name: string; enforcer_code: string }) => `${e.name} (${e.enforcer_code})`,
+  interceptEnforcementOverrideResults();
+  interceptLocalJusticeAreas();
+  interceptEnforcers();
 
-  getLocalJusticeAreaPrettyName: (lja: { name: string; local_justice_area_id: string }) =>
-    `${lja.name} (${lja.local_justice_area_id})`,
+  ['ABDC', 'AEOC', 'BWTD', 'BWTU', 'CLAMPO', 'CWN', 'DW', 'MAN', 'NBWT', 'REGF', 'SUMA', 'TFOOUT'].forEach((code) =>
+    interceptResultByCode(code),
+  );
+  interceptPatchDefendantAccount();
 
-  getResultPrettyName: (r: { result_title: string; result_id: string }) => `${r.result_title} (${r.result_id})`,
-
-  getResult: () => of({ requires_enforcer: false, requires_lja: false }),
-  patchDefendantAccount: () => of({}),
-};
-
-const mockUtilsService = {
-  scrollToTop: () => {},
-};
-
-const setup = () => {
-  return mount(FinesAccEnfOverrideAddChangeComponent, {
-    providers: [
-      { provide: ActivatedRoute, useValue: mockRoute },
-      { provide: FinesAccountStore, useValue: mockStore },
-      { provide: FinesAccPayloadService, useValue: mockPayloadService },
-      { provide: OpalFines, useValue: mockOpalFinesService },
-      { provide: UtilsService, useValue: mockUtilsService },
-    ],
-  });
-};
+  setupAccountEnquiryComponent({ ...componentProperties, accountId: accountId });
+  cy.get(ENF.addEnforcementOverrideLink).click();
+}
 
 describe('Add Enforcement Override', () => {
   it('AC1a, AC1b. Should render the form with title', () => {
-    setup();
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.title).should('contain.text', 'Test Person - 123456');
-    cy.get(DOM_ELEMENTS.title).should('contain.text', 'Add enforcement override');
+    cy.get(ENF_OVR.title).should('contain.text', 'Mr Robert THOMSON - 177A');
+    cy.get(ENF_OVR.title).should('contain.text', 'Add enforcement override');
   });
 
   it('AC1c, AC1d. Select an enforcement override dropdown, add override button and cancel link', () => {
-    setup();
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).click();
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('ABDC').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('AEOC').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTD').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTU').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('CLAMPO').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('CWN').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('DW').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('FSN').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('MAN').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('NBWT').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('REGF').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('SUMA').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('TFOOUT').should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).click();
+    cy.get(ENF_OVR.dropdownOptions).contains('ABDC').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('AEOC').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTD').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTU').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('CLAMPO').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('CWN').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('DW').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('FSN').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('MAN').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('NBWT').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('REGF').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('SUMA').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('TFOOUT').should('exist');
 
-    cy.get(DOM_ELEMENTS.addOverrideButton).should('exist');
-    cy.get(DOM_ELEMENTS.cancelLink).should('exist');
+    cy.get(ENF_OVR.addOverrideButton).should('exist');
+    cy.get(ENF_OVR.cancelLink).should('exist');
   });
 
   it('AC2. Enforcer dropdown for valid override', () => {
-    setup();
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).click().type('AB');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('ABDC').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('AEOC').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTD').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTU').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('CLAMPO').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('CWN').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('DW').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('FSN').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('MAN').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('NBWT').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('REGF').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('SUMA').should('not.exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('TFOOUT').should('not.exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).click().type('AB');
+    cy.get(ENF_OVR.dropdownOptions).contains('ABDC').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('AEOC').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTD').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTU').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('CLAMPO').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('CWN').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('DW').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('FSN').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('MAN').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('NBWT').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('REGF').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('SUMA').should('not.exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('TFOOUT').should('not.exist');
 
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('ABDC').click();
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('have.value', 'ABDC');
+    cy.get(ENF_OVR.dropdownOptions).contains('ABDC').click();
+    cy.get(ENF_OVR.enfOverrideDropdown).should('contain.value', 'ABDC');
 
-    cy.get(DOM_ELEMENTS.enforcerDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.enforcerDropdown).click();
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('<PlaceholderEnforcer>').should('exist');
+    cy.get(ENF_OVR.enforcerDropdown).should('exist');
+    cy.get(ENF_OVR.enforcerDropdown).click();
+    cy.get(ENF_OVR.dropdownOptions).contains('The DWP (3)').should('exist');
   });
 
   it('AC3. LJA dropdown for valid override', () => {
-    setup();
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).click().type('TFO');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('TFOOUT').should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).click().type('TFO');
+    cy.get(ENF_OVR.dropdownOptions).contains('TFOOUT').should('exist');
 
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('TFOOUT').click();
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('have.value', 'TFOOUT');
-    cy.get(DOM_ELEMENTS.localJusticeAreaDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.localJusticeAreaDropdown).click();
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('<PlaceholderLJA>').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('TFOOUT').click();
+    cy.get(ENF_OVR.enfOverrideDropdown).should('contain.value', 'TFOOUT');
+    cy.get(ENF_OVR.localJusticeAreaDropdown).should('exist');
+    cy.get(ENF_OVR.localJusticeAreaDropdown).click();
+    cy.get(ENF_OVR.dropdownOptions).contains("Bedfordshire Magistrates' Court (4165)").should('exist');
   });
 
   it('AC4a. Error when no enforcement override is selected', () => {
-    setup();
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.addOverrideButton).click();
-    cy.get(DOM_ELEMENTS.errorSummary)
+    cy.get(ENF_OVR.enfOverrideDropdown).should('exist');
+    cy.get(ENF_OVR.addOverrideButton).click();
+    cy.get(ENF_OVR.errorSummary)
       .should('exist')
       .contains('There is a problem')
       .next()
@@ -161,36 +145,36 @@ describe('Add Enforcement Override', () => {
   });
 
   it('AC4b. Error when no enforcer is selected', () => {
-    setup();
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).click().type('BW');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTD').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTU').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('BWTD').click();
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('have.value', 'BWTD');
-    cy.get(DOM_ELEMENTS.enforcerDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).click().type('BW');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTD').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTU').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('BWTD').click();
+    cy.get(ENF_OVR.enfOverrideDropdown).should('contain.value', 'BWTD');
+    cy.get(ENF_OVR.enforcerDropdown).should('exist');
 
-    cy.get(DOM_ELEMENTS.addOverrideButton).click();
-    cy.get(DOM_ELEMENTS.errorSummary)
+    cy.get(ENF_OVR.addOverrideButton).click();
+    cy.get(ENF_OVR.errorSummary)
       .should('exist')
       .contains('There is a problem')
       .next()
       .should('contain.text', 'Select an enforcer');
   });
 
-  it('AC4c. Error when no LJA is selected', () => {
-    setup();
+  it.only('AC4c. Error when no LJA is selected', () => {
+    commonSetup();
 
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('exist');
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).click().type('TFO');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('TFOOUT').should('exist');
-    cy.get(DOM_ELEMENTS.dropdownOptions).contains('TFOOUT').click();
-    cy.get(DOM_ELEMENTS.enfOverrideDropdown).should('have.value', 'TFOOUT');
-    cy.get(DOM_ELEMENTS.localJusticeAreaDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).should('exist');
+    cy.get(ENF_OVR.enfOverrideDropdown).click().type('TFO');
+    cy.get(ENF_OVR.dropdownOptions).contains('TFOOUT').should('exist');
+    cy.get(ENF_OVR.dropdownOptions).contains('TFOOUT').click();
+    cy.get(ENF_OVR.enfOverrideDropdown).should('contain.value', 'TFOOUT');
+    cy.get(ENF_OVR.localJusticeAreaDropdown).should('exist');
 
-    cy.get(DOM_ELEMENTS.addOverrideButton).click();
-    cy.get(DOM_ELEMENTS.errorSummary)
+    cy.get(ENF_OVR.addOverrideButton).click();
+    cy.get(ENF_OVR.errorSummary)
       .should('exist')
       .contains('There is a problem')
       .next()
