649f9c651f87622ac6bd483b5cd6954205a3c82f
Frankie Moran <frankie.jack.moran@gmail.com>
2026-03-31 13:57:56 +0100

    feat: add FinesSaPayloadService and transform payload functionality

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,6 +1,16 @@
+import '@angular/compiler';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import 'zone.js';
 import 'zone.js/testing';
 import { GovukRadioComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-radio';
 import { MojDatePickerComponent } from '@hmcts/opal-frontend-common/components/moj/moj-date-picker';
+
+const testBed = getTestBed();
+
+if (!testBed.platform) {
+  testBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+}
 
 // JSDOM cannot initialize GOV.UK and MOJ frontend widgets used by shared components.
 // Keep component behavior testable without executing browser-only initializers.
