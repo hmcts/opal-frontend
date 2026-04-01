61db0226aef954ac81cb19e7ce2240ab00dca51b
Nipa1410 <nipa.patel@hmcts.net>
2026-04-01 15:16:11 +0100

    verify future pay-by-date updated message  in MAC payment terms

--- cypress/component/manualAccountCreation/FinesMacPaymentTerms/FinesMacPaymentTerms.cy.ts
+++ cypress/component/manualAccountCreation/FinesMacPaymentTerms/FinesMacPaymentTerms.cy.ts
@@ -707,7 +707,9 @@ describe('FinesMacPaymentTermsComponent', () => {
 
           finesMacState.paymentTerms.formData.fm_payment_terms_payment_terms = 'payInFull';
           finesMacState.paymentTerms.formData.fm_payment_terms_pay_by_date = '01/01/2033';
-          cy.get(DOM_ELEMENTS.mojTicketPanel).should('contain', ERROR_MESSAGES.dateInFuture);
+          cy.get(DOM_ELEMENTS.mojTicketPanel)
+            .should('contain', ERROR_MESSAGES.dateInFuture)
+            .and('contain', ERROR_MESSAGES.dateInFutureMessage);
         });
       });
     },
