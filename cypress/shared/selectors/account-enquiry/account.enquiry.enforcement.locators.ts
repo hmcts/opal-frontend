fb5a7993e469f0ad651d0499d2d65c8c1dd2d8a8
Ant-Collins <ant.collins@hmcts.net>
2026-04-10 14:06:13 +0100

    Added component tests to cover PO-1870, PO-1869, PO-1871

--- cypress/shared/selectors/account-enquiry/account.enquiry.enforcement.locators.ts
+++ cypress/shared/selectors/account-enquiry/account.enquiry.enforcement.locators.ts
@@ -49,4 +49,5 @@ export const ACCOUNT_ENQUIRY_ENFORCEMENT_STATUS_ELEMENTS = {
   detailsReason: '#enforcementActionDetailsReasonKey',
 
   actionsColumnHeader: '.govuk-grid-column-one-third > .govuk-\\!-margin-bottom-2',
+  changeEnforcementOverrideLink: '#enforcementOverrideDetailsEnforcement_overrideActions > .govuk-link',
 } as const;
