c344d9d47155eae5dd6b82e879cd877f6f118c21
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2026-04-21 13:39:47 +0100

    adding in extension to interface

--- src/app/flows/fines/fines-acc/fines-acc-enf-action-select/interfaces/fines-acc-enf-action-select-routing-paths.interface.ts
+++ src/app/flows/fines/fines-acc/fines-acc-enf-action-select/interfaces/fines-acc-enf-action-select-routing-paths.interface.ts
@@ -1,4 +1,6 @@
-export interface IFinesAccEnfActionRoutingPaths {
+import { IChildRoutingPaths } from '@hmcts/opal-frontend-common/pages/routing/interfaces';
+
+export interface IFinesAccEnfActionRoutingPaths extends IChildRoutingPaths {
   root: string;
   children: {
     select: string;
