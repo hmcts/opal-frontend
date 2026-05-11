6005b34b1ef15b7619b108ee8687edf34815da10
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2026-05-11 14:09:15 +0100

    changing routing to route constant

--- src/app/flows/fines/fines-acc/fines-acc-defendant-details/fines-acc-defendant-details.component.spec.ts
+++ src/app/flows/fines/fines-acc/fines-acc-defendant-details/fines-acc-defendant-details.component.spec.ts
@@ -26,6 +26,7 @@ import { FINES_ACC_ENF_COURT_CHANGE_ROUTING_PATHS } from '../fines-acc-enf-court
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { FINES_ACC_ENF_OVERRIDE_ADD_CHANGE_ROUTING_PATHS } from '../fines-acc-enf-override-add-change/constants/fines-acc-enf-override-add-change-routing-paths.constant';
 import { FINES_ACC_ENF_ACTION_ROUTING_PATHS } from '../fines-acc-enf-action-select/constants/fines-acc-enf-action-select-routing-paths.constant';
+import { FINES_ACC_REMOVE_NON_PAYING_PG_ROUTING_PATHS } from '../fines-acc-remove-non-paying-pg/constants/fines-acc-remove-non-paying-pg-routing-paths.constant';
 
 describe('FinesAccDefendantDetailsComponent', () => {
   let component: FinesAccDefendantDetailsComponent;
@@ -466,7 +467,9 @@ describe('FinesAccDefendantDetailsComponent', () => {
 
     expect(event.preventDefault).toHaveBeenCalled();
     expect(routerSpy.navigate).toHaveBeenCalledWith(
-      [`../${FINES_ACC_DEFENDANT_ROUTING_PATHS.children.remove}/non-paying/parent-guardian`],
+      [
+        `../${FINES_ACC_DEFENDANT_ROUTING_PATHS.children.remove}/${FINES_ACC_REMOVE_NON_PAYING_PG_ROUTING_PATHS.root}/${FINES_ACC_REMOVE_NON_PAYING_PG_ROUTING_PATHS.children.parentGuardian}`,
+      ],
       {
         relativeTo: component['activatedRoute'],
       },
--- src/app/flows/fines/fines-acc/routing/fines-acc.routes.ts
+++ src/app/flows/fines/fines-acc/routing/fines-acc.routes.ts
@@ -33,6 +33,7 @@ import { FINES_ACC_ENF_ACTION_ROUTING_PATHS } from '../fines-acc-enf-action-sele
 import { FINES_ACC_ENF_ACTION_ROUTING_TITLES } from '../fines-acc-enf-action-select/constants/fines-acc-enf-action-select-routing-titles.constant';
 import { nextPermittedEnfActionsResolver } from './resolvers/defendant-account-next-permitted-enf-actions.resolver';
 import { FINES_ACC_PAYMENT_HOLD_ROUTING_PATHS } from '../fines-acc-payment-hold-add-remove/constants/fines-acc-payment-hold-routing-paths.constant';
+import { FINES_ACC_REMOVE_NON_PAYING_PG_ROUTING_PATHS } from '../fines-acc-remove-non-paying-pg/constants/fines-acc-remove-non-paying-pg-routing-paths.constant';
 
 const accRootPermissionIds = FINES_PERMISSIONS;
 
@@ -171,7 +172,7 @@ export const routing: Routes = [
             },
           },
           {
-            path: `${FINES_ACC_DEFENDANT_ROUTING_PATHS.children.remove}/non-paying/parent-guardian`,
+            path: `${FINES_ACC_DEFENDANT_ROUTING_PATHS.children.remove}/${FINES_ACC_REMOVE_NON_PAYING_PG_ROUTING_PATHS.root}/${FINES_ACC_REMOVE_NON_PAYING_PG_ROUTING_PATHS.children.parentGuardian}`,
 
             loadComponent: () =>
               import('../fines-acc-remove-non-paying-pg/fines-acc-remove-non-paying-pg.component').then(
