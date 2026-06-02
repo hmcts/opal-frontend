0b3f01ae146ffeb57863a7b17c08ca0a7ad2d8e7
louisbriggs <louis.briggs@gmail.com>
2026-06-02 09:24:37 +0100

    Extending helpers to include release_1c_write_off

--- src/app/flows/fines/constants/feature-flag-section-permission-exclusions.constant.ts
+++ src/app/flows/fines/constants/feature-flag-section-permission-exclusions.constant.ts
@@ -1,9 +1,10 @@
 import { type FeatureFlagPermissionExclusionsBySection } from '../types/feature-flag-permission-exclusions-by-section.type';
 import { FEATURE_FLAG_RELEASE_PERMISSIONS } from './feature-flag-release-permissions.constant';
-import { RELEASE_1A_FEATURE_FLAG } from './release-feature-flags.constant';
+import { RELEASE_1A_FEATURE_FLAG, RELEASE_1C_WRITE_OFF_FEATURE_FLAG } from './release-feature-flags.constant';
 
 export const FEATURE_FLAG_SECTION_PERMISSION_EXCLUSIONS: FeatureFlagPermissionExclusionsBySection = {
   accounts: {
     [RELEASE_1A_FEATURE_FLAG]: FEATURE_FLAG_RELEASE_PERMISSIONS[RELEASE_1A_FEATURE_FLAG] ?? [],
+    [RELEASE_1C_WRITE_OFF_FEATURE_FLAG]: FEATURE_FLAG_RELEASE_PERMISSIONS[RELEASE_1C_WRITE_OFF_FEATURE_FLAG] ?? [],
   },
 };
