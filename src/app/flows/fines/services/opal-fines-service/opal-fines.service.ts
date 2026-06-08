48387ed5a355ec7601c8c068413c280ea29b4002
louisbriggs <louis.briggs@gmail.com>
2026-06-08 09:52:47 +0100

    Changing to mock data until endpoint is available.

--- src/app/flows/fines/services/opal-fines-service/opal-fines.service.ts
+++ src/app/flows/fines/services/opal-fines-service/opal-fines.service.ts
@@ -61,6 +61,7 @@ import { IOpalFinesAccountMinorCreditorCreditor } from './interfaces/opal-fines-
 import { IOpalFinesDraftAccountPatchRequestPayload } from '@services/fines/opal-fines-service/types/opal-fines-draft-account-patch-request-payload.type';
 import { IOpalFinesDeleteDefendantAccountPartyPayload } from './interfaces/opal-fines-delete-defendant-account-party-payload.interface';
 import { IOpalFinesAccountMajorCreditorDetailsHeader } from '../../fines-acc/fines-acc-major-creditor-details/interfaces/fines-acc-major-creditor-details-header.interface';
+import { FINES_ACC_MAJOR_CREDITOR_DETAILS_HEADER_MOCK } from '../../fines-acc/fines-acc-major-creditor-details/mocks/fines-acc-major-creditor-details-header.mock';
 
 @Injectable({
   providedIn: 'root',
@@ -1230,16 +1231,17 @@ export class OpalFines {
   public getMajorCreditorAccountHeadingData(
     accountId: number,
   ): Observable<IOpalFinesAccountMajorCreditorDetailsHeader> {
-    const url = `${OPAL_FINES_PATHS.majorCreditorAccounts}/${accountId}/header-summary`;
-    return this.http.get<IOpalFinesAccountMajorCreditorDetailsHeader>(url, { observe: 'response' }).pipe(
-      map((response: HttpResponse<IOpalFinesAccountMajorCreditorDetailsHeader>) => {
-        const payload = response.body as IOpalFinesAccountMajorCreditorDetailsHeader;
-        const version = this.extractEtagVersion(response.headers);
-        return {
-          ...payload,
-          version,
-        };
-      }),
-    );
+    // const url = `${OPAL_FINES_PATHS.majorCreditorAccounts}/${accountId}/header-summary`;
+    return of(FINES_ACC_MAJOR_CREDITOR_DETAILS_HEADER_MOCK);
+    // return this.http.get<IOpalFinesAccountMajorCreditorDetailsHeader>(url, { observe: 'response' }).pipe(
+    //   map((response: HttpResponse<IOpalFinesAccountMajorCreditorDetailsHeader>) => {
+    //     const payload = response.body as IOpalFinesAccountMajorCreditorDetailsHeader;
+    //     const version = this.extractEtagVersion(response.headers);
+    //     return {
+    //       ...payload,
+    //       version,
+    //     };
+    //   }),
+    // );
   }
 }
