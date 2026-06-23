8df37e2597a37fd39719a62737aa1927606deeb9
GarethLangley1 <127423185+GarethLangley1@users.noreply.github.com>
2026-06-23 18:49:57 +0100

    PO-7266 implement release-1c-administration and release-1c-financial-movements feature flag functionality (#2722)
    Co-authored-by: Nipa1410 <nipa.patel@hmcts.net>

--- src/app/flows/fines/routing/guards/dashboard-landing/dashboard-landing.guard.spec.ts
+++ src/app/flows/fines/routing/guards/dashboard-landing/dashboard-landing.guard.spec.ts
@@ -13,9 +13,11 @@ import { REPORTS_PERMISSIONS } from '@app/flows/fines/constants/reports-permissi
 import { SEARCH_PERMISSIONS } from '@app/flows/fines/constants/search-permissions.constant';
 import {
   RELEASE_1A_FEATURE_FLAG,
+  RELEASE_1C_ADMINISTRATION_FEATURE_FLAG,
   RELEASE_1B_FEATURE_FLAG,
   RELEASE_1C_ENFORCEMENT_OPERATIONAL_REPORTING_FEATURE_FLAG,
   RELEASE_1C_WRITE_OFF_FEATURE_FLAG,
+  RELEASE_1C_FINANCIAL_MOVEMENTS_FEATURE_FLAG,
 } from '@app/flows/fines/constants/release-feature-flags.constant';
 
 const resolveFeatureFlagGuardMock = vi.fn();
@@ -24,6 +26,8 @@ const DEFAULT_RELEASE_FEATURE_FLAGS = {
   [RELEASE_1B_FEATURE_FLAG]: true,
   [RELEASE_1C_WRITE_OFF_FEATURE_FLAG]: true,
   [RELEASE_1C_ENFORCEMENT_OPERATIONAL_REPORTING_FEATURE_FLAG]: true,
+  [RELEASE_1C_ADMINISTRATION_FEATURE_FLAG]: true,
+  [RELEASE_1C_FINANCIAL_MOVEMENTS_FEATURE_FLAG]: true,
 };
 
 const mockFeatureFlags = (featureFlags: Record<string, boolean>) => {
@@ -237,6 +241,16 @@ describe('dashboardLandingGuard', () => {
     );
     expect(resolveFeatureFlagGuardMock).toHaveBeenCalledWith(
       RELEASE_1C_ENFORCEMENT_OPERATIONAL_REPORTING_FEATURE_FLAG,
+      expect.any(Object),
+      expect.any(Object),
+    );
+    expect(resolveFeatureFlagGuardMock).toHaveBeenCalledWith(
+      RELEASE_1C_ADMINISTRATION_FEATURE_FLAG,
+      expect.any(Object),
+      expect.any(Object),
+    );
+    expect(resolveFeatureFlagGuardMock).toHaveBeenCalledWith(
+      RELEASE_1C_FINANCIAL_MOVEMENTS_FEATURE_FLAG,
       expect.any(Object),
       expect.any(Object),
     );
