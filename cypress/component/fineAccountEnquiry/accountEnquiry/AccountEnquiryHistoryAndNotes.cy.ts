5d7498dfa66da8cfcc0374d3539c5fcc9c92f0ff
Justin <ju.morgan@cgi.com>
2026-06-26 14:24:09 +0100

    yarn lint style fix up

--- cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryHistoryAndNotes.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryHistoryAndNotes.cy.ts
@@ -34,7 +34,10 @@ describe('Account Enquiry History and notes', () => {
     cy.get('router-outlet').should('exist');
   };
 
-  it('AC2a. will render the history and notes tab shell with the current filter controls', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8303'] }, () => {
+  it(
+    'AC2a. will render the history and notes tab shell with the current filter controls',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8303'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.get(HISTORY_AND_NOTES_TAB.pageHeader).should('exist');
@@ -55,9 +58,13 @@ describe('Account Enquiry History and notes', () => {
       cy.get(HISTORY_AND_NOTES_TAB.notesCheckbox).should('exist');
       cy.get(HISTORY_AND_NOTES_TAB.paymentTermsCheckbox).should('exist');
       cy.get(HISTORY_AND_NOTES_TAB.filterButton).should('be.visible').and('contain.text', 'Filter');
-    });
+    },
+  );
 
-  it('AC2a. should send an initial unfiltered history request when the history and notes tab loads', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8304'] }, () => {
+  it(
+    'AC2a. should send an initial unfiltered history request when the history and notes tab loads',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8304'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes').then(({ request, response }) => {
@@ -70,9 +77,13 @@ describe('Account Enquiry History and notes', () => {
         expect(response?.statusCode).to.equal(200);
         expect(response?.body).to.deep.equal(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_HISTORY_AND_NOTES_TAB_REF_DATA_MOCK);
       });
-    });
+    },
+  );
 
-  it('AC2b. should send dateFrom and dateTo query params when both dates are entered and Filter is selected', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8305'] }, () => {
+  it(
+    'AC2b. should send dateFrom and dateTo query params when both dates are entered and Filter is selected',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8305'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -92,9 +103,13 @@ describe('Account Enquiry History and notes', () => {
         expect(response?.statusCode).to.equal(200);
         expect(response?.body).to.deep.equal(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_HISTORY_AND_NOTES_TAB_REF_DATA_MOCK);
       });
-    });
+    },
+  );
 
-  it('AC2b. should omit dateTo when Date to is left blank and Filter is selected', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8306'] }, () => {
+  it(
+    'AC2b. should omit dateTo when Date to is left blank and Filter is selected',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8306'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -114,9 +129,13 @@ describe('Account Enquiry History and notes', () => {
         expect(response?.statusCode).to.equal(200);
         expect(response?.body).to.deep.equal(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_HISTORY_AND_NOTES_TAB_REF_DATA_MOCK);
       });
-    });
+    },
+  );
 
-  it('AC2b. should omit dateFrom when Date to is left blank and Filter is selected', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8307'] }, () => {
+  it(
+    'AC2b. should omit dateFrom when Date to is left blank and Filter is selected',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8307'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -136,9 +155,13 @@ describe('Account Enquiry History and notes', () => {
         expect(response?.statusCode).to.equal(200);
         expect(response?.body).to.deep.equal(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_HISTORY_AND_NOTES_TAB_REF_DATA_MOCK);
       });
-    });
+    },
+  );
 
-  it('AC2b. should show an error and not send a filtered request when Date from is later than Date to', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8308'] }, () => {
+  it(
+    'AC2b. should show an error and not send a filtered request when Date from is later than Date to',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8308'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -157,9 +180,13 @@ describe('Account Enquiry History and notes', () => {
         .and('contain.text', 'Date from must be the same as or earlier than Date to');
 
       cy.get('@getHistoryAndNotes.all').should('have.length', 1);
-    });
+    },
+  );
 
-  it('AC2c. should send itemTypes when a single category is selected and Filter is selected', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8309'] }, () => {
+  it(
+    'AC2c. should send itemTypes when a single category is selected and Filter is selected',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8309'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -178,9 +205,13 @@ describe('Account Enquiry History and notes', () => {
         expect(response?.statusCode).to.equal(200);
         expect(response?.body).to.deep.equal(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_HISTORY_AND_NOTES_TAB_REF_DATA_MOCK);
       });
-    });
+    },
+  );
 
-  it('AC2c. should send a comma-separated itemTypes list when all categories are selected and Filter is selected', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8310'] }, () => {
+  it(
+    'AC2c. should send a comma-separated itemTypes list when all categories are selected and Filter is selected',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8310'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -203,9 +234,13 @@ describe('Account Enquiry History and notes', () => {
         expect(response?.statusCode).to.equal(200);
         expect(response?.body).to.deep.equal(OPAL_FINES_ACCOUNT_DEFENDANT_DETAILS_HISTORY_AND_NOTES_TAB_REF_DATA_MOCK);
       });
-    });
+    },
+  );
 
-  it('AC2d. should not send a filtered request when filter values are changed without selecting Filter', { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8311'] }, () => {
+  it(
+    'AC2d. should not send a filtered request when filter values are changed without selecting Filter',
+    { tags: [...buildTags('@JIRA-STORY:PO-2633', '@JIRA-EPIC:PO-2621'), '@JIRA-TEST-KEY:PO-8311'] },
+    () => {
       setupHistoryAndNotesScreen(structuredClone(DEFENDANT_HEADER_MOCK));
 
       cy.wait('@getHistoryAndNotes');
@@ -216,5 +251,6 @@ describe('Account Enquiry History and notes', () => {
       cy.get(HISTORY_AND_NOTES_TAB.notesCheckbox).check();
 
       cy.get('@getHistoryAndNotes.all').should('have.length', 1);
-    });
+    },
+  );
 });
