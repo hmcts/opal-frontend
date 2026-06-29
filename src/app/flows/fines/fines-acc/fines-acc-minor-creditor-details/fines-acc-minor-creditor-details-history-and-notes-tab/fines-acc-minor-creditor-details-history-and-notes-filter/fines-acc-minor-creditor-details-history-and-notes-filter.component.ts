3bd56d07d266f9282ce0658b157de25a9d1c2802
louisbriggs <louis.briggs@gmail.com>
2026-06-29 14:40:07 +0100

    Moving history and notes filter component up a level to be shared between defendant and minor creditor accounts.

--- src/app/flows/fines/fines-acc/fines-acc-minor-creditor-details/fines-acc-minor-creditor-details-history-and-notes-tab/fines-acc-minor-creditor-details-history-and-notes-filter/fines-acc-minor-creditor-details-history-and-notes-filter.component.ts
+++ src/app/flows/fines/fines-acc/fines-acc-minor-creditor-details/fines-acc-minor-creditor-details-history-and-notes-tab/fines-acc-minor-creditor-details-history-and-notes-filter/fines-acc-minor-creditor-details-history-and-notes-filter.component.ts
@@ -1,15 +1,23 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { AbstractFormParentBaseComponent } from '@hmcts/opal-frontend-common/components/abstract/abstract-form-parent-base';
+import { FinesAccHistoryAndNotesFilterFormComponent } from '../../../fines-acc-history-and-notes/fines-acc-history-and-notes-filter-form/fines-acc-history-and-notes-filter-form.component';
+import { IFinesAccHistoryAndNotesFilterForm } from '../../../fines-acc-history-and-notes/interfaces/fines-acc-history-and-notes-filter-form.interface';
+import { FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_FILTER_CATEGORIES } from '../constants/fines-acc-minor-creditor-details-history-and-notes-filter-categories.constant';
+import { FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_FILTER_FIELD_ERRORS } from '../constants/fines-acc-minor-creditor-details-history-and-notes-filter-field-errors.constant';
+import { FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_FILTER_SUMMARY_TEXT } from '../constants/fines-acc-minor-creditor-details-history-and-notes-filter-summary-text.constant';
 import { IFinesAccMinorCreditorDetailsHistoryAndNotesFilterForm } from '../interfaces/fines-acc-minor-creditor-details-history-and-notes-filter-form.interface';
-import { FinesAccMinorCreditorDetailsHistoryAndNotesFilterFormComponent } from './fines-acc-minor-creditor-details-history-and-notes-filter-form/fines-acc-minor-creditor-details-history-and-notes-filter-form.component';
 
 @Component({
   selector: 'app-fines-acc-minor-creditor-details-history-and-notes-filter',
-  imports: [FinesAccMinorCreditorDetailsHistoryAndNotesFilterFormComponent],
+  imports: [FinesAccHistoryAndNotesFilterFormComponent],
   templateUrl: './fines-acc-minor-creditor-details-history-and-notes-filter.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FinesAccMinorCreditorDetailsHistoryAndNotesFilterComponent extends AbstractFormParentBaseComponent {
+  public readonly categories = FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_FILTER_CATEGORIES;
+  public readonly fieldErrors = FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_FILTER_FIELD_ERRORS;
+  public readonly summaryText = FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_FILTER_SUMMARY_TEXT;
+
   @Input() public filterForm: IFinesAccMinorCreditorDetailsHistoryAndNotesFilterForm | null = null;
   @Input() public filterOpen = false;
   @Output() public filterApplied = new EventEmitter<IFinesAccMinorCreditorDetailsHistoryAndNotesFilterForm>();
@@ -20,8 +28,8 @@ export class FinesAccMinorCreditorDetailsHistoryAndNotesFilterComponent extends 
    *
    * @param form - The submitted history and notes filter form.
    */
-  public handleFilterSubmit(form: IFinesAccMinorCreditorDetailsHistoryAndNotesFilterForm): void {
-    this.filterApplied.emit(form);
+  public handleFilterSubmit(form: IFinesAccHistoryAndNotesFilterForm): void {
+    this.filterApplied.emit(form as IFinesAccMinorCreditorDetailsHistoryAndNotesFilterForm);
   }
 
   /**
