bd705ed794b3ba7e99f037307e80cc3a82983155
TimDanielsCQI <275969854+TimDanielsCQI@users.noreply.github.com>
2026-07-10 11:45:20 +0100

    Restore explicit report configuration

--- src/app/flows/fines/fines-reports/fines-reports-summary-list/constants/fines-reports-summary-list-report-configuration.constant.ts
+++ src/app/flows/fines/fines-reports/fines-reports-summary-list/constants/fines-reports-summary-list-report-configuration.constant.ts
@@ -1,10 +1,25 @@
-import { FINES_REPORTS_DEFINITIONS } from '../../constants/fines-reports-definitions.constant';
+import { OPERATIONAL_REPORT_ROUTE_PERMISSIONS } from '@app/flows/fines/constants/operational-report-route-permissions.constant';
+import { FINES_REPORTS_SUMMARY_LIST_ROUTING_PATHS } from '../routing/constants/fines-reports-summary-list-routing-paths.constant';
+import { FINES_REPORTS_SUMMARY_LIST_ROUTING_TITLES } from '../routing/constants/fines-reports-summary-list-routing-titles.constant';
 import { IFinesReportSummaryListReportConfiguration } from '../interfaces/fines-report-summary-list-report-configuration.interface';
 
-export const FINES_REPORT_SUMMARY_LIST_REPORT_CONFIGURATION: IFinesReportSummaryListReportConfiguration[] =
-  FINES_REPORTS_DEFINITIONS.map(({ id, heading, title, permissionIds }) => ({
-    id,
-    heading,
-    title,
-    permissionIds,
-  }));
+export const FINES_REPORT_SUMMARY_LIST_REPORT_CONFIGURATION: IFinesReportSummaryListReportConfiguration[] = [
+  {
+    id: FINES_REPORTS_SUMMARY_LIST_ROUTING_PATHS.children.yourReports,
+    heading: 'Your reports',
+    title: FINES_REPORTS_SUMMARY_LIST_ROUTING_TITLES.children.yourReports,
+    permissionIds: [],
+  },
+  {
+    id: FINES_REPORTS_SUMMARY_LIST_ROUTING_PATHS.children.operationalReportsByEnforcement,
+    heading: 'Operational reports (by enforcement)',
+    title: FINES_REPORTS_SUMMARY_LIST_ROUTING_TITLES.children.operationalReportsByEnforcement,
+    permissionIds: OPERATIONAL_REPORT_ROUTE_PERMISSIONS,
+  },
+  {
+    id: FINES_REPORTS_SUMMARY_LIST_ROUTING_PATHS.children.operationalReportsByPayments,
+    heading: 'Operational reports (by payments)',
+    title: FINES_REPORTS_SUMMARY_LIST_ROUTING_TITLES.children.operationalReportsByPayments,
+    permissionIds: OPERATIONAL_REPORT_ROUTE_PERMISSIONS,
+  },
+];
