307c3f64f52237901007bcf075010a88452e530d
Bredan Pires <bredan.pires1@hmcts.net>
2026-07-20 12:12:17 +0100

    PO-2673 changes

--- src/app/flows/fines/fines-acc/fines-acc-summary-header/fines-acc-summary-header.component.ts
+++ src/app/flows/fines/fines-acc/fines-acc-summary-header/fines-acc-summary-header.component.ts
@@ -2,20 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FinesAccountStoreType } from '../types/fines-account-store.type';
 import { FinesAccBannerMessagesComponent } from '../fines-acc-banner-messages/fines-acc-banner-messages.component';
 import { CustomPageHeaderComponent } from '@hmcts/opal-frontend-common/components/custom/custom-page-header';
-import { GovukHeadingWithCaptionComponent } from '@hmcts/opal-frontend-common/components/govuk/govuk-heading-with-caption';
 import { GovukButtonDirective } from '@hmcts/opal-frontend-common/directives/govuk-button';
 import { MojButtonMenuComponent } from '@hmcts/opal-frontend-common/components/moj/moj-button-menu';
 
 @Component({
   selector: 'app-fines-acc-summary-header',
   templateUrl: './fines-acc-summary-header.component.html',
-  imports: [
-    FinesAccBannerMessagesComponent,
-    CustomPageHeaderComponent,
-    GovukHeadingWithCaptionComponent,
-    GovukButtonDirective,
-    MojButtonMenuComponent,
-  ],
+  imports: [FinesAccBannerMessagesComponent, CustomPageHeaderComponent, GovukButtonDirective, MojButtonMenuComponent],
 })
 export class FinesAccSummaryHeaderComponent {
   @Input({ required: true }) accountStore!: FinesAccountStoreType;
