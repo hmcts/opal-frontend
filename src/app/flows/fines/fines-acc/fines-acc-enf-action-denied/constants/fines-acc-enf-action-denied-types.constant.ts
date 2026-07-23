428cd2564ceb162c1a15708ee610996f0e9b2377
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2026-07-23 09:20:37 +0100

    PO-5754

--- src/app/flows/fines/fines-acc/fines-acc-enf-action-denied/constants/fines-acc-enf-action-denied-types.constant.ts
+++ src/app/flows/fines/fines-acc/fines-acc-enf-action-denied/constants/fines-acc-enf-action-denied-types.constant.ts
@@ -1,6 +1,5 @@
 export const FINES_ACC_ENF_ACTION_DENIED_TYPES = {
   permission: 'permission',
-  accountStatus: 'account-status',
   enforcementHold: 'enforcement-hold',
   noNextActions: 'no-next-actions',
   employmentData: 'employment-data',
