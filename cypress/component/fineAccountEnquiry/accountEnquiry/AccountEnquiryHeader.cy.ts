8b76d4d50a51c5de0181c0a2530a0d2102940548
benjitelagalapudi <benji.telagalapudi@hmcts.net>
2026-07-28 19:32:30 +0100

    fix(cypress): clean up failing component specs

--- cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryHeader.cy.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/AccountEnquiryHeader.cy.ts
@@ -57,7 +57,7 @@ describe('Account Enquiry - Defendant Header', () => {
       interceptDefendantHeader(77, DEFENDANT_HEADER_MOCK, '1');
       interceptAtAGlance(77, OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK, '1');
 
-      setupAccountEnquiryComponent(minorCreditorComponentProperties);
+      setupAccountEnquiryComponent(componentProperties);
 
       cy.get(DOM.pageHeader).should('exist');
       cy.get(DOM.headingWithCaption).should('exist');
