a26387ed00e2390d654ff9c34a2f07a0958217e1
Ant-Collins <ant.collins@hmcts.net>
2026-07-30 10:04:34 +0100

    JsDoc added for the at a glance intercept

--- cypress/component/fineAccountEnquiry/accountEnquiry/intercept/defendantAccountIntercepts.ts
+++ cypress/component/fineAccountEnquiry/accountEnquiry/intercept/defendantAccountIntercepts.ts
@@ -51,6 +51,20 @@ export function interceptConsolidatedAccounts(
     .as('getConsolidatedAccounts');
 }
 
+/**
+ * Intercepts the GET request to the defendant accounts "at-a-glance" endpoint and mocks the response.
+ *
+ * @param accountId - The unique identifier for the account.
+ * @param mockData - The mock data to be returned in the response body.
+ * @param respHeaderEtag - The value to set for the ETag response header.
+ * @returns Cypress chainable object with the intercepted request aliased as 'getAtAGlance'.
+ * @example
+ * ```typescript
+ * const mockAtAGlance: IOpalFinesAccountDefendantAtAGlance = structuredClone(OPAL_FINES_ACCOUNT_DEFENDANT_AT_A_GLANCE_MOCK);
+ * interceptAtAGlance(mockAtAGlance, 'W/"123456"');
+ * cy.wait('@getAtAGlance');
+ * ```
+ */
 export function interceptAtAGlance(
   accountId: number,
   mockData: IOpalFinesAccountDefendantAtAGlance,
