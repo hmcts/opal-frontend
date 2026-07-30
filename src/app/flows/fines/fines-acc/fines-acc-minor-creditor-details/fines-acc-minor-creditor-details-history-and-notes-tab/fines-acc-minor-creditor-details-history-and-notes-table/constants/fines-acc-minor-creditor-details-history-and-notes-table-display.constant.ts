4da66c6fc99f0016c32f017d8f3d7c5e495ae986
Bredan Pires <bredan.pires1@hmcts.net>
2026-07-30 12:43:32 +0100

    PO-8957 refactor(fines): simplify Sonar readability issues

--- src/app/flows/fines/fines-acc/fines-acc-minor-creditor-details/fines-acc-minor-creditor-details-history-and-notes-tab/fines-acc-minor-creditor-details-history-and-notes-table/constants/fines-acc-minor-creditor-details-history-and-notes-table-display.constant.ts
+++ src/app/flows/fines/fines-acc/fines-acc-minor-creditor-details/fines-acc-minor-creditor-details-history-and-notes-tab/fines-acc-minor-creditor-details-history-and-notes-table/constants/fines-acc-minor-creditor-details-history-and-notes-table-display.constant.ts
@@ -1,4 +1,1 @@
-import { FINES_ACCOUNT_HISTORY_TABLE_MAPPING_DISPLAY } from '../../../../fines-account-history-table/constants/fines-account-history-table-mapping-display.constant';
-
-export const FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_TABLE_DISPLAY =
-  FINES_ACCOUNT_HISTORY_TABLE_MAPPING_DISPLAY;
+export { FINES_ACCOUNT_HISTORY_TABLE_MAPPING_DISPLAY as FINES_ACC_MINOR_CREDITOR_DETAILS_HISTORY_AND_NOTES_TABLE_DISPLAY } from '../../../../fines-account-history-table/constants/fines-account-history-table-mapping-display.constant';
