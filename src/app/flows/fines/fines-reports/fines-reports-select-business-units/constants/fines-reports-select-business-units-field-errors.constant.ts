f9ed255d5f374bb03336636b6b38324306f30a53
TimDanielsCQI <275969854+TimDanielsCQI@users.noreply.github.com>
2026-08-03 14:24:17 +0100

    PO-2305 Add business unit selection form

---
+++ src/app/flows/fines/fines-reports/fines-reports-select-business-units/constants/fines-reports-select-business-units-field-errors.constant.ts
@@ -0,0 +1,10 @@
+import { IFinesReportsSelectBusinessUnitsFieldErrors } from '../fines-reports-select-business-units-form/interfaces/fines-reports-select-business-units-field-errors.interface';
+
+export const FINES_REPORTS_SELECT_BUSINESS_UNITS_FIELD_ERRORS: IFinesReportsSelectBusinessUnitsFieldErrors = {
+  fines_reports_select_business_unit_ids: {
+    required: {
+      message: 'Select 1 or more business unit',
+      priority: 1,
+    },
+  },
+};
