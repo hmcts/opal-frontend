c7a3eec381f30c5367fbe3e06a2233bd40aeadf7
Gareth Langley <Gareth.Langley@HMCTS.NET>
2026-08-07 12:37:42 +0100

    PO-3480 - update title

--- src/app/flows/fines/fines-mci/routing/constants/fines-mci-routing-titles.constant.ts
+++ src/app/flows/fines/fines-mci/routing/constants/fines-mci-routing-titles.constant.ts
@@ -3,6 +3,6 @@ import { IFinesMciRoutingPaths } from '../interfaces/fines-mci-routing-paths.int
 export const FINES_MCI_ROUTING_TITLES: IFinesMciRoutingPaths = {
   root: 'Manual cash input',
   children: {
-    createAllocate: 'Create and allocate manual cash input',
+    createAllocate: 'Create and allocate tills',
   },
 };
