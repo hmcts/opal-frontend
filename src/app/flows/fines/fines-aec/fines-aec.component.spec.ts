1cfae72c0a49505debcd91ff067dd75d5e36de84
Martin Scullion <Martin.Scullion@HMCTS.NET>
2026-08-13 14:18:51 +0100

    PO-2460 - Further PR comments addressed

--- src/app/flows/fines/fines-aec/fines-aec.component.spec.ts
+++ src/app/flows/fines/fines-aec/fines-aec.component.spec.ts
@@ -1,16 +1,10 @@
-import { ɵresolveComponentResources as resolveComponentResources } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { beforeAll, beforeEach, describe, expect, it } from 'vitest';
-
+import { beforeEach, describe, expect, it } from 'vitest';
 import { FinesAecComponent } from './fines-aec.component';
 
 describe('FinesAec', () => {
   let component: FinesAecComponent;
   let fixture: ComponentFixture<FinesAecComponent>;
-
-  beforeAll(async () => {
-    await resolveComponentResources(() => Promise.resolve(''));
-  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
