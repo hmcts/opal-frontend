8f686b2cb22d9cb697e1110efeb2dc2ca95be8bf
Martin Scullion <Martin.Scullion@HMCTS.NET>
2026-08-28 11:40:22 +0100

    PO-8691 - Correct SonarQube duplication issues

--- src/app/flows/fines/fines-finance/constants/fines-finance-links.constant.ts
+++ src/app/flows/fines/fines-finance/constants/fines-finance-links.constant.ts
@@ -1,6 +1,6 @@
 import { IDashboardPageConfigurationLink } from '@hmcts/opal-frontend-common/pages/dashboard-page/interfaces';
 import { FINES_ROUTING_PATHS } from '../../routing/constants/fines-routing-paths.constant';
-import { FINES_PERMISSIONS } from 'src/app/constants/fines-permissions.constant';
+import { FINES_PERMISSIONS } from '@app/constants/fines-permissions.constant';
 
 const FINES_FINANCE_ROUTE_PREFIX = ['/', FINES_ROUTING_PATHS.root, FINES_ROUTING_PATHS.children.finance.root] as const;
 
@@ -10,38 +10,42 @@ const FINES_FINANCE_LINK_DEFAULTS = {
   style: 'guidance-panel-blue',
 } as const;
 
+const createFinesFinanceLink = (
+  id: string,
+  text: string,
+  route: readonly string[],
+  permissionId: number,
+): IDashboardPageConfigurationLink => ({
+  id,
+  text,
+  routerLink: [...FINES_FINANCE_ROUTE_PREFIX, ...route],
+  ...FINES_FINANCE_LINK_DEFAULTS,
+  permissionIds: [permissionId],
+});
+
 export const FINES_FINANCE_LINKS: IDashboardPageConfigurationLink[] = [
-  {
-    id: 'finesFinanceInboundFilesLink',
-    text: 'Inbound files',
-    routerLink: [
-      ...FINES_FINANCE_ROUTE_PREFIX,
-      FINES_ROUTING_PATHS.children.finance.children['inbound'],
-      FINES_ROUTING_PATHS.children.finance.children['search'],
-    ],
-    ...FINES_FINANCE_LINK_DEFAULTS,
-    permissionIds: [FINES_PERMISSIONS['view-interface-files']],
-  },
-  {
-    id: 'finesFinanceOutboundFilesLink',
-    text: 'Outbound files',
-    routerLink: [
-      ...FINES_FINANCE_ROUTE_PREFIX,
+  createFinesFinanceLink(
+    'finesFinanceInboundFilesLink',
+    'Inbound files',
+    [FINES_ROUTING_PATHS.children.finance.children['inbound'], FINES_ROUTING_PATHS.children.finance.children['search']],
+    FINES_PERMISSIONS['view-interface-files'],
+  ),
+  createFinesFinanceLink(
+    'finesFinanceOutboundFilesLink',
+    'Outbound files',
+    [
       FINES_ROUTING_PATHS.children.finance.children['outbound'],
       FINES_ROUTING_PATHS.children.finance.children['search'],
     ],
-    ...FINES_FINANCE_LINK_DEFAULTS,
-    permissionIds: [FINES_PERMISSIONS['view-interface-files']],
-  },
-  {
-    id: 'finesFinanceUploadVariantBankingFilesLink',
-    text: 'Upload variant banking files',
-    routerLink: [
-      ...FINES_FINANCE_ROUTE_PREFIX,
+    FINES_PERMISSIONS['view-interface-files'],
+  ),
+  createFinesFinanceLink(
+    'finesFinanceUploadVariantBankingFilesLink',
+    'Upload variant banking files',
+    [
       FINES_ROUTING_PATHS.children.finance.children['variantBankingFiles'],
       FINES_ROUTING_PATHS.children.finance.children['upload'],
     ],
-    ...FINES_FINANCE_LINK_DEFAULTS,
-    permissionIds: [FINES_PERMISSIONS['create-interface-files']],
-  },
+    FINES_PERMISSIONS['create-interface-files'],
+  ),
 ];
