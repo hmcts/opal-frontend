836c8b9e25e655c152ab7a8150e6cae40dec9a54
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2026-09-10 10:13:40 +0100

    Comment response

--- src/app/flows/fines/fines-api/stores/fines-api.store.spec.ts
+++ src/app/flows/fines/fines-api/stores/fines-api.store.spec.ts
@@ -132,8 +132,8 @@ describe('FinesApiStore', () => {
   it('should set and clear retained process interface jobs', () => {
     store.setProcessInterfaceJobs(processInterfaceJobs);
 
-    expect(store.processInterfaceJobs()).toEqual(processInterfaceJobs)
-    
+    expect(store.processInterfaceJobs()).toEqual(processInterfaceJobs);
+
     store.clearProcessInterfaceJobs();
 
     expect(store.processInterfaceJobs()).toBeNull();
