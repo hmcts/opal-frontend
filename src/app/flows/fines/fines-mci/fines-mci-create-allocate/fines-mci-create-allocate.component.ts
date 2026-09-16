1e67dbcd5ff1109591082c497b15e0f028157752
Gareth Langley <Gareth.Langley@HMCTS.NET>
2026-09-16 14:57:17 +0100

    PO-3475 - add create till and allocate button UI

--- src/app/flows/fines/fines-mci/fines-mci-create-allocate/fines-mci-create-allocate.component.ts
+++ src/app/flows/fines/fines-mci/fines-mci-create-allocate/fines-mci-create-allocate.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FINES_ROUTING_PATHS } from '@app/flows/fines/routing/constants/fines-routing-paths.constant';
 import { FINES_DASHBOARD_ROUTING_PATHS } from '@app/flows/fines/constants/fines-dashboard-routing-paths.constant';
+import { GovukButtonDirective } from '@hmcts/opal-frontend-common/directives/govuk-button';
 
 @Component({
   selector: 'app-fines-mci-create-allocate',
-  imports: [RouterLink],
+  imports: [RouterLink, GovukButtonDirective],
   templateUrl: './fines-mci-create-allocate.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -16,4 +17,6 @@ export class FinesMciCreateAllocateComponent {
     FINES_DASHBOARD_ROUTING_PATHS.root,
     FINES_DASHBOARD_ROUTING_PATHS.children.finance,
   ];
+
+  public tillsAvailable = true;
 }
