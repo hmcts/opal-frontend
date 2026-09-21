078f419dff8bf13a25c0cd455976f38bb82185b5
bpires2 <134072550+bpires2@users.noreply.github.com>
2026-09-21 12:58:45 +0100

    optional title, forenames and surname

--- src/app/flows/fines/services/opal-fines-service/interfaces/opal-fines-account-minor-creditor-at-a-glance.interface.ts
+++ src/app/flows/fines/services/opal-fines-service/interfaces/opal-fines-account-minor-creditor-at-a-glance.interface.ts
@@ -11,9 +11,9 @@ export interface IOpalFinesAccountMinorCreditorAtAGlance extends IOpalFinesVersi
     account_id: number;
     organisation: boolean;
     organisation_name: string | null;
-    title: string;
-    forenames: string;
-    surname: string;
+    title?: string;
+    forenames?: string;
+    surname?: string;
   } | null;
   payment: {
     is_bacs: boolean;
